711a27fb8e8d488642f022f4e36d142eb70e4037
gmbs <gmbs@matriz>
2015-05-09 13:16:22 -0500

    Milt, PREDICT!

--- miltpredict.php
+++ miltpredict.php
@@ -1,14 +1,14 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "4sale";
-$dbname = "robot";
-$output = array();
+$dbhost	 = "localhost";
+$dbuser	 = "root";
+$dbpass	 = "4sale";
+$dbname	 = "robot";
+$output	 = array();
 try {
-    $dbh = new PDO("mysql:".$dbname.":".$dbhost,$dbuser,$dbpass);
+	$dbh = new PDO("mysql:".$dbname.":".$dbhost, $dbuser, $dbpass);
 } catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-    die(json_encode(''));
+	echo 'Connection failed: '.$e->getMessage();
+	die(json_encode(''));
 }
 
 $q0 = "SELECT distinct auto, msg, tipo, lineas FROM robot.msglist
@@ -17,34 +17,36 @@ and concat(fecha_start,' ',hour_start,':00:00')>now()
 and concat(fecha_end,' ',hour_end,':00:00')<now()
 ;";
 try {
-    $sth = $dbh->query($q0);
+	$sth = $dbh->query($q0);
 } catch (PDOException $e) {
-    echo 'Query failed: ' . $e->getMessage();
+	echo 'Query failed: '.$e->getMessage();
 }
-$result0 = $sth->fetchAll();
-foreach ($result0 as $row0) {
+if ($sth) {
+	$result0 = $sth->fetchAll();
+	foreach ($result0 as $row0) {
 
-$msg=$row0['msg'].'_'.$row0['tipo'];
-$lim=$row0['lineas'];
+		$msg	 = $row0['msg'].'_'.$row0['tipo'];
+		$lim	 = $row0['lineas'];
 
-$q1 = "SELECT auto,id,tel,turno FROM robot.calllist " .
-"WHERE msg = :msg ".
-"AND id <> '' AND tel <> '' ".
-"ORDER BY turno LIMIT ".$lim.";";
-try {
-    $sth1 = $dbh->prepare($q1);
-} catch (PDOException $e) {
-    echo 'Prepare failed: ' . $e->getMessage();
-}
-$sth1->bindParam(':msg', $msg);
-$sth1->execute(); 
-$result = $sth1->fetchAll();
-foreach ($result as $row) {
-$tt = $row[2];
-$auto=$row[0];
-$cta=$row[1];
+		$q1 = "SELECT auto,id,tel,turno FROM robot.calllist ".
+		    "WHERE msg = :msg ".
+		    "AND id <> '' AND tel <> '' ".
+		    "ORDER BY turno LIMIT ".$lim.";";
+		try {
+			$sth1 = $dbh->prepare($q1);
+		} catch (PDOException $e) {
+			echo 'Prepare failed: '.$e->getMessage();
+		}
+		$sth1->bindParam(':msg', $msg);
+		$sth1->execute();
+		$result = $sth1->fetchAll();
+		foreach ($result as $row) {
+			$tt	 = $row[2];
+			$auto	 = $row[0];
+			$cta	 = $row[1];
 //$output[] = array('id' => $auto, 'cuenta' => $cta, 'tel' => $tt, 'msg' => $msg);
-}
+		}
+	}
 }
 $output[] = array('id' => 0, 'cuenta' => 0, 'tel' => '18716578', 'msg' => '');
 echo json_encode($output);
