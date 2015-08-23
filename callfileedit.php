d832d7ab44781c8b37d1679cef7b948e82091713
gmbs <gmbs@matriz>
2015-08-23 08:14:04 -0500

    modernize callfileedit

--- callfileedit.php
+++ callfileedit.php
@@ -1,132 +1,67 @@
-<?php  
-include('admin_hdr_2.php');
-$i=0;
-while ($answercheck=mysql_fetch_row($resultcheck)) {
-if ($answercheck[0]!=1) {}
-else {
-$cliente=mysql_real_escape_string($_GET['cliente']);
-$nombre_deudor=mysql_real_escape_string($_GET['nombre_deudor']);
-$cuenta=mysql_real_escape_string($_GET['cuenta']);
-$numero_de_cuenta=mysql_real_escape_string($_GET['numero_de_cuenta']);
-$id=mysql_real_escape_string($_GET['id']);
-$tel=mysql_real_escape_string($_GET['tel']);
-if (!empty($_GET['go'])) {
-$wherestr='';
-$querymsg="select msg from robot.msglist where client='".$cliente."' LIMIT 1";
-$resultmsg=mysql_query($querymsg);
-while ($answermsg=mysql_fetch_row($resultmsg)) {
-    $wherestring=$wherestring.' msg ="'.$answermsg[0].'"';
+<?php
+require_once 'pdoConnect.php';
+$pdoc          = new pdoConnect();
+$pdo           = $pdoc->dbConnectAdmin();
+$capt          = filter_input(INPUT_GET, 'capt');
+$cliente       = filter_input(INPUT_GET, 'cliente');
+$nombre_deudor = filter_input(INPUT_GET, 'nombre_deudor');
+$id            = filter_input(INPUT_GET, 'id');
+$tel           = filter_input(INPUT_GET, 'tel');
+if (filter_has_var(INPUT_GET, 'go')) {
+    $wherestring = " WHERE cliente <> '' ";
+    if (!empty($cliente)) {
+        $cliente .= ' AND id=:cliente ';
     }
-if (!empty($nombre_deudor)) {
-$wherestring=$wherestring.' AND id IN (select id_cuenta from cobra.resumen where nombre_deudor="'.$nombre_deudor.'")';
-}
-if (!empty($id)) {
-$wherestring=$wherestring.' AND id IN (select id_cuenta from cobra.resumen where numero_de_cuenta="'.$id.'"';
-}
-if (!empty($tel)) {$wherestring=$wherestring.' AND tel="'.$tel.'"';}
-$querydel="UPDATE robot.calllist SET camp=0 WHERE ".$wherestring;
-mysql_query($querydel);
-$querydel2="update cobra.resumen set norobot=1 WHERE ".$wherestring;
-mysql_query($querydel2);
+    if (!empty($nombre_deudor)) {
+        $wherestring .= ' AND id IN (select numero_de_cuenta from cobra.resumen where nombre_deudor=:nombre_deudor) ';
+    }
+    if (!empty($id)) {
+        $wherestring .= ' AND id IN (select numero_de_cuenta from cobra.resumen where numero_de_cuenta=:id) ';
+    }
+    if (!empty($tel)) {
+        $wherestring .= ' AND tel=:tel ';
+    }
+    $querydel = "DELETE FROM robot.calllist WHERE ".$wherestring;
+    $std      = $pdo->prepare($querydel);
+    if (!empty($cliente)) {
+        $std->bindParam(':cliente', $cliente);
+    }
+    if (!empty($nombre_deudor)) {
+        $std->bindParam(':nombre_deudor', $nombre_deudor);
+    }
+    if (!empty($id)) {
+        $std->bindParam(':id', $id);
+    }
+    if (!empty($tel)) {
+        $std->bindParam(':tel', $tel);
+    }
+    $std->execute();
 }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
- "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE HTML>
 
 <html>
-<head>
-<title>ROBOT Edit</title>
-<script type="text/javascript">
-
-function fill(field,thisValue) {
-   var x=document.getElementById("quitar")
-for (var i=0;i<x.length;i++)
-  {if (x.elements[i].id==field) {
-        x.elements[i].value=thisValue;}
-        }
-    }
-
-function lookup(field){
-    var ajaxRequest;  // The variable that makes Ajax possible!
-    var fields=field+"s";	
-    var fieldl=fields+"List";
-    var box=document.getElementById('list');
-    while (box.firstChild) 
- {
-    //The list is LIVE so it will re-index each call
-    box.removeChild(box.firstChild);
- };	
-	try{
-		// Opera 8.0+, Firefox, Safari
-		ajaxRequest = new XMLHttpRequest();
-	} catch (e){
-		// Internet Explorer Browsers
-		try{
-			ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e) {
-			try{
-				ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (e){
-				// Something went wrong
-				alert("Your browser broke!");
-				return false;
-			}
-		}
-	}
-	// Create a function that will receive data sent from the server
-	ajaxRequest.onreadystatechange = function(){
-		if(ajaxRequest.readyState == 4){
-   var out=ajaxRequest.responseText;
-   box.innerHTML=out;
-    }
-	}
-switch(field)
-{
-case "tel":
-  var find = document.getElementById('tel').value;
-  break;
-case "id":
-  var find = document.getElementById('id').value;
-  break;
-case "cliente":
-  var find = document.getElementById('cliente').value;
-  break;
-case "nombre_deudor":
-  var find = document.getElementById('nombre_deudor').value;
-  break;
-default:
-  var find = document.getElementById('tel').value;
-}	
-	var queryString = "?capt=<?php echo $capt;?>&field=" + field + "&find=" + find;
-	ajaxRequest.open("GET", "getrobot.php" + queryString, true);
-	ajaxRequest.send(null); 
-}
-
-
-</script></head>
-<body>
-<?php if (!empty($_GET['go'])) {?>
-<p>Llamadas estan quitado</p>
-<?php } ?>
-<button onclick="window.location='reports.php?capt=<?php echo $capt;?>'">Regressar a la plantilla administrativa</button><br>
-<form id="quitar" name="quitar" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
-<p>Cliente <input type='text' name='cliente' id='cliente' value='<?php echo $cliente;?>' onkeyup="lookup('cliente');">
-</p>
-<p>Nombre <input type='text' name='nombre_deudor' id='nombre_deudor' value='<?php echo $nombre_deudor;?>' onkeyup="lookup('nombre_deudor');">
-</p>
-<p>Cuenta <input type='text' name='id' id='id' value='<?php echo $id;?>' onkeyup="lookup('id');">
-</p>
-<p>Tel&eacute;fono <input type='text' name='tel' id='tel' value='<?php echo $tel;?>' onkeyup="lookup('tel');">
-</p>
-<p id='list'>
-</p>
-<input type="hidden" name="capt" value="<?php echo $capt ?>" />
-<input type="submit" name="go" value="quitar" />
-</form>
-</body>
+    <head>
+        <title>ROBOT Edit</title>
+    </head>
+    <body>
+        <?php if (filter_has_var(INPUT_GET, 'go')) { ?>
+            <p>Llamadas est&aacute;n quitado</p>
+        <?php } ?>
+        <button onclick="window.location = 'reports.php?capt=<?php echo $capt; ?>'">Regressar a la plantilla administrativa</button><br>
+        <form id="quitar" name="quitar" action="callfileedit.php" method="get">
+            <p>Cliente <input type='text' name='cliente' id='cliente' value='<?php echo $cliente; ?>' onkeyup="lookup('cliente');">
+            </p>
+            <p>Nombre <input type='text' name='nombre_deudor' id='nombre_deudor' value='<?php echo $nombre_deudor; ?>' onkeyup="lookup('nombre_deudor');">
+            </p>
+            <p>Cuenta <input type='text' name='id' id='id' value='<?php echo $id; ?>' onkeyup="lookup('id');">
+            </p>
+            <p>Tel&eacute;fono <input type='text' name='tel' id='tel' value='<?php echo $tel; ?>' onkeyup="lookup('tel');">
+            </p>
+            <p id='list'>
+            </p>
+            <input type="hidden" name="capt" value="<?php echo $capt ?>" />
+            <input type="submit" name="go" value="quitar" />
+        </form>
+    </body>
 </html>
-<?php
-}
-}
-mysql_close()
-?>
