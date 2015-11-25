3aa55a5cb039926158bec95bd92d8bc681403028
gmbs <gmbs@matriz.gmbs-consulting.com>
2015-11-25 17:13:31 -0600

    cargaPic

---
+++ cargaPicLoad.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'pdoConnect.php';
+$pdoc   = new pdoConnect();
+$pdo    = $pdoc->dbConnectAdmin();
+$capt   = filter_input(INPUT_POST, 'capt');
+$cuenta = filter_input(INPUT_POST, 'cuenta');
+if ($post['go'] == 'cargar') {
+
+    if ($_FILES["file"]["error"] == 0) {
+        $deststr = "/var/www/pics/".$cuenta.'jpg';
+        move_uploaded_file($_FILES["file"]["tmp_name"], $deststr);
+        $result  = TRUE;
+    }
+}
+include 'cargaPic.php';
