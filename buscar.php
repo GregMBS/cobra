9e923f99f3d1145fb986d62cc862f0eb5d9c66c5
gmbs <gmbs@matriz.gmbs-consulting.com>
2016-04-19 08:03:50 -0500

    buscar

--- buscar.php
+++ buscar.php
@@ -1,186 +1,196 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pwd = "DeathSta1";
-$db = "cobra4";
-$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$con = (mysql_connect($host, $user, $pwd)) or die ("Could not connect to MySQL");
-mysql_query("USE $db") or die ("Could not select $db database");
-$ticket=mysql_real_escape_string($_COOKIE['auth']);
-$capt=mysql_real_escape_string($_REQUEST['capt']);
+require_once 'pdoConnect.php';
+$pdoc	 = new pdoConnect();
+$pdo	 = $pdoc->dbConnectUser();
+$capt	 = filter_input(INPUT_GET, 'capt');
 set_time_limit(300);
-$querycheck="SELECT count(1),max(tipo) FROM nombres WHERE ticket='".$ticket."' AND iniciales='".$capt."';";
-$resultcheck=mysql_query($querycheck) or die("ERROR UM1 - ".mysql_error());
-while ($answercheck=mysql_fetch_row($resultcheck)) {
-if ($answercheck[0]!=1) {
-        $redirector = "Location: index.php";
-        header($redirector);
-}
-else {
-$mytipo=$answercheck[1];
-$field=mysql_real_escape_string ($_REQUEST['field']);
-$find=mysql_real_escape_string ($_REQUEST['find']);
-$from=mysql_real_escape_string ($_REQUEST['from']);
-if (isset($_GET['C_CONT'])) {
-	$C_CONT=mysql_real_escape_string ($_GET['C_CONT']);
+$field	 = filter_input(INPUT_GET, 'field');
+$find	 = filter_input(INPUT_GET, 'find');
+$from	 = filter_input(INPUT_GET, 'from');
+if (filter_has_var(INPUT_GET, 'C_CONT')) {
+	$C_CONT = filter_input(INPUT_GET, 'C_CONT');
 } else {
-	$C_CONT=0;
+	$C_CONT = 0;
 }
-$CLIENTE=mysql_real_escape_string ($_REQUEST['cliente']);
-$querymain = "SELECT SQL_NO_CACHE numero_de_cuenta,nombre_deudor,cliente,
+$CLIENTE	 = filter_input(INPUT_GET, 'cliente');
+/*
+  $querymain	 = "SELECT SQL_NO_CACHE numero_de_cuenta,nombre_deudor,cliente,
 id_cuenta,status_de_credito from resumen where ".$field." like '%".$find."%'";
-if ($field=='id_cuenta')  {$querymain = "SELECT numero_de_cuenta,nombre_deudor,
-cliente,id_cuenta from resumen where id_cuenta = ".$find." order by id_cuenta;";}
-if ($field=='numero_de_cuenta')  {$querymain = "SELECT numero_de_cuenta,
-nombre_deudor,cliente,id_cuenta,status_de_credito from resumen 
-where numero_de_cuenta regexp '".$find."' ";}
-if ($field=='producto')  {$querymain = "SELECT numero_de_cuenta,
-nombre_deudor,cliente,id_cuenta,status_de_credito from resumen 
-where producto regexp '".$find."' ";}
-if ($field=='subproducto')  {$querymain = "SELECT numero_de_cuenta,
-nombre_deudor,cliente,id_cuenta,status_de_credito from resumen 
-where subproducto regexp '".$find."' ";}
-if ($field=='REFS')  {
-    $querymain = "SELECT SQL_NO_CACHE numero_de_cuenta,nombre_deudor,cliente,
-    id_cuenta,status_de_credito 
-    FROM resumen WHERE 
-(nombre_deudor_alterno regexp '".$find."' or 
-nombre_referencia_1 like '%".$find."%' or 
-nombre_referencia_2 like '%".$find."%' or 
-nombre_referencia_3 like '%".$find."%' or 
-nombre_referencia_4 like '%".$find."%')";    
-    }
-if ($field=='ROBOT')  {
-    $querymain = "SELECT SQL_NO_CACHE distinct numero_de_cuenta,nombre_deudor,
-    cliente,id_cuenta,status_de_credito 
-FROM resumen, historia use index (tel) 
-WHERE c_tele LIKE '%".$find."' and c_cont=id_cuenta
-";    
-    }
-if ($field=='EXACTO')  {
-    $querymain = "SELECT SQL_NO_CACHE distinct numero_de_cuenta,nombre_deudor,
-    cliente,id_cuenta,status_de_credito
-FROM resumen
-WHERE numero_de_cuenta= '".$find."' order by numero_de_cuenta";
-    }
-if ($field=='TELS')  {
-    $querymain = "SELECT SQL_NO_CACHE numero_de_cuenta,nombre_deudor,cliente,
-    id_cuenta,status_de_credito FROM resumen WHERE 
-(tel_1 like '%".$find."' or 
-tel_2 like '%".$find."' or 
-tel_3 like '%".$find."' or 
-tel_4 like '%".$find."' or 
-tel_1_alterno like '%".$find."' or 
-tel_2_alterno like '%".$find."' or 
-tel_3_alterno like '%".$find."' or 
-tel_4_alterno like '%".$find."' or 
-tel_1_ref_1 like '%".$find."' or 
-tel_2_ref_1 like '%".$find."' or 
-tel_1_ref_2 like '%".$find."' or 
-tel_2_ref_2 like '%".$find."' or 
-tel_1_ref_3 like '%".$find."' or 
-tel_2_ref_3 like '%".$find."' or 
-tel_1_ref_4 like '%".$find."' or 
-tel_2_ref_4 like '%".$find."' or 
-tel_1_laboral like '%".$find."' or 
-tel_2_laboral like '%".$find."' or 
-tel_1_verif like '%".$find."' or 
-tel_2_verif like '%".$find."' or 
-tel_3_verif like '%".$find."' or 
-tel_4_verif like '%".$find."' or 
-telefonos_marcados like '%".$find."')";    
-    }
-if (strlen($CLIENTE)>1) {$querymain=$querymain." and cliente='".$CLIENTE."'";}
-if ($mytipo!='admin'&&$mytipo!='supervisor') {
-        $querymain=$querymain.";";}
-$result = mysql_query($querymain) or die("ERROR UM2 - ".mysql_error());
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<title>COBRA - Buscar</title>
+ */
+$queryhead	 = "SELECT SQL_NO_CACHE numero_de_cuenta, nombre_deudor,
+cliente, id_cuenta, status_de_credito from resumen ";
+switch ($field) {
+	case 'id_cuenta':
+		$querymain	 = $queryhead."where id_cuenta = :find order by id_cuenta;";
+		break;
+	case 'nombre_deudor':
+		$querymain	 = $queryhead."where nombre_deudor regexp :find ";
+		break;
+	case 'numero_de_cuenta':
+		$querymain	 = $queryhead."where numero_de_cuenta regexp :find ";
+		break;
+	case 'numero_de_credito':
+		$querymain	 = $queryhead."where numero_de_credito regexp :find ";
+		break;
+	case 'domicilio_deudor':
+		$querymain	 = $queryhead."where domicilio_deudor regexp :find ";
+		break;
+	case 'REFS':
+		$querymain	 = $queryhead."WHERE
+(nombre_deudor_alterno regexp :find or
+nombre_referencia_1 regexp :find or
+nombre_referencia_2 regexp :find or
+nombre_referencia_3 regexp :find or
+nombre_referencia_4 regexp :find)";
+		break;
+	case 'TELS':
+		$querymain	 = $queryhead."WHERE
+(tel_1 regexp :find or
+tel_2 regexp :find or
+tel_3 regexp :find or
+tel_4 regexp :find or
+tel_1_alterno regexp :find or
+tel_2_alterno regexp :find or
+tel_3_alterno regexp :find or
+tel_4_alterno regexp :find or
+tel_1_ref_1 regexp :find or
+tel_2_ref_1 regexp :find or
+tel_1_ref_2 regexp :find or
+tel_2_ref_2 regexp :find or
+tel_1_ref_3 regexp :find or
+tel_2_ref_3 regexp :find or
+tel_1_ref_4 regexp :find or
+tel_2_ref_4 regexp :find or
+tel_1_laboral regexp :find or
+tel_2_laboral regexp :find or
+tel_1_verif regexp :find or
+tel_2_verif regexp :find or
+tel_3_verif regexp :find or
+tel_4_verif regexp :find or
+telefonos_marcados regexp :find)";
+		break;
+	case 'ROBOT':
+		$querymain	 = "SELECT SQL_NO_CACHE
+			distinct numero_de_cuenta,nombre_deudor, cliente,
+			id_cuenta, status_de_credito
+			FROM resumen, historia
+			WHERE c_tele REGEXP :find and c_cont=id_cuenta";
+		break;
 
-<style type="text/css">
-       body {font-family: arial, helvetica, sans-serif; font-size: 8pt; background-color: #ffffff; color:#000000;}
-       table {border: 1pt solid #000000;background-color: #ffffff;}
-     tr:hover {background-color: #ff0000;}
-       th {border: 1pt solid #000000;background-color: #ffffff;}
-	.loud {text-align:center; font-weight:bold; color:red;}  
-	.num {text-align:right;}
- </style>
-
-</head>
-<body>
-<h1>BUSCAR</h1>
-<button onClick="window.location='<?php echo $from;?>?go=FROMBUSCAR&i=0&field=id_cuenta&find=<?php echo $C_CONT;?>&capt=<?php echo $capt;?>'">Regresar al resumen</button>
-<table summary="Cuentas">
-<thead>
-<tr>
-<th>CUENTA</th>
-<th>NOMBRE</th>
-<th>CLIENTE</th>
-<th>CAMPAÑA</th>
-</tr>
-</thead>
-<tbody>
-<?php
-$j=0;
-while($row = mysql_fetch_row($result)) {
-$j=$j+1;
-$CUENTA=$row[0];
-$NOMBRE=utf8_decode($row[1]);
-$CLIENTE=$row[2];
-$ID_CUENTA=$row[3];
-$STATUS=$row[4];
-?>
-<tr>
-<td><a<?php if ((preg_match('/ivo/', $STATUS))||(preg_match('/alo/', $STATUS))) { ?> style="color:#c0c0c0;"<?php } ?> href='<?php echo $from;?>?go=FROMBUSCAR&i=0&field=id_cuenta&find=<?php echo $ID_CUENTA;?>&capt=<?php echo $capt;?>&highlight=<?php echo $field?>&hfind=<?php echo $find?>'><?php echo $CUENTA;?></a></td>
-<td><?php echo utf8_decode($NOMBRE);?></td>
-<td><?php echo $CLIENTE;?></td>
-<td><?php echo $STATUS;?></td>
-</tr>
-<?php } ?>
-</tbody>
-</table>
-<div id="searchbox">
-<h2>Buscar</h2>
-<form name="search" method="get" action=
-"buscar.php" id="search">Buscar a: <input type=
-"text" name="find"> en <select name="field">
-<option value="numero_de_cuenta">Cuenta</option>
-<option value="numero_de_credito"># del Grupo</option>
-<option value="nombre_deudor">Nombre</option>
-<option value="domicilio_deudor">Direcci&oacute;n</option>
-<option value="EXACTO">Cuenta Exacta</option>
-<option value="TELS">Telefonos</option>
-<option value="ROBOT">Telefonos marcados</option>
-<option value="REFS">Aval/Referencias</option>
-<option value="id_cuenta">Expediente</option>
-</select><br>
-Client = <select name="cliente">
-<option value=" ">Todos</option>
-<?php 
-$querycl = "SELECT cliente FROM clientes;";
-$resultcl = mysql_query($querycl);
-while ($answercl = mysql_fetch_array($resultcl)) {?>
-<option value="<?php echo $answercl[0];?>"><?php echo $answercl[0];?>
-</option>
-<?php  } ?>
-</select><br>
-<input type="hidden" name="i" value="0">
-<input type="hidden" name="capt" value="<?php if (isset($capt)) {echo $capt;} ?>">
-<input type="hidden" name="go" value="BUSCAR">
-<input type="hidden" name="from" value="resumen.php">
-<input type="submit" name="go1" value="BUSCAR">
-<input type="button" name="cancel" onclick="cancelbox('searchbox')"
-value="Cancel"> 
-</form>
-</div>
-<?php   
-}
-}
-mysql_close($con);
-?>
-</body>
+	default:
+		break;
+        }
+$cliFlag = 0;
+if ((isset($querymain)) && (strlen($CLIENTE) > 1)) {
+	$querymain	 = $querymain." and cliente = :cliente ";
+	$cliFlag	 = 1;
+        }
+if (isset($querymain)) {
+	try {
+	$stm = $pdo->prepare($querymain);
+	$stm->bindParam(':find', $find);
+	if ($cliFlag == 1) {
+		$stm->bindParam(':cliente', $CLIENTE);
+        }
+	$stm->execute();
+	$result = $stm->fetchAll(PDO::FETCH_ASSOC);
+	} catch (PDOException $e) {
+		die($e->getMessage());
+        }
+        }
+        ?>
+<!DOCTYPE html>
+        <html>
+            <head>
+                <title>COBRA - Buscar</title>
+        <meta charset="utf-8">
+	<link href="bower_components/jqueryui/themes/redmond/jquery-ui.css" rel="stylesheet" type="text/css"/>
+	<link href="bower_components/datatables/media/css/jquery.dataTables.css" rel="stylesheet" type="text/css"/>
+        <script src="bower_components/jquery/dist/jquery.js" type="text/javascript"></script>
+	<script src="bower_components/datatables/media/js/jquery.dataTables.js" type="text/javascript"></script>
+        <script src="bower_components/jqueryui/jquery-ui.js" type="text/javascript"></script>
+        <script>
+            $(function() {
+                $('#buscarTable').dataTable({
+			"bPaginate": false,
+			"bJQueryUI": true
+		});
+            });
+        </script>
+            </head>
+            <body>
+                <h1>BUSCAR</h1>
+                <button onClick="window.location = '<?php echo $from; ?>?go=FROMBUSCAR&i=0&field=id_cuenta&find=<?php echo $C_CONT; ?>&capt=<?php echo $capt; ?>'">Regresar al resumen</button>
+	<table class="ui-widget" id="buscarTable">
+	    <thead class="ui-widget-header">
+                        <tr>
+                            <th>CUENTA</th>
+                            <th>NOMBRE</th>
+                            <th>CLIENTE</th>
+                            <th>CAMPAÑA</th>
+                        </tr>
+                    </thead>
+	    <tbody class="ui-widget-c">
+        <?php
+		$j = 0;
+		if ($result) {
+			foreach ($result as $row) {
+				$j		 = $j + 1;
+				$CUENTA		 = $row['numero_de_cuenta'];
+				$NOMBRE		 = $row['nombre_deudor'];
+				$CLIENTE	 = $row['cliente'];
+				$ID_CUENTA	 = $row['id_cuenta'];
+				$STATUS		 = $row['status_de_credito'];
+            ?>
+                            <tr>
+				    <td><a<?php if (preg_match('/-/', $STATUS)) { ?> style="color:#c0c0c0;"<?php } ?> href='<?php echo $from; ?>?go=FROMBUSCAR&i=0&field=id_cuenta&find=<?php echo $ID_CUENTA; ?>&capt=<?php echo $capt; ?>&highlight=<?php echo $field ?>&hfind=<?php echo $find ?>'><?php echo $CUENTA; ?></a></td>
+				    <td><?php echo htmlentities($NOMBRE); ?></td>
+                                <td><?php echo $CLIENTE; ?></td>
+                                <td><?php echo $STATUS; ?></td>
+                            </tr>
+				<?php
+			}
+		}
+		?>
+                    </tbody>
+                </table>
+                <div id="searchbox">
+                    <h2>Buscar</h2>
+                    <form name="search" method="get" action=
+                          "buscar.php" id="search">Buscar a: <input type=
+                                                              "text" name="find"> en <select name="field">
+                            <option value="numero_de_cuenta">Cuenta</option>
+		    <option value="numero_de_credito"># del Grupo</option>
+                            <option value="nombre_deudor">Nombre</option>
+                            <option value="domicilio_deudor">Direcci&oacute;n</option>
+                            <option value="TELS">Telefonos</option>
+                            <option value="ROBOT">Telefonos marcados</option>
+                            <option value="REFS">Aval/Referencias</option>
+                            <option value="id_cuenta">Expediente</option>
+                        </select><br>
+                        Client = <select name="cliente">
+                            <option value=" ">Todos</option>
+        <?php
+		    $querycl	 = "SELECT cliente FROM clientes;";
+		    $resultcl	 = $pdo->query($querycl);
+		    foreach ($resultcl as $answercl) {
+            ?>
+			    <option value="<?php echo $answercl['cliente']; ?>"><?php echo $answercl[0]; ?>
+                                </option>
+                <?php } ?>
+                        </select><br>
+                        <input type="hidden" name="i" value="0">
+		<input type="hidden" name="capt" value="<?php
+		if (isset($capt)) {
+            echo $capt;
+		}
+		?>">
+                        <input type="hidden" name="go" value="BUSCAR">
+                        <input type="hidden" name="from" value="resumen.php">
+                        <input type="submit" name="go1" value="BUSCAR">
+                        <input type="button" name="cancel" onclick="cancelbox('searchbox')"
+                               value="Cancel">
+                    </form>
+                </div>
+    </body>
 </html> 
