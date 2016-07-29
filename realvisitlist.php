f9cc8b54757e45c15a0ef9eb21bc5caf8f5699c3
gmbs <gmbs@server>
2016-07-29 18:59:27 -0500

    more cleanup

--- realvisitlist.php
+++ realvisitlist.php
@@ -1,116 +1,114 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pwd = "DeathSta1";
-$db = "cobrajdlr";
-$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$con = (mysql_connect($host, $user, $pwd)) or die ("Could not connect to MySQL");
-mysql_query("USE $db") or die ("Could not select $db database");
-	 set_time_limit(300);
-$ticket=mysql_real_escape_string($_COOKIE['auth']);
-$capt=mysql_real_escape_string($_REQUEST['capt']);
-$id_cuenta=mysql_real_escape_string($_REQUEST['id_cuenta']);
-$ejecutivo_asignado_call_center=mysql_real_escape_string($_REQUEST['ejecutivo_asignado_call_center']);
-$tcapt=$capt;
-$C_CVGE=$capt;
-if (substr($capt,0,8)=="practica") {$tcapt="practica";}
-$queryg = "SELECT usuaria,tipo FROM nombres join grupos on tipo=grupo WHERE iniciales = '".$capt."';";
-$resultg = mysql_query($queryg) or die(mysql_error());
-while($answerg = mysql_fetch_row($resultg)) {$mynombre=$answerg[0];$mytipo=$answerg[1];}
-if (!empty($mytipo)) {
-$querysub = "SELECT c_cvst,concat(d_fech,' ',c_hrin),if(c_visit is null,c_cvge,c_visit),left(c_obse1,50),c_obse1,auto FROM historia 
-WHERE (historia.C_CONT=".$id_cuenta.") AND (c_visit <> '')
+require_once 'pdoConnect.php'; // returns $pdo
+$pdoc		 = new pdoConnect();
+$pdo		 = $pdoc->dbConnectUser();
+$capt		 = filter_input(INPUT_GET, 'capt');
+$ID_CUENTA	 = filter_input(INPUT_GET, 'id_cuenta');
+$querysub	 = "SELECT c_cvst, concat(d_fech,' ',c_hrin) as fh,
+	if(c_visit is null,c_cvge,c_visit) as gestor,
+	left(c_obse1,50) as short, c_obse1, auto
+	FROM historia
+WHERE (historia.C_CONT=:id_cuenta) AND (c_visit <> '')
 ORDER BY historia.D_FECH DESC, historia.C_HRIN DESC";
-$rowsub = mysql_query($querysub);
+$sts		 = $pdo->prepare($querysub);
+$sts->bindParam(':id_cuenta', $ID_CUENTA);
+$sts->execute();
+$rowsub	 = $sts->fetchAll(PDO::FETCH_ASSOC);
 if (!(empty($rowsub))) {
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<title>Visitas</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<style type="text/css">
-    body {font-family: verdana,arial, helvetica, sans-serif; font-size: 10pt; background-color: #ffffff;color:#000000;}
-	table {color:#000000;}
-	tr {height:2em;}
-	th {width: 9em;}
-	th.gestion {width: 32em;}
-	th.status {width: 16em;}
-	th.timestamp {width: 8em;}
-	th.telefono {width: 8em;}
-	th.chico {width: 5em;}
-  td {border: 1pt solid #000000;background-color: #ffffff; width:10em;}
-	td.gestion {width: 32em;height: 1em;overflow:hidden;}
-	td.status {width: 16em;}
-	td.timestamp {width: 8em;}
-	td.telefono {width: 8em;}
-	td.chico {width: 5em;}
-    #tableContainer {height: 100%; overflow: scroll;}
-</style>
-</head>
-<body>
-<div id="historybox">
-<table summary="historiahead" border='0' cellpadding='0' cellspacing=
-'0' width='100%' id="historyhead">
-<tr>
-<?php 
-$fieldnames = array("Status","Fecha/Hora","Visitador","Gestion","Gestion");
-$fieldsize = array("status", "timestamp",    "chico",   "gestion","hidebox");
-                  for ($j=0; $j<4; $j++) { 
-                    $fieldname = $fieldnames[$j]; ?>
-                    <th<?php echo ' class="'.$fieldsize[$j].'"';?>><?php if (isset($fieldname)) {echo $fieldname;} ?></th> <?php
-                  }
-                ?></tr>
-</table>
-<div id='tableContainer' class='tableContainer'>
-<table summary="historia" border='0' cellpadding='0' cellspacing=
-'0' width='100%' id='historybody'>
-<tbody class="scrollContent"><?php
-                $j=0;
-				$c=0;
-                while ($answer = mysql_fetch_array($rowsub)) { 
-$auto=$answer[5];
-$gestor=utf8_encode($answer[2]);
-$gestion=utf8_encode($answer[4]);
-?>
-<tr>
-<?php 
-                   for ($k=0; $k<4; $k++) { 
-                    $ank = utf8_encode($answer[$k]);
-							      if (is_null($ank)) {$ank="&nbsp;";};
-                    $ank=str_replace('00:00:00', '', $ank);
-                    $jscode='';
-                    if ($fieldsize[$k]=="gestion") {
-											$jscode1=" onClick='alert(";
-											$jscode2=")'";
-											$jscode=$jscode1.'"'.ereg_replace("[\n\r]", " ", $gestion).'"'.$jscode2;
-											}
-										?>
-                    <td<?php if ($c==1) {echo " style='background-color:#dddddd'";}; echo ' class="'.$fieldsize[$k].'"'.$jscode;?>>
-					<?php
-					if (isset($ank)) {echo $ank;} 
+	?>
+	<!DOCTYPE html>
+	<html>
+	    <head>
+		<title>Visitas</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="public/bower_resources/jqueryui/themes/redmond/jquery-ui.css" type="text/css" media="all" />
+		<style>
+		    th {width: 9em;}
+		    th.gestion {width: 32em;}
+		    th.status {width: 16em;}
+		    th.timestamp {width: 8em;}
+		    th.telefono {width: 8em;}
+		    th.chico {width: 5em;}
+		    td {width:10em;}
+		    td.gestion {width: 32em;height: 1em;overflow:hidden;}
+		    td.status {width: 16em;}
+		    td.timestamp {width: 8em;}
+		    td.telefono {width: 8em;}
+		    td.chico {width: 5em;}
+		</style>
+		<script src="public/bower_resources/jquery/dist/jquery.js" type="text/javascript"></script>
+		<script src="public/bower_resources/jqueryui/jquery-ui.js" type="text/javascript"></script>
+	    </head>
+	    <body>
+		<div id="historybox">
+		    <table class="ui-widget" id="historyhead">
+			<thead class="ui-widget-header">
+			    <tr>
+				<?php
+				$fields	 = array("c_cvst", "fh", "gestor", "short", "Gestion");
+				$fieldnames	 = array("Status", "Fecha/Hora", "Visitador", "Gestion", "Gestion");
+				$fieldsize	 = array("status", "timestamp", "chico", "gestion", "hidebox");
+				for ($j = 0; $j < 4; $j++) {
+					$fieldname = $fieldnames[$j];
 					?>
-					</td> 
+					<th<?php echo ' class="'.$fieldsize[$j].'"'; ?>><?php
+					    if (isset($fieldname)) {
+						    echo $fieldname;
+					    }
+					    ?></th> <?php
+		}
+		?></tr>
+			</thead>
+			<tbody class="ui-widget-content"><?php
+			    $j	 = 0;
+			    $c	 = 0;
+			    foreach ($rowsub as $answer) {
+				    $auto	 = $answer['auto'];
+				    $gestor	 = utf8_encode($answer['gestor']);
+				    $gestion = utf8_encode($answer['c_obse1']);
+				    ?>
+				    <tr>
 					<?php
-					} $c=1-$c;
-					?>
-                  </tr> 
-				  <?php 
-                  $j++;
-                }  
-              ?>
-			  </tbody>
-</table>
-</div>
-<?php }  ?>
-</div>
-<button onClick='window.close()'>CIERRA</button>
-<?php   
-}
-mysql_close($con);
-?>
+					for ($k = 0; $k < 4; $k++) {
+						$field = $fields[$k];
+						$anku = utf8_encode($answer[$field]);
+						if (is_null($anku)) {
+							$anku = "&nbsp;";
+						}
+						$ank	 = str_replace('00:00:00', '', $anku);
+						$jscode	 = '';
+						if ($field == "short") {
+							$jscode1 = " onClick='alert(";
+							$jscode2 = ")'";
+							$jscode	 = $jscode1.'"'.ereg_replace("[\n\r]", " ", $gestion).'"'.$jscode2;
+						}
+						?>
+						<td<?php
+						    if ($c == 1) {
+							    echo " style='background-color:#dddddd'";
+						    }
+						    echo ' class="'.$fieldsize[$k].'"'.$jscode;
+						    ?>>
+							<?php
+							if (isset($ank)) {
+								echo $ank;
+							}
+							?>
+						</td>
+					    <?php
+				    } $c = 1 - $c;
+				    ?>
+				    </tr>
+		<?php
+		$j++;
+	}
+	?>
+			</tbody>
+		    </table>
+		</div>
+<?php } ?>
+    </div>
+    <button onClick='window.close()'>CIERRA</button>
 </body>
 </html> 
-
 
--- reports.php
+++ reports.php
@@ -1,60 +1,50 @@
 <?php
 require_once 'pdoConnect.php';
 $pdoc = new pdoConnect();
-$pdo  = $pdoc->dbConnectAdmin();
+$pdo = $pdoc->dbConnectAdmin();
 $capt = filter_input(INPUT_GET, 'capt');
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Cobra Reports Menu</title>
-        <link rel="Stylesheet" href="bower_components/jqueryui/themes/redmond/jquery-ui.css" />
-        <script type="text/javascript" charset="utf8" src="bower_components/jquery/dist/jquery.js"></script>
-        <script type="text/javascript" charset="utf8" src="bower_components/jqueryui/jquery-ui.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cobra Administraci&oacute;n</title>
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     </head>
-    <body id="demos">
-        <script>
-	    $(function () {
-		    $("#tabs").tabs();
-		    $("button").button();
-	    });
-        </script>
-        <div id="tabs">
-            <ul>
-                <li><a href="#admin">Administraci&oacute;n</a></li>
-                <li><a href="#gen">Reportes Generales</a></li>
-                <li><a href="#spec">Reportes por Clientes</a></li>
-                <li><a href="#bot">ROBOT y ELASTIX</a></li>
-            </ul>
-            <div id="admin">
-                <h2>Cargas</h2>
+    <body>
+        <div data-role="page" id='Administracion'>
+            <div data-role="header">
+                <div data-role="navbar">
+                    <UL>
+                        <LI><A href="#Administracion" class="ui-btn-active">Administraci&oacute;n</A></LI>
+                        <LI><A href="#Generales">Reportes Generales</A></LI>
+                    </UL>
+                </div>
+            </div>
+            <div data-role="collapsible">
+                <h2>Cargar</h2>
                 <button onclick="window.location = 'carga2.php?capt=<?php echo $capt; ?>'">Cargar Cartera</button>
                 <button onclick="window.location = 'pagobulk.php?capt=<?php echo $capt; ?>'">Cargar Pagos Confirmados</button>
-                <button onclick="window.location = 'cargaPic.php?capt=<?php echo $capt; ?>'">Cargar Foto (FAMSA)</button><br>
-                <h2>Visitas</h2>
+            </div>
+            <div data-role="collapsible">
+                <h2>Visitar</h2>
                 <button onclick="window.location = 'checkout.php?capt=<?php echo $capt; ?>'">Asignar Visitas</button>
                 <button onclick="window.location = 'checkin.php?capt=<?php echo $capt; ?>'">Recibir Visitas</button>
-                <button onclick="window.location = 'checkoutlist.php?capt=<?php echo $capt; ?>'">Check List</button><br>
-                <h2>Administraci&oacute;n</h2>
+            </div>
+	    <div data-role="collapsible">
+		<h2>Administrar</h2>
                 <button onclick="window.location = 'gestoradmin.php?capt=<?php echo $capt; ?>'">Administrar Acesso</button>
                 <button onclick="window.location = 'breakadmin.php?capt=<?php echo $capt; ?>'">Administrar Breaks</button>
                 <button onclick="window.location = 'queues.php?capt=<?php echo $capt; ?>'">Administrar Queues</button>
                 <button onclick="window.location = 'notadmin.php?capt=<?php echo $capt; ?>'">Administrar Notas</button>
-                <button onclick="window.location = 'segmentadmin.php?capt=<?php echo $capt; ?>'">Administrar Segmetos</button><br>
-                <button onclick="window.location = 'changest.php?capt=<?php echo $capt; ?>'">
-                    Cambiar Status de Credito</button>
-                <!--
-                <button onclick="window.location='changegest.php?capt=<?php echo $capt; ?>'">
-                Cambiar Gestor Asignado</button>
-                -->
-                <button onclick="window.location = 'inactivar.php?capt=<?php echo $capt; ?>'">Inactivar Cuentas</button><br>
-                <button onclick="window.location = 'activar.php?capt=<?php echo $capt; ?>'">Activar Cuentas</button><br>
+		<button onclick="window.location = 'segmentadmin.php?capt=<?php echo $capt; ?>'">Administrar Segmetos</button>
+		<button onclick="window.location = 'changest.php?capt=<?php echo $capt; ?>'">Cambiar Status de Credito</button>
+		<button onclick="window.location = 'inactivar.php?capt=<?php echo $capt; ?>'">Inactivar Cuentas</button>
             </div>
-            <div id="gen">
-                <h2>Reportes Generales</h2>
-                <!--<button onclick="window.location='queuesqcg.php?capt=<?php echo $capt; ?>'">Queues por Gestor</button>-->
+            <div data-role="page" id="Generales">
                 <button onclick="window.location = 'queuesqc.php?capt=<?php echo $capt; ?>'">Queues por Cliente</button>
-                <!--<button onclick="window.location='queueReview.php?capt=<?php echo $capt; ?>'">Reporte de Queues</button><br>-->
                 <button onclick="window.location = 'latest_best.php?capt=<?php echo $capt; ?>'">Ultimo y Mejor Status</button>
                 <button onclick="window.location = 'rotas.php?capt=<?php echo $capt; ?>'">Promesas del Mes Actual</button>
                 <button onclick="window.location = 'pagosum.php?capt=<?php echo $capt; ?>'">Pagos por Cliente</button>
@@ -72,22 +62,11 @@ $capt = filter_input(INPUT_GET, 'capt');
                 <button onclick="window.location = 'tels_contactados.php?capt=<?php echo $capt; ?>'">Reporte de Tel&eacute;fonos Contactados XLS</button>
                 <button onclick="window.location = 'tels_marcados.php?capt=<?php echo $capt; ?>'">Reporte de Tel&eacute;fonos Marcados XLS</button>
             </div>
-            <div id="spec">
-                <h2>Reportes Specializados</h2>
-                <button onclick="window.location = 'folios_new.php?capt=<?php echo $capt; ?>'">Carga Folios</button><br>
-                <br>
-                <h3>Credito Real</h3>
-                <button onclick="window.location = 'CreditoRealReport.php'">Reporte Lunes, Miercoles, Viernes para Credito Real</button>
-                <button onclick="window.location = 'CreditoRealFdm.php'">Reporte Mensual</button>
-                <br>
-            </div>
-            <div id="bot">
-            </div>
         </div>
-        <p>
+        <div data-role="footer">
             <button onclick="window.location = 'quick.php?capt=<?php echo $capt; ?>'">Tiempo Real</button>
             <button onclick="window.location = 'resumen.php?capt=<?php echo $capt; ?>'">Las Cuentas</button>
             <button onclick="window.location = 'resumen.php?capt=<?php echo $capt; ?>&go=LOGOUT'">LOGOUT</button><br>
-        </p>
+        </div>
     </body>
 </html>
