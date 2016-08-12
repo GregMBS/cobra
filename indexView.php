ed423a3e72a82f4fec1fc1afe2eec023292908d9
gmbs <gmbs@server>
2016-08-12 15:01:12 -0500

    split logout.php

---
+++ indexView.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CobraMas</title>
+        <?php
+        $msg = 'CobraMas';
+        ?>
+        <style type="text/css">
+            body {text-align: center; background-color: #ffffff; width: 50em; color:#000000;}
+            div.forma {margin-left:5.5cm; font-weight: bold;}
+            div.logo {text-align:center; font-weight: bold;}
+            fieldset {width: 21em; background-color: #c0c0c0;}
+        </style>
+    </head>
+    <body>
+        <h1><?php echo $msg; ?></h1>
+        <em>versi&oacute;n configurado para Servicios Automáticos en Linea SA</em>
+        <div class="forma">
+            <form action="index.php" method="post" autocomplete="off">
+                <fieldset>
+                    <div class="username">
+                        <span class="formcap">Usuario:</span><input type="password" name="capt" value="" onchange="this.value = this.value.replace(/ /g, '');" /><br>
+                    </div>
+                    <div class="contrasena">
+                        <span class="formcap">Contrase&ntilde;a:</span><input type="password" name="pwd" value=""/><br>
+                    </div>
+                    <input type="submit" name="go" value="Empezar" />
+                </fieldset>
+            </form>
+        </div>
+        <p>&nbsp;</p>
+        <a href="licencia.txt">
+            <cite>&#169;Gregory Miles Blumenthal Scharf 2016</cite>
+        </a>
+    </body>
+</html>
--- logout.php
+++ logout.php
@@ -60,28 +60,4 @@ if ($go != "") {
 	}
 	header($page);
 }
-?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-	<title>LOGOUT de CobraMas</title>
-	<link href="public/bower_resources/jqueryui/themes/redmond/jquery-ui.css" rel="stylesheet" type="text/css"/>
-        <script src="public/bower_resources/jquery/dist/jquery.js" type="text/javascript"></script>
-        <script src="public/bower_resources/jqueryui/jquery-ui.js" type="text/javascript"></script>
-    </head>
-    <body>
-	<h1>CobraMas - LOGOUT</h1>
-	<div class="forma">
-	    <form action='logout.php' method='get'>
-		<input type='hidden' name='capt' value='<?php echo $capt ?>'>
-		<button name='gone' value='Bano'>BA&Ntilde;O</button><br>
-		<button name='gone' value='Junta'>JUNTA</button><br>
-		<button name='gone' value='Break'>BREAK</button><br>
-		<button name='gone' value='Salir'>SALIR</button><br>
-	    </form>
-	</div>
-	<div class="logo">
-	</div>
-    </body>
-</html>
+include 'logoutView.php';
