e7d162cf46043d8150cbd8cd384a1d46dd5568bb
gmbs <gmbs@server>
2016-08-20 13:20:49 -0500

    carga is failing

--- carga2.php
+++ carga2.php
@@ -33,7 +33,7 @@ if ($go == 'cargar') {
         echo "<p>Preparada para cargar datos.</p>";
         $cc->loadData($pdo, $filename, $dataNames);
         echo "<p>Datos cargados.</p>";
-        $fieldlist = $cc->prepareUpdate($columnNames);
+        $fieldlist = $cc->prepareUpdate($dataNames);
         var_dump($fieldlist);
         die();
         $cc->updateResumen($pdo, $fieldlist);
