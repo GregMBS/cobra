74d8cd75f48e39173aa7de56ac96f5223f31e617
gmbs <gmbs@server>
2016-08-20 17:14:53 -0500

    https

--- views/resumen-mobileView.php
+++ views/resumen-mobileView.php
@@ -4,9 +4,9 @@
 
         <title>Resumen Mob&iacute;l</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" type="text/css" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+        <link rel="stylesheet" type="text/css" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
         <script type="text/JavaScript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-        <script type="text/JavaScript" src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+        <script type="text/JavaScript" src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
         <SCRIPT TYPE="text/JavaScript">
             function npromChange(thisform)
             {
