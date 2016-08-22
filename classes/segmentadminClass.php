350c665634cd48e61062b823bda55ac0fe0b51ba
gmbs <gmbs@server>
2016-08-22 15:00:59 -0500

    classify segmentadmin

---
+++ classes/segmentadminClass.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * Description of segmentadminClass
+ *
+ * @author gmbs
+ */
+class segmentadminClass {
+
+    /**
+     *
+     * @var PDO
+     */
+    private $pdo;
+
+    /**
+     * 
+     * @param PDO $pdo
+     */
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * 
+     * @param string $cliente
+     * @param string $segmento
+     */
+    function borrarSegmento(string $cliente, string $segmento) {
+        $queryborrar = "DELETE FROM queuelist
+            WHERE cliente=:cliente
+            AND sdc=:segmento;";
+        $stb = $this->pdo->prepare($queryborrar);
+        $stb->bindParam(':cliente', $cliente);
+        $stb->bindParam(':segmento', $segmento);
+        $stb->execute();
+    }
+
+    /**
+     * 
+     * @param string $cliente
+     * @param string $segmento
+     */
+    function agregarSegmento(string $cliente, string $segmento) {
+        $querylistin = "INSERT IGNORE INTO queuelist
+            (gestor, cliente, status_aarsa, updown1, orden1, camp, sdc,
+            bloqueado)
+            SELECT distinct gestor, :cliente, status_aarsa, updown1,
+            orden1, 9999999, :segmento, 0
+            FROM queuelist;";
+        $stl = $this->pdo->prepare($querylistin);
+        $stl->bindParam(':cliente', $cliente);
+        $stl->bindParam(':segmento', $segmento);
+        $stl->execute();
+        $querylistcamp = "update queuelist
+            set camp=auto where camp=9999999;";
+        $this->pdo->query($querylistcamp);
+    }
+
+    /**
+     * 
+     */
+    function addAllSegmentos() {
+        $querycliseg = "SELECT DISTINCT cliente, status_de_credito "
+                . "FROM resumen "
+                . "WHERE status_de_credito NOT REGEXP '-'";
+        $result = $$this->pdo->query($querycliseg);
+        $querylistin = "INSERT IGNORE INTO queuelist
+            (gestor, cliente, status_aarsa, updown1, orden1, camp, sdc,
+            bloqueado)
+            SELECT distinct gestor, :cliente, status_aarsa, updown1,
+            orden1, 9999999, :segmento, 0
+            FROM queuelist";
+        $stl = $$this->pdo->prepare($querylistin);
+        foreach ($result as $row) {
+            $stl->bindParam(':cliente', $row['cliente']);
+            $stl->bindParam(':segmento', $row['status_de_credito']);
+            $stl->execute();
+        }
+        $querylistcamp = "update queuelist
+            set camp=auto where camp=9999999;";
+        $$this->pdo->query($querylistcamp);
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    function listQueuedSegmentos() {
+        $querytempr = "create temporary table csdcr
+select cliente, status_de_credito, count(id_cuenta) as cnt from resumen
+where status_de_credito not regexp '-'
+group by cliente, status_de_credito";
+        $this->pdo->query($querytempr);
+        $querymain = "SELECT q.cliente as 'cliente', sdc, cnt
+    FROM queuelist q
+    LEFT JOIN csdcr r
+    ON q.cliente=r.cliente and sdc=status_de_credito
+    WHERE sdc<>'' and q.status_aarsa='sin gestion'
+    group by q.cliente,sdc
+    ";
+        $result = $this->pdo->query($querymain);
+        return $result;
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    function listUnqueuedSegments() {
+        $querytemp = "create temporary table csdc
+select distinct cliente,sdc from queuelist";
+        $this->pdo->query($querytemp);
+        $querymain2 = "SELECT r.cliente as 'cliente',status_de_credito as 'sdc',
+    count(1)
+    FROM resumen r
+    LEFT JOIN csdc q
+    ON q.cliente=r.cliente and sdc=status_de_credito
+    WHERE sdc is null
+    AND r.cliente <> ''
+    AND status_de_credito not regexp '-'
+    group by r.cliente,status_de_credito
+    ";
+        $result = $this->pdo->query($querymain2);
+        return $result;
+    }
+
+}
--- segmentadmin.php
+++ segmentadmin.php
@@ -1,20 +1,16 @@
 <?php
 require_once 'classes/pdoConnect.php';
+require_once 'classes/segmentadminClass.php';
 $pc   = new pdoConnect();
 $pdo  = $pc->dbConnectAdmin();
+$sc   = new segmentadminClass($pdo);
 $capt = filter_input(INPUT_GET, 'capt');
 $go   = filter_input(INPUT_GET, 'go');
 if (!empty($go)) {
     if ($go == "BORRAR") {
         $cliente     = filter_input(INPUT_GET, 'cliente');
         $segmento    = filter_input(INPUT_GET, 'segmento');
-        $queryborrar = "DELETE FROM queuelist
-            WHERE cliente=:cliente
-            AND sdc=:segmento;";
-        $stb         = $pdo->prepare($queryborrar);
-        $stb->bindParam(':cliente', $cliente);
-        $stb->bindParam(':segmento', $segmento);
-        $stb->execute();
+        $sc->borrarSegmento($cliente, $segmento);
     }
 
     if ($go == "AGREGAR") {
@@ -22,69 +18,14 @@ if (!empty($go)) {
         $clientesegmento = explode(';', $cliseg);
         $cliente         = $clientesegmento[0];
         $segmento        = $clientesegmento[1];
-        $querylistin     = "INSERT IGNORE INTO queuelist
-            (gestor, cliente, status_aarsa, updown1, orden1, camp, sdc,
-            bloqueado)
-            SELECT distinct gestor, :cliente, status_aarsa, updown1,
-            orden1, 9999999, :segmento, 0
-            FROM queuelist;";
-        $stl             = $pdo->prepare($querylistin);
-        $stl->bindParam(':cliente', $cliente);
-        $stl->bindParam(':segmento', $segmento);
-        $stl->execute();
-        $querylistcamp   = "update queuelist
-            set camp=auto where camp=9999999;";
-        $pdo->query($querylistcamp);
+        $sc->agregarSegmento($cliente, $segmento);
         header("Location: segmentadmin.php?capt=".$capt);
     }
     if ($go == "AGREGARALL") {
-        $querycliseg = "SELECT DISTINCT cliente, status_de_credito "
-            ."FROM resumen "
-            ."WHERE status_de_credito NOT REGEXP '-'";
-        $result      = $pdo->query($querycliseg);
-        $querylistin = "INSERT IGNORE INTO queuelist
-            (gestor, cliente, status_aarsa, updown1, orden1, camp, sdc,
-            bloqueado)
-            SELECT distinct gestor, :cliente, status_aarsa, updown1,
-            orden1, 9999999, :segmento, 0
-            FROM queuelist";
-        $stl         = $pdo->prepare($querylistin);
-        foreach ($result as $row) {
-            $stl->bindParam(':cliente', $row['cliente']);
-            $stl->bindParam(':segmento', $row['status_de_credito']);
-            $stl->execute();
-        }
-        $querylistcamp = "update queuelist
-            set camp=auto where camp=9999999;";
-        $pdo->query($querylistcamp);
+        $sc->addAllSegmentos();
         header("Location: segmentadmin.php?capt=".$capt);
     }
 }
-$querytempr = "create temporary table csdcr
-select cliente, status_de_credito, count(id_cuenta) as cnt from resumen
-where status_de_credito not regexp '-'
-group by cliente, status_de_credito";
-$pdo->query($querytempr);
-$querymain  = "SELECT q.cliente as 'cliente', sdc, cnt
-    FROM queuelist q
-    LEFT JOIN csdcr r
-    ON q.cliente=r.cliente and sdc=status_de_credito
-    WHERE sdc<>'' and q.status_aarsa='sin gestion'
-    group by q.cliente,sdc
-    ";
-$result     = $pdo->query($querymain);
-$querytemp  = "create temporary table csdc
-select distinct cliente,sdc from queuelist";
-$pdo->query($querytemp);
-$querymain2 = "SELECT r.cliente as 'cliente',status_de_credito as 'sdc',
-    count(1)
-    FROM resumen r
-    LEFT JOIN csdc q
-    ON q.cliente=r.cliente and sdc=status_de_credito
-    WHERE sdc is null
-    AND r.cliente <> ''
-    AND status_de_credito not regexp '-'
-    group by r.cliente,status_de_credito
-    ";
-$result2    = $pdo->query($querymain2);
+$result     = $sc->listQueuedSegmentos();
+$result2    = $sc->listUnqueuedSegments();
 require_once 'views/segmentadminView.php';
