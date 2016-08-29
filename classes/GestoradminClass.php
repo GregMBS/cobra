7653af5ab0445f314729ba16a5264c9c6e8c4611
gmbs <gmbs@server>
2016-08-29 14:30:40 -0500

    fix gestoradmin

---
+++ classes/GestoradminClass.php
@@ -0,0 +1,163 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace cobra_salsa;
+
+/**
+ * Description of GestoradminClass
+ *
+ * @author gmbs
+ */
+class GestoradminClass {
+
+    /**
+     *
+     * @var \PDO 
+     */
+    private $pdo;
+
+    /**
+     * 
+     * @param \PDO $pdo
+     */
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * 
+     * @param string $completo
+     * @param string $tipo
+     * @param string $usuaria
+     */
+    public function updateOpenParams($completo, $tipo, $usuaria) {
+        $queryu = "UPDATE nombres
+            SET completo = :completo,
+            tipo = :tipo
+            WHERE usuaria = :usuaria";
+        $stu = $this->pdo->prepare($queryu);
+        $stu->bindParam(':completo', $completo);
+        $stu->bindParam(':tipo', $tipo);
+        $stu->bindParam(':usuaria', $usuaria);
+        $stu->execute();
+    }
+
+    /**
+     * 
+     * @param string $passw
+     * @param string $usuaria
+     */
+    public function updatePassword($passw, $usuaria) {
+        $queryp = "UPDATE nombres
+            SET passw = sha(:passw)
+            WHERE usuaria = :usuaria
+	    AND passw <> :passw";
+        $stp = $this->pdo->prepare($queryp);
+        $stp->bindParam(':passw', $passw);
+        $stp->bindParam(':usuaria', $usuaria);
+        $stp->execute();
+    }
+
+    /**
+     * 
+     * @param string $usuaria
+     */
+    public function deleteFromNombres($usuaria) {
+        $queryb = "DELETE FROM nombres WHERE usuaria = :usuaria";
+        $stb = $this->pdo->prepare($queryb);
+        $stb->bindParam(':usuaria', $usuaria);
+        $stb->execute();
+    }
+
+    /**
+     * 
+     * @param string $usuaria
+     */
+    public function deleteFromQueuelist($usuaria) {
+        $queryb = "DELETE FROM queuelist WHERE gestor = :usuaria";
+        $stb = $this->pdo->prepare($queryb);
+        $stb->bindParam(':usuaria', $usuaria);
+        $stb->execute();
+    }
+
+    /**
+     * 
+     * @param string $usuaria
+     */
+    public function deleteFromResumen($usuaria) {
+        $queryb = "UPDATE resumen SET ejecutivo_asignado_call_center='sinasig'
+            WHERE ejecutivo_asignado_call_center = :usuaria";
+        $stb = $this->pdo->prepare($queryb);
+        $stb->bindParam(':usuaria', $usuaria);
+        $stb->execute();
+    }
+
+    /**
+     * 
+     * @param string $completo
+     * @param string $tipo
+     * @param string $usuaria
+     * @param string $iniciales
+     * @param string $passw
+     */
+    public function addToNombres($completo, $tipo, $usuaria, $iniciales, $passw) {
+        $queryin = "INSERT INTO nombres (USUARIA, INICIALES, COMPLETO, PASSW,
+            TIPO, CAMP) 
+	VALUES (:usuaria, :iniciales, :completo, sha(:passw), :tipo, 999999)";
+        $sti = $this->pdo->prepare($queryin);
+        $sti->bindParam(':completo', $completo);
+        $sti->bindParam(':tipo', $tipo);
+        $sti->bindParam(':usuaria', $usuaria);
+        $sti->bindParam(':iniciales', $iniciales);
+        $sti->bindParam(':passw', $passw);
+        $sti->execute();
+    }
+
+    /**
+     * 
+     * @param string $iniciales
+     */
+    public function addToQueuelists($iniciales) {
+        $querylistin = "insert ignore into queuelist
+		SELECT distinct null, :iniciales, cliente, status_aarsa, 999999,
+		orden1, updown1, orden2, updown2, orden3, updown3,
+		sdc, bloqueado
+		FROM queuelist;";
+        $stl = $this->pdo->prepare($querylistin);
+        $stl->bindParam(':iniciales', $iniciales);
+        $stl->execute();
+        $querylistcamp = "update queuelist
+            set camp=auto where camp=999999;";
+        $this->pdo->query($querylistcamp);
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    public function getNombres() {
+        $querymain = "SELECT USUARIA, COMPLETO, TIPO, CAMP, INICIALES, PASSW 
+    FROM nombres 
+    where iniciales <> 'gmbs'
+    order by TIPO, USUARIA";
+        $result = $this->pdo->query($querymain);
+        return $result;
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    public function getGroups() {
+        $queryg = "SELECT grupo FROM grupos";
+        $resultg = $this->pdo->query($queryg);
+        $groups = $resultg->fetchAll(PDO::FETCH_ASSOC);
+        return $groups;
+    }
+
+}
--- gestoradmin.php
+++ gestoradmin.php
@@ -1,10 +1,13 @@
 <?php
 
 use cobra_salsa\PdoClass;
+use cobra_salsa\GestoradminClass;
 
 require_once 'classes/PdoClass.php';
+require_once 'classes/GestoradminClass.php';
 $pc = new PdoClass();
 $pdo = $pc->dbConnectAdmin();
+$gc = new GestoradminClass($pdo);
 
 $go = filter_input(INPUT_GET, 'go');
 $completo = filter_input(INPUT_GET, 'completo');
@@ -15,76 +18,26 @@ $capt = filter_input(INPUT_GET, 'capt');
 
 if (!empty($go)) {
     if ($go == "GUARDAR") {
-        $queryu = "UPDATE nombres
-            SET completo = :completo
-            tipo = :tipo
-            WHERE usuaria = :usuaria";
-        $stu = $pdo->prepare($queryu);
-        $stu->bindParam(':completo', $completo);
-        $stu->bindParam(':tipo', $tipo);
-        $stu->bindParam(':usuaria', $usuaria);
-        $stu->execute();
-        $queryp = "UPDATE nombres
-            SET passw = sha(:passw)
-            WHERE usuaria = :usuaria
-	    AND passw <> :passw";
-        $stp = $pdo->prepare($queryp);
-        $stp->bindParam(':passw', $passw);
-        $stp->bindParam(':usuaria', $usuaria);
-        $stp->execute();
+        $gc->updateOpenParams($completo, $tipo, $usuaria);
+        $gc->updatePassword($passw, $usuaria);
         header("Location: gestoradmin.php?capt=" . $capt);
     }
 
     if ($go == "BORRAR") {
-        $queryb = "DELETE FROM nombres WHERE usuaria = :usuaria";
-        $stb = $pdo->prepare($queryb);
-        $stb->bindParam(':usuaria', $usuaria);
-        $stb->execute();
-        $queryb2 = "DELETE FROM queuelist WHERE gestor = :usuaria";
-        $stb2 = $pdo->prepare($queryb2);
-        $stb2->bindParam(':usuaria', $usuaria);
-        $stb2->execute();
-        $queryb3 = "UPDATE resumen SET ejecutivo_asignado_call_center='sinasig'
-            WHERE ejecutivo_asignado_call_center = :usuaria";
-        $stb3 = $pdo->prepare($queryb3);
-        $stb3->bindParam(':usuaria', $usuaria);
-        $stb3->execute();
+        $gc->deleteFromNombres($usuaria);
+        $gc->deleteFromQueuelist($usuaria);
+        $gc->deleteFromResumen($usuaria);
         header("Location: gestoradmin.php?capt=" . $capt);
     }
 
     if ($go == "AGREGAR") {
         $iniciales = strtolower($usuaria);
-        $queryin = "INSERT INTO nombres (USUARIA, INICIALES, COMPLETO, PASSW,
-            TIPO, CAMP) 
-	VALUES (:usuaria, :iniciales, :completo, sha(:passw), :tipo, 999999)";
-        $sti = $pdo->prepare($queryin);
-        $sti->bindParam(':completo', $completo);
-        $sti->bindParam(':tipo', $tipo);
-        $sti->bindParam(':usuaria', $usuaria);
-        $sti->bindParam(':iniciales', $iniciales);
-        $sti->bindParam(':passw', $passw);
-        $sti->execute();
-        $querylistin = "insert ignore into queuelist
-		SELECT distinct null, :iniciales, cliente, status_aarsa, 999999,
-		orden1, updown1, orden2, updown2, orden3, updown3,
-		sdc, bloqueado
-		FROM queuelist;";
-        $stl = $pdo->prepare($querylistin);
-        $stl->bindParam(':iniciales', $iniciales);
-        $stl->execute();
-        $querylistcamp = "update queuelist
-            set camp=auto where camp=999999;";
-        $resultlistcamp = $pdo->query($querylistcamp);
+        $gc->addToNombres($completo, $tipo, $usuaria, $iniciales, $passw);
+        $gc->addToQueuelists($iniciales);
         header("Location: gestoradmin.php?capt=" . $capt);
     }
 }
-$querymain = "SELECT USUARIA, COMPLETO, TIPO, CAMP, INICIALES, PASSW 
-    FROM nombres 
-    where iniciales <> 'gmbs'
-    order by TIPO, USUARIA";
-$result = $pdo->query($querymain);
+$result = $gc->getNombres();
 
-$queryg = "SELECT grupo FROM grupos";
-$resultg = $pdo->query($queryg) or die($pdo->error);
-$groups = $resultg->fetchAll(PDO::FETCH_ASSOC) or die($resultg->error);
+$groups = $gc->getGroups();
 require_once 'views/gestoradminView.php';
