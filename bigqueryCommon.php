8981087cbf627af483ec8cfe2374bad5aa125ee7
gmbs <gmbs@server>
2016-08-30 18:04:40 -0500

    bigQuery

--- bigqueryCommon.php
+++ bigqueryCommon.php
@@ -1,4 +1,5 @@
 <?php
+
 set_time_limit(300);
 require_once 'vendor/autoload.php';
 
@@ -8,12 +9,12 @@ use Box\Spout\Common\Type;
 
 require_once 'classes/PdoClass.php';
 $pdoc = new PdoClass();
-$pdo  = $pdoc->dbConnectAdmin();
+$pdo = $pdoc->dbConnectAdmin();
 $capt = filter_input(INPUT_GET, 'capt');
-$get  = filter_input_array(INPUT_GET);
+$get = filter_input_array(INPUT_GET);
 
 if (isset($get['fecha1'])) {
-    $go     = $get['go'];
+    $go = $get['go'];
     $gestor = $get['gestor'];
     $fecha1 = $get['fecha1'];
     $fecha2 = $get['fecha2'];
@@ -26,14 +27,12 @@ if (isset($get['fecha1'])) {
         list($fecha1, $fecha2) = array($fecha2, $fecha1);
     }
 //$gestorstr=" and ejecutivo_asignado_call_center not regexp '-' ";
-    $gestorstr  = '';
+    $gestorstr = '';
     $clientestr = '';
     if (isset($get['cliente'])) {
         $cliente_array = $get['cliente'];
-        $clientes      = filter_var_array($cliente_array,
-            FILTER_SANITIZE_ENCODED);
-        $clientestr    = " AND resumen.cliente IN ('".urldecode(implode("','",
-                    $clientes))."') ";
+        $clientes = filter_var_array($cliente_array, FILTER_SANITIZE_ENCODED);
+        $clientestr = " AND resumen.cliente IN ('" . urldecode(implode("','", $clientes)) . "') ";
     }
     if ($gestor != 'todos') {
         $gestorstr = " and c_cvge=:gestor ";
@@ -65,10 +64,10 @@ left join dictamenes d1 on status_aarsa=d1.dictamen
 left join dictamenes d2 on c_cvst=d2.dictamen
 left join pagos on c_cont=pagos.id_cuenta and d2.queue='PAGOS' and fecha between last_day(d_fech-interval 1 month) and d_fech
 where d_fech between :fecha1 and :fecha2
-".$gestorstr.$clientestr."
+" . $gestorstr . $clientestr . "
 ORDER BY d_fech,c_hrin
     ;";
-    $stm       = $pdo->prepare($querymain);
+    $stm = $pdo->prepare($querymain);
     $stm->bindParam(':fecha1', $fecha1);
     $stm->bindParam(':fecha2', $fecha2);
     if ($gestor != 'todos') {
@@ -76,4 +75,17 @@ ORDER BY d_fech,c_hrin
     }
     $stm->execute();
     $result = $stm->fetchAll(\PDO::FETCH_ASSOC);
+    $queryg = "SELECT distinct c_cvge FROM historia
+        where d_fech>last_day(curdate()-interval 2 month)
+        and c_cvge <> ''
+        order by c_cvge
+        limit 1000
+	";
+    $resultg = $pdo->query($queryg);
+    $queryc = "SELECT distinct c_cvba FROM historia
+        where d_fech>last_day(curdate()-interval 2 month)
+        order by c_cvba
+        limit 100
+	";
+    $resultc = $pdo->query($queryc);
 }
