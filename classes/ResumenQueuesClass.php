7f7a3ce4555f124e679f89cf388798f27c09eb03
gmbs <gmbs@server>
2016-08-30 11:33:26 -0500

    resumen Main Query done

---
+++ classes/ResumenQueuesClass.php
@@ -0,0 +1,184 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace cobra_salsa;
+
+/**
+ * Description of ResumenQueuesClass
+ *
+ * @author gmbs
+ */
+class ResumenQueuesClass {
+
+    /**
+     * @var \PDO $pdo
+     */
+    protected $pdo;
+
+    /**
+     * 
+     * @param \PDO $pdo
+     */
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * 
+     * @param string $capt
+     * @param int $camp
+     * @return array
+     */
+    public function getMyQueue($capt, $camp) {
+        $queryquery = "SELECT cliente, status_aarsa as cr, sdc 
+        FROM queuelist 
+        WHERE gestor = :capt 
+        AND camp= :camp 
+        LIMIT 1";
+        $stq = $this->pdo->prepare($queryquery);
+        $stq->bindParam(':capt', $capt);
+        $stq->bindParam(':camp', $camp, \PDO::PARAM_INT);
+        $stq->execute();
+        $result = $stq->fetch(\PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    /**
+     * 
+     * @param string $field
+     * @param string $find
+     * @return int
+     */
+    public function searchCount($field, $find) {
+        $querycount = "SELECT count(1) as ct 
+            FROM resumen 
+            WHERE " . $field . " = :find 
+            LIMIT 1";
+        $stc = $this->pdo->prepare($querycount);
+        $stc->bindParam(':find', $find);
+        $stc->execute();
+        $result = $stc->fetch(\PDO::FETCH_ASSOC);
+        return $result['ct'];
+    }
+
+    /**
+     * 
+     * @param string $cliente
+     * @param string $sdc
+     * @param string $cr
+     * @return \PDOStatement
+     */
+    public function prepareResumenMain($cliente, $sdc, $cr) {
+        if (empty($cliente)) {
+            $clientStr = '';
+        } else {
+            $clientStr = " AND cliente = :cliente ";
+        }
+        if (empty($sdc)) {
+            $sdcStr = " AND status_de_credito not regexp '-' ";
+        } else {
+            $sdcStr = " AND status_de_credito = :sdc ";
+        }
+        if (empty($cr)) {
+            $crStr = " AND status_aarsa not in ('PAGO TOTAL','PAGO PARCIAL','PAGANDO CONVENIO', 'ACLARACION') ";
+        } else {
+            $crStr = " AND queue = :cr ";
+        }
+
+        $querymain = "SELECT * FROM resumen 
+join dictamenes on dictamen=status_aarsa 
+WHERE locker is null
+$clientStr
+$sdcStr
+$crStr
+ORDER BY fecha_ultima_gestion LIMIT 1";
+
+        if ($cr == 'SIN GESTION') {
+            $querymain = "SELECT * FROM resumen 
+WHERE locker is null
+ $clientStr
+     $sdcStr
+ AND ((status_aarsa='') or (status_aarsa is null)))
+ ORDER BY saldo_total desc LIMIT 1";
+        }
+
+        if (($cr == 'INICIAL')) {
+            $querymain = "SELECT * FROM resumen
+WHERE status_de_credito not regexp '-' 
+AND status_aarsa not in ('PAGO TOTAL','PAGO PARCIAL','PAGANDO CONVENIO', 'ACLARACION')
+AND ejecutivo_asignado_call_center = :capt
+AND locker is null 
+and fecha_ultima_gestion < curdate()
+order by fecha_ultima_gestion  LIMIT 1";
+        }
+        if ($cr == 'ESPECIAL') {
+            $querymain = "SELECT * FROM resumen
+WHERE locker is null
+ $clientStr
+     $sdcStr
+AND fecha_ultima_gestion<last_day(curdate()-interval 1 month)+interval 1 day
+order by fecha_ultima_gestion  LIMIT 1
+";
+        }
+        $stm = $this->pdo->prepare($querymain);
+        return $stm;
+    }
+
+    /**
+     * 
+     * @param \PDOStatement $stm
+     * @param string $capt
+     * @param string $cliente
+     * @param string $sdc
+     * @param string $cr
+     * @return \PDOStatement
+     */
+    public function bindResumenMain($stm, $capt, $cliente, $sdc, $cr) {
+        if (($cr == 'INICIAL')) {
+            $stm->bindParam(':capt', $capt);
+            return $stm;
+        }
+        if (!empty($cliente)) {
+            $stm->bindParam(':cliente', $cliente);
+        }
+        if (!empty($sdc)) {
+            $stm->bindParam(':sdc', $sdc);
+        }
+        if (in_array($cr, array('ESPECIAL', 'SIN GESTION'))) {
+            return $stm;
+        }
+        if (empty($cr)) {
+            return $stm;
+        }
+        $stm->bindParam(':cr', $cr);
+        return $stm;
+    }
+
+    /**
+     * 
+     * @param int $id_cuenta
+     * @return \PDOStatement
+     */
+    public function prepareQuicksearch($id_cuenta) {
+        $querymain = "SELECT * FROM resumen WHERE id_cuenta = :id_cuenta LIMIT 1";
+        $stm = $this->pdo->prepare($querymain);
+        $stm->bindParam(':id_cuenta', $id_cuenta);
+        return $stm;
+    }
+
+    /**
+     * 
+     * @param \PDOStatement $stm
+     * @return array
+     */
+    public function runResumenMain($stm) {
+       $stm->execute();
+       $result = $stm->fetch(\PDO::FETCH_ASSOC);
+       return $result;
+    }
+}
--- resumen.php
+++ resumen.php
@@ -17,6 +17,7 @@
 use cobra_salsa\PdoClass;
 use cobra_salsa\ResumenClass;
 use cobra_salsa\GestionClass;
+use cobra_salsa\ResumenQueuesClass;
 
 $get = filter_input_array(INPUT_GET);
 date_default_timezone_set('America/Monterrey');
@@ -25,10 +26,12 @@ setlocale(LC_MONETARY, 'en_US');
 require_once 'classes/PdoClass.php';
 require_once 'classes/ResumenClass.php';
 require_once 'classes/GestionClass.php';
+require_once 'classes/ResumenQueuesClass.php';
 $pdoc = new PdoClass();
 $pdo = $pdoc->dbConnectUser();
 $rc = new ResumenClass($pdo);
 $gc = new GestionClass($pdo);
+$qc = new ResumenQueuesClass($pdo);
 $mytipo = $pdoc->tipo;
 
 /*
@@ -122,7 +125,6 @@ if (empty($mytipo)) {
         }
     }
 
-
     if ($go == 'NUEVOS') {
         if (!empty($C_NTEL)) {
             $gc->addNewTel($C_CONT, $C_NTEL);
@@ -136,6 +138,7 @@ if (empty($mytipo)) {
         $redirector = "Location: resumen.php?&capt=" . $capt;
         header($redirector);
     }
+
     if ($go == 'GUARDAR' && !empty($C_CVST)) {
         $oldgo = filter_input(INPUT_GET, 'oldgo');
         $flag = filter_input(INPUT_GET, 'error');
@@ -198,7 +201,7 @@ if (empty($mytipo)) {
 
     if (empty($capt)) {
         $redirector = "Location: index.php";
-        header($redirector);        
+        header($redirector);
     }
     $resultg = $rc->getUserData($capt);
     if (isset($resultg['tipo'])) {
@@ -207,49 +210,11 @@ if (empty($mytipo)) {
         $camp = $resultg['camp'];
     } else {
         $redirector = "Location: index.php";
-        header($redirector);        
+        header($redirector);
     }
 
-    $id_cuenta = 0;
-    $lockflag = 0;
-    $sdc = '';
-    $cr = '';
-    $queryquery = "SELECT cliente, status_aarsa, camp, 
-orden1, updown1, orden2, updown2, orden3, updown3, sdc FROM queuelist 
-WHERE gestor='" . $capt . "' AND camp='" . $camp . "'";
-    $resultquery = mysqli_query($con, $queryquery) or die("ERROR RM38 - " . mysqli_error($con));
-    while ($answerquery = mysqli_fetch_row($resultquery)) {
-        $cliente = $answerquery[0];
-        $sdc = $answerquery[9];
-        $CR = $answerquery[1];
-        $cr = $answerquery[1];
-        $order1 = $answerquery[3];
-        $updown1 = '';
-        if ($answerquery[4] == 1) {
-            $updown1 = ' desc';
-        }
-        $order2 = $answerquery[5];
-        $updown2 = '';
-        if ($answerquery[6] == 1) {
-            $updown2 = ' desc';
-        }
-        $sep12 = '';
-        $lockflag = 0;
-
-        if ($order2 != '') {
-            $sep12 = ',';
-        }
-        $order3 = $answerquery[7];
-        $updown3 = '';
-        if ($answerquery[8] == 1) {
-            $updown3 = ' desc';
-        }
-        if (($order3 != '') && ($order1 . $order2 != '')) {
-            $sep23 = ',';
-        } else {
-            $sep23 = '';
-        }
-    }
+    $resultquery = $qc->getMyQueue($capt, $camp);
+    extract($resultquery);
     if (isset($cr)) {
         $codres = ' AND queue="' . $cr . '" ';
     } else {
@@ -259,239 +224,48 @@ WHERE gestor='" . $capt . "' AND camp='" . $camp . "'";
     if ($cr == '') {
         $camp = 0;
     }
-    if ($camp > 0) {
-        $querymain = "SELECT * FROM resumen 
-left join dictamenes on status_aarsa=dictamen
-WHERE status_de_credito = '" . $sdc . "'
- AND locker is null
- ORDER BY fecha_ultima_gestion, vcc(status_aarsa), saldo_total desc LIMIT 1";
-        if ($cr <> '') {
-            $querymain = "SELECT * FROM resumen 
-join dictamenes on dictamen=status_aarsa 
-WHERE status_de_credito  = '" . $sdc . "' 
- AND locker is null
- AND cliente='" . $cliente . "'" . $codres .
-                    "
- ORDER BY " . $order1 . $updown1 . $sep12 . $order2 . $updown2 . $sep23 . $order3 . $updown3 . " LIMIT 1";
-        }
-        if ($cr == 'SIN GESTION') {
-            $querymain = "SELECT * FROM resumen 
-WHERE (status_de_credito  = '" . $sdc . "' 
- AND locker is null
- AND status_de_credito not regexp '[dv]o$'
- AND cliente='" . $cliente . "' 
- AND ((status_aarsa='') or (status_aarsa is null)))
- ORDER BY saldo_total desc LIMIT 1";
-        }
-        if ($cr == 'TOPS') {
-            $querymain = "select * from (select * from resumen 
-where cliente='" . $cliente . "' 
-and status_de_credito  = '" . $sdc . "'
-and fecha_de_actualizacion > last_day(curdate() - interval 6 week)
-order by saldo_total desc limit 15) as tmp 
-order by tmp.fecha_ultima_gestion limit 1";
-        }
 
-        if (($cr == 'INICIAL')) {
-            $querymain = "SELECT * FROM resumen
-WHERE status_de_credito not regexp '[dv]o$' 
-AND status_aarsa not in ('PAGO TOTAL','PAGO PARCIAL','PAGANDO CONVENIO')
-AND ejecutivo_asignado_call_center='" . $capt . "'
-AND locker is null 
-and fecha_ultima_gestion < curdate()
-order by fecha_ultima_gestion  LIMIT 1
-";
+
+
+    if (in_array($go, array('FROMBUSCAR', 'FROMMIGO', 'FROMULTIMA', 'FROMPROM'))) {
+        $stm = $qc->prepareQuicksearch($find);
+    } else {
+        if ($camp > 0) {
+            $stp = $qc->prepareResumenMain($cliente, $sdc, $cr);
+            $stm = $qc->bindResumenMain($stp, $capt, $cliente, $sdc, $cr);
         }
-        if (($cr == 'ESPECIAL')) {
-            $querymain = "SELECT * FROM resumen
-WHERE status_de_credito = '" . $sdc . "' 
-AND cliente='" . $cliente . "'
- AND locker is null
-AND fecha_ultima_gestion<last_day(curdate()-interval 1 month)+interval 1 day
-order by fecha_ultima_gestion  LIMIT 1
-";
-            if ($sdc == '') {
-                $querymain = "SELECT * FROM resumen
-WHERE cliente='" . $cliente . "'
- AND locker is null
-AND fecha_ultima_gestion<curdate()
-order by fecha_ultima_gestion  LIMIT 1
-";
-            }
+    }
+
+    $row = $qc->runResumenMain($stm);
+    if ($row) {
+        extract($row);
+        if (empty($status_de_credito)) {
+            $status_de_credito = '';
+        }
+        if (empty($saldo_cuota)) {
+            $saldo_cuota = 0;
         }
     } else {
-        $clientestr = '';
-        if (!empty($get['clientefilt'])) {
-            $clientefilter = filter_input(INPUT_GET, 'clientefilt');
-            $clientefilt = mysqli_real_escape_string($con, $clientefilter);
-            if (strlen($clientefilt) > 1) {
-                $clientestr = "AND cliente='" . $clientefilt . "' ";
-            }
-        }
-        $gestorstr = "";
-        //if (($mytipo=='supervisor'||$mytipo=='admin')&&(substr($CR,0,4)!='SELF')) {$gestorstr='';}
-        $querymain = "SELECT * FROM resumen 
-WHERE status_de_credito  = '" . $sdc . "' 
- AND locker is null
- " . $clientestr . " 
-ORDER BY fecha_ultima_gestion,saldo_total desc LIMIT 1";
+        unset($id_cuenta);
     }
-    if (($go == 'FROMBUSCAR') || ($go == 'FROMMIGO') || ($go == 'FROMULTIMA') || ($go == 'FROMPROM')) {
-        $querymain = "SELECT * FROM resumen WHERE id_cuenta = '" . $find . "' LIMIT 1";
-    }
-    $qcount = 0;
-    if ($go == 'QUICKSEARCH' || $go == 'FROMALERT') {
-        $querycount = "SELECT count(1) FROM resumen 
-    WHERE " . $field . " = '" . $find . "';";
-        //if ($capt=='moises') {die(htmlentities($querymain));} 
-        $resultcount = mysqli_query($con, $querycount) or die("ERROR RM39 - " . mysqli_error($con));
-        while ($answercount = mysqli_fetch_row($resultcount)) {
-            $qcount = $answercount[0];
-        }
-        $querymain = "SELECT * FROM resumen 
-    WHERE " . $field . " = '" . $find . "' order by " . $field . " 
-    LIMIT 1";
-    }
-//if ($capt=='gmbs') {die(htmlentities($querymain));}
-    $row = array_fill(0, 200, '');
-    $result = mysqli_query($con, $querymain) or die("ERROR RM40 - " . mysqli_error($con) . htmlentities($querymain));
-    if ($result) {
-        $row = mysqli_fetch_row($result);
-    }
-    $nombre_deudor = $row[0];
-    $domicilio_deudor = $row[1];
-    $colonia_deudor = $row[2];
-    $ciudad_deudor = $row[3];
-    $estado_deudor = $row[4];
-    $cp_deudor = $row[5];
-    $plano_guia_roji = $row[6];
-    $cuadrante_guia_roji = $row[7];
-    $tel_1 = $row[8];
-    $tel_2 = $row[9];
-    $tel_3 = $row[10];
-    $tel_4 = $row[11];
-    $nombre_deudor_alterno = $row[12];
 
-    $domicilio_deudor_alterno = $row[13];
-    $colonia_deudor_alterno = $row[14];
-    $ciudad_deudor_alterno = $row[15];
-    $estado_deudor_alterno = $row[16];
-    $cp_deudor_aterno = $row[17];
-    $tel_1_alterno = $row[18];
-    $tel_2_alterno = $row[19];
-    $tel_3_alterno = $row[20];
-    $tel_4_alterno = $row[21];
-    $plazo = $row[22];
-    $dia_corte = $row[23];
-    $status_aarsa = $row[24];
-    $avapar = $row[25];
-    $referencias_1 = $row[26];
-    $nombre_referencia_1 = $row[27];
-    $domicilio_referencia_1 = $row[28];
-    $colonia_referencia_1 = $row[29];
-    $ciudad_referencia_1 = $row[30];
-    $estado_referencia_1 = $row[31];
-    $cp_referencia_1 = $row[32];
-    $tel_1_ref_1 = $row[33];
-    $tel_2_ref_1 = $row[34];
-    $referencias_2 = $row[35];
-    $nombre_referencia_2 = $row[36];
-    $domicilio_referencia_2 = $row[37];
-    $colonia_referencia_2 = $row[38];
-    $ciudad_referencia_2 = $row[39];
-    $estado_referencia_2 = $row[40];
-    $cp_referencia_2 = $row[41];
-    $tel_1_ref_2 = $row[42];
-    $tel_2_ref_2 = $row[43];
-    $referencias_3 = $row[44];
-    $nombre_referencia_3 = $row[45];
-    $domicilio_referencia_3 = $row[46];
-    $colonia_referencia_3 = $row[47];
-    $ciudad_referencia_3 = $row[48];
-    $estado_referencia_3 = $row[49];
-    $cp_referencia_3 = $row[50];
-    $tel_1_ref_3 = $row[51];
-    $tel_2_ref_3 = $row[52];
-    $referencias_4 = $row[53];
-    $nombre_referencia_4 = $row[54];
-    $domicilio_deudor_2 = $row[55];
-    $frecuencia = $row[56];
-    $originacion = $row[57];
-    $estado_referencia_4 = $row[58];
-    $cp_referencia_4 = $row[59];
-    $tel_1_ref_4 = $row[60];
-    $tel_2_ref_4 = $row[61];
-    $domicilio_laboral = $row[62];
-    $colonia_laboral = $row[63];
-    $ciudad_laboral = $row[64];
-    $estado_laboral = $row[65];
-    $cp_laboral = $row[66];
-    $tel_1_laboral = $row[67];
-    $tel_2_laboral = $row[68];
-    $gastos_de_cobranza = $row[69];
-    $fecha_de_actualizacion = $row[70];
-    $numero_de_cuenta = $row[71];
-    $numero_de_credito = $row[72];
-    $contrato = $row[73];
-    $saldo_total = $row[74];
-    $saldo_vencido = $row[75];
-    $saldo_descuento_1 = $row[76];
-    $saldo_descuento_2 = $row[77];
-    $fecha_corte = $row[78];
-    $fecha_1er_pago = $row[79];
-    $fecha_de_ultimo_pago = $row[80];
-    $monto_ultimo_pago = $row[81];
-    $producto = $row[82];
-    $subproducto = $row[83];
-    $cliente = $row[84];
-    $status_de_credito = $row[85];
-    if (empty($status_de_credito)) {
-        $status_de_credito = '';
-    }
-    $pagos_vencidos = $row[86];
-    $monto_adeudado = $row[87];
-    $fecha_de_asignacion = $row[88];
-    $fecha_de_deasignacion = $row[89];
-    $cuenta_concentradora_1 = $row[90];
-    $saldo_cuota = $row[91];
-    if (empty($saldo_cuota)) {
-        $saldo_cuota = 0;
-    }
-    $email_deudor = $row[92];
-    if (isset($row[93])) {
-        $id_cuenta = $row[93];
+    if (empty($id_cuenta)) {
+        $id_cuenta = 0;
+    } else {
         $qsliced = "delete from rslice where user='" . $capt . "';";
         mysqli_query($con, $qsliced) or die("ERROR RM55 - " . mysqli_error($con));
         $qslice = "replace into rslice select *, '" . $capt . "', now() from resumen where id_cuenta=" . $id_cuenta;
         mysqli_query($con, $qslice) or die("ERROR RM55 - " . mysqli_error($con));
-    }
-    $nss = $row[94];
-    $rfc_deudor = $row[95];
-    $telefonos_marcados = $row[96];
-    $tel_1_verif = $row[97];
-    $tel_2_verif = $row[98];
-    $tel_3_verif = $row[99];
-    $tel_4_verif = $row[100];
-    $telefono_de_ultimo_contacto = $row[101];
-    $dias_vencidos = $row[102];
-    $ejecutivo_asignado_call_center = $row[103];
-    $ejecutivo_asignado_domiciliario = $row[104];
-    $prioridad_de_gestion = $row[105];
-    $nrpp = $row[106];
-    $parentesco_aval = $row[107];
-    $localizar = $row[108];
-    $campo_libre_9 = $row[109];
-    $empresa = $row[110];
-    $fecha_de_convenio = $row[113];
-    $direccion_nueva = $row[115];
-    $C_OBSE2 = '';
-    $CUANDO = '';
-    $querycom = "select c_obse2,c_cvst,cuando from historia where c_cont='" . $id_cuenta . "' order by d_fech desc, c_hrin desc limit 1";
-    $resultcom = mysqli_query($con, $querycom) or die("ERROR RM41 - " . mysqli_error($con));
-    while ($answercom = mysqli_fetch_row($resultcom)) {
-        $C_OBSE2 = $answercom[0];
-        $ultimo_status_de_la_gestion = $answercom[1];
-        $CUANDO = $answercom[2];
+
+        $C_OBSE2 = '';
+        $CUANDO = '';
+        $querycom = "select c_obse2,c_cvst,cuando from historia where c_cont='" . $id_cuenta . "' order by d_fech desc, c_hrin desc limit 1";
+        $resultcom = mysqli_query($con, $querycom) or die("ERROR RM41 - " . mysqli_error($con));
+        while ($answercom = mysqli_fetch_row($resultcom)) {
+            $C_OBSE2 = $answercom[0];
+            $ultimo_status_de_la_gestion = $answercom[1];
+            $CUANDO = $answercom[2];
+        }
     }
     if ($id_cuenta == 0) {
         $newcamp = 3;
@@ -505,8 +279,8 @@ ORDER BY queuelist.camp LIMIT 1";
         }
         $queryccamp = "UPDATE nombres SET camp=" . $newcamp . " WHERE iniciales='" . $capt . "';";
         mysqli_query($con, $queryccamp) or die("ERROR RM43 - " . mysqli_error($con));
-    }
-    $queryprom = "select n_prom,d_prom,
+
+        $queryprom = "select n_prom,d_prom,
     n_prom1,d_prom1,n_prom2,d_prom2,
     n_prom3,d_prom3,n_prom4,d_prom4,
     c_freq 
@@ -514,26 +288,27 @@ from historia
 where c_cont=" . $id_cuenta . " and n_prom>0 
 and c_cvst like 'PROM%DE%'
 order by d_fech desc, c_hrin desc limit 1";
-    $resultprom = mysqli_query($con, $queryprom) or die("ERROR RM45 - " . mysqli_error($con));
-    while ($answerprom = mysqli_fetch_row($resultprom)) {
-        $N_PROM_OLD = $answerprom[0];
-        $D_PROM_OLD = $answerprom[1];
-        $N_PROM1_OLD = $answerprom[2];
-        $D_PROM1_OLD = $answerprom[3];
-        $N_PROM2_OLD = $answerprom[4];
-        $D_PROM2_OLD = $answerprom[5];
-        $N_PROM3_OLD = $answerprom[6];
-        $D_PROM3_OLD = $answerprom[7];
-        $N_PROM4_OLD = $answerprom[8];
-        $D_PROM4_OLD = $answerprom[9];
-    }
-    $nmerc = 0;
-    $querycheck = "SELECT timelock, locker,time_to_sec(timediff(now(),timelock))/60 from resumen  WHERE id_cuenta='" . $id_cuenta . "';";
-    $resultcheck = mysqli_query($con, $querycheck) or die("ERROR RM50 - " . mysqli_error($con));
-    while ($answercheck = mysqli_fetch_row($resultcheck)) {
-        $timelock = $answercheck[0];
-        $locker = $answercheck[1];
-        $sofar = $answercheck[2];
+        $resultprom = mysqli_query($con, $queryprom) or die("ERROR RM45 - " . mysqli_error($con));
+        while ($answerprom = mysqli_fetch_row($resultprom)) {
+            $N_PROM_OLD = $answerprom[0];
+            $D_PROM_OLD = $answerprom[1];
+            $N_PROM1_OLD = $answerprom[2];
+            $D_PROM1_OLD = $answerprom[3];
+            $N_PROM2_OLD = $answerprom[4];
+            $D_PROM2_OLD = $answerprom[5];
+            $N_PROM3_OLD = $answerprom[6];
+            $D_PROM3_OLD = $answerprom[7];
+            $N_PROM4_OLD = $answerprom[8];
+            $D_PROM4_OLD = $answerprom[9];
+        }
+        $nmerc = 0;
+        $querycheck = "SELECT timelock, locker,time_to_sec(timediff(now(),timelock))/60 from resumen  WHERE id_cuenta='" . $id_cuenta . "';";
+        $resultcheck = mysqli_query($con, $querycheck) or die("ERROR RM50 - " . mysqli_error($con));
+        while ($answercheck = mysqli_fetch_row($resultcheck)) {
+            $timelock = $answercheck[0];
+            $locker = $answercheck[1];
+            $sofar = $answercheck[2];
+        }
     }
     $tl = date('r');
     if ($mytipo != 'admin') {
