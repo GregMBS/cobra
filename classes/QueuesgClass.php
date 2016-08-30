4414b35fd5f572129eedb05b271573dbfc3b5990
gmbs <gmbs@server>
2016-08-30 16:38:49 -0500

    fix queuesg

--- classes/QueuesgClass.php
+++ classes/QueuesgClass.php
@@ -38,6 +38,8 @@ class QueuesgClass {
      * @return int
      */
     public function getCamp($cliente, $queue, $sdc, $capt) {
+        $array = array($cliente, $queue, $sdc, $capt); 
+        var_dump($array);die();
         $queryqueue  = "select camp from queuelist
     where cliente=:cliente
     and status_aarsa=:queue
