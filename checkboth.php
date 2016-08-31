ed48ef2765067c61bc3c5c4b7d481beeb1a4e084
gmbs <gmbs@server>
2016-08-31 17:05:41 -0500

    checking

--- checkboth.php
+++ checkboth.php
@@ -1,60 +1,33 @@
 <?php
 
 use cobra_salsa\PdoClass;
+use cobra_salsa\CheckClass;
 
 require_once 'classes/PdoClass.php';
+require_once 'classes/CheckClass.php';
 $pdoc = new PdoClass();
 $pdo = $pdoc->dbConnectAdmin();
+$cc = new CheckClass($pdo);
 $capt = filter_input(INPUT_GET, 'capt');
 $tipo = filter_input(INPUT_GET, 'tipo');
-$get = filter_input_array(INPUT_GET);
+$go = filter_input(INPUT_GET, 'go');
 $gestor = filter_input(INPUT_GET, 'gestor');
 $CUENTA = trim(filter_input(INPUT_GET, 'CUENTA'));
-$message = '';
-$go = $get['go'];
 $fechaout = filter_input(INPUT_GET, 'fechaout');
+$message = '';
 if ($go == 'RECIBIR') {
     if (!empty($CUENTA)) {
-        $querycc = "select id_cuenta from resumen
-where ((numero_de_cuenta=:cuenta and cliente<>'Prestamo Relampago')
-or (numero_de_credito=:cuenta and cliente='Prestamo Relampago'))
-and status_de_credito=who(status_de_credito);";
-        $stcc = $pdo->prepare($querycc);
-        $stcc->bindParam(':cuenta', $CUENTA);
-        $stcc->execute();
-
-        $resultcc = $stcc->fetchAll(\PDO::FETCH_ASSOC);
-        foreach ($resultcc as $answercc) {
-            $ID_CUENTA = $answercc['id_cuenta'];
-        }
-        $queryins = "INSERT INTO vasign (cuenta, gestor, fechaout, fechain,c_cont)
-VALUES (:cuenta, :gestor, :fechaout, now(), :idc);";
-        $sti = $pdo->prepare($queryins);
-        if ($C_CONT > 0) {
-            $sti->bindParam(':cuenta', $CUENTA);
-            $sti->bindParam(':gestor', $gestor);
-            $sti->bindParam(':fechaout', $fechaout);
-            $sti->bindParam(':id_cuenta', $ID_CUENTA);
-            $sti->execute();
+        $ID_CUENTA = $cc->getIdCuentafromCuenta($CUENTA);
+        if ($ID_CUENTA > 0) {
+            $cc->insertVasign($CUENTA, $gestor, $fechaout, $ID_CUENTA);
         } else {
             $message = 'No se guard&oacute;';
         }
     }
 }
-$query = "SELECT usuaria,completo FROM nombres where completo<>''
-and (tipo='visitador' or tipo='admin')";
-$result = $pdo->query($query);
-$queryd = "SELECT distinct concat_ws('-',year(d_fech),month(d_fech),day(d_fech)) FROM historia
-where d_fech between (curdate()-interval 1 month) AND curdate()
-order by d_fech
-";
-$resultd = $pdo->query($queryd);
-$querycount = "select sum(fechaout>curdate()),sum(fechain>curdate()) from vasign
-where gestor=:gestor";
-$stc = $pdo->query($querycount);
-$stc->bindParam(':gestor', $gestor);
-$stc->execute();
-$resultcount = $stc->fetchAll();
+$result = $cc->getVisitadores();
+$resultd = $cc->getOneMonth();
+$resultcount = $cc->countInOut($gestor);
 $querymain = "select id_cuenta, numero_de_cuenta, nombre_deudor, cliente, saldo_total,
 q(status_aarsa),completo, fechaout, fechain
 from resumen join vasign on id_cuenta=c_cont join nombres on iniciales=gestor " . $gstring;
---
+++ classes/CheckClass.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace cobra_salsa;
+
+/**
+ * Description of CheckClass
+ *
+ * @author gmbs
+ */
+class CheckClass {
+
+    /**
+     *
+     * @var \PDO
+     */
+    private $pdo;
+
+    /**
+     * 
+     * @param \PDO $pdo
+     */
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * 
+     * @param string $CUENTA
+     * @return int
+     */
+    public function getIdCuentafromCuenta($CUENTA) {
+        $querycc = "select id_cuenta from resumen
+where numero_de_cuenta=:cuenta 
+and status_de_creditonot regexp '-' LIMIT 1";
+        $stcc = $this->pdo->prepare($querycc);
+        $stcc->bindParam(':cuenta', $CUENTA);
+        $stcc->execute();
+        $resultcc = $stcc->fetch(\PDO::FETCH_ASSOC);
+        if (isset($resultcc['id_cuenta'])) {
+            $id_cuenta = $resultcc['id_cuenta'];
+        } else {
+            $id_cuenta = 0;
+        }
+        return $id_cuenta;
+    }
+
+    /**
+     * 
+     * @param string $CUENTA
+     * @param string $gestor
+     * @param string $fechaout
+     * @param int $ID_CUENTA
+     */
+    public function insertVasign($CUENTA, $gestor, $fechaout, $ID_CUENTA) {
+        $queryins = "INSERT INTO vasign (cuenta, gestor, fechaout, fechain,c_cont)
+VALUES (:cuenta, :gestor, :fechaout, now(), :idc);";
+        $sti = $this->pdo->prepare($queryins);
+        $sti->bindParam(':cuenta', $CUENTA);
+        $sti->bindParam(':gestor', $gestor);
+        $sti->bindParam(':fechaout', $fechaout);
+        $sti->bindParam(':id_cuenta', $ID_CUENTA);
+        $sti->execute();
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    public function getVisitadores() {
+        $query = "SELECT usuaria,completo FROM nombres where completo<>''
+and tipo IN ('visitador','admin')";
+        $result = $this->pdo->query($query);
+        return $result;
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    public function getOneMonth() {
+        $output = array();
+        $end = new DateTime();
+        $begin = $end->modify('-1 month');
+
+        $interval = new DateInterval('P1D');
+        $daterange = new DatePeriod($begin, $interval, $end);
+
+        foreach ($daterange as $date) {
+            $output[] = $date->format("Y-m-d");
+        }
+        return $output;
+    }
+
+    /**
+     * 
+     * @param string $gestor
+     * @return array
+     */
+    public function countInOut($gestor) {
+        $querycount = "select sum(fechaout>curdate()) as countOut,
+    sum(fechain>curdate()) as countIn 
+    from vasign
+where gestor=:gestor";
+        $stc = $this->pdo->query($querycount);
+        $stc->bindParam(':gestor', $gestor);
+        $stc->execute();
+        $resultcount = $stc->fetch();
+        return $resultcount;
+    }
+
+}
