6cabd41e4ae38f3dba15adcf03ab9e4de5b99be4
gmbs <gmbs@server>
2016-08-31 10:41:59 -0500

    fix index/login

--- index.php
+++ index.php
@@ -1,96 +1,48 @@
 <?php
 
 use cobra_salsa\PdoClass;
+use cobra_salsa\LoginClass;
 
 $local = $_SERVER['REMOTE_ADDR'];
-$go    = filter_input(INPUT_POST, 'go');
-$capt  = filter_input(INPUT_POST, 'capt');
-$pw    = filter_input(INPUT_POST, 'pwd');
+$go = filter_input(INPUT_POST, 'go');
+$capt = filter_input(INPUT_POST, 'capt');
+$pw = filter_input(INPUT_POST, 'pwd');
 if (!empty($go)) {
     require_once 'classes/PdoClass.php';
+    require_once 'classes/LoginClass.php';
     $pdoc = new PdoClass();
-    $pdo  = $pdoc->dbConnectNobody(); 
-    $mynombre = '';
-    $field    = '';
-    $tipo     = '';
-    $enlace   = '';
-    $queryg   = "SELECT iniciales, enlace, tipo "
-        ."FROM nombres JOIN grupos ON grupo=tipo "
-        ."WHERE passw = sha(:pw) "
-        ."AND LOWER(iniciales) = LOWER(:capt) "
-        ."LIMIT 1";
-    $stg      = $pdo->prepare($queryg);
-    $stg->bindParam(':pw', $pw);
-    $stg->bindParam(':capt', $capt);
-    $stg->execute();
-    $resultg  = $stg->fetchAll(\PDO::FETCH_ASSOC);
-    foreach ($resultg as $answerg) {
-        $mynombre = $answerg['iniciales'];
-        $enlace   = $answerg['enlace'];
-        $tipo     = $answerg['tipo'];
-        if ($tipo == "callcenter") {
-            $field = "ejecutivo_asignado_call_center";
+    $pdo = $pdoc->dbConnectNobody();
+    $lc = new LoginClass($pdo);
+    $userData = $lc->getUserData($capt, $pw);
+    extract($userData);
+    if (isset($tipo)) {
+        switch ($tipo) {
+            case "callcenter":
+                $field = "ejecutivo_asignado_call_center";
+                break;
+
+            case "visitador":
+                $field = "ejecutivo_asignado_domiciliario";
+                break;
+
+            case "admin":
+                $field = "ejecutivo_asignado_call_center";
+                break;
+
+            default:
+                break;
         }
-        if ($tipo == "visitador") {
-            $field = "ejecutivo_asignado_domiciliario";
-        }
-        if ($tipo == "admin") {
-            $field = "ejecutivo_asignado_call_center";
-        }
-        $cpw = $mynombre.sha1($pw).date('U');
+        $cpw = $mynombre . sha1($pw) . date('U');
         if ($mynombre == "gmbs") {
             setcookie('auth', $cpw, time() + 60 * 60 * 24);
         } else {
             setcookie('auth', $cpw, time() + 60 * 60 * 11);
         }
-        $queryc = "update nombres "
-            ."set ticket = :cpw "
-            ."where iniciales = :capt "
-            ."and tipo = :tipo";
-        $stc    = $pdo->prepare($queryc);
-        $stc->bindParam(':cpw', $cpw);
-        $stc->bindParam(':capt', $capt);
-        $stc->bindParam(':tipo', $tipo);
-        $stc->execute();
-        $queryq = "update nombres n,queuelist qu
-			set n.camp = qu.camp
-			where iniciales = gestor
-			and status_aarsa = 'Inicial'
-			and tipo = 'callcenter'
-			and gestor = :capt";
-        $stq    = $pdo->prepare($queryq);
-        $stq->bindParam(':capt', $capt);
-        $stq->execute();
-        $queryu = "delete from userlog "
-            ."where gestor = :capt "
-            ."or usuario = :local";
-        $stdu   = $pdo->prepare($queryu);
-        $stdu->bindParam(':capt', $capt);
-        $stdu->bindParam(':local', $local);
-        $stdu->execute();
-        $queryl = "insert into userlog (usuario,tipo,fechahora,gestor) "
-            ."values (:local, 'login', now(), :capt)";
-        $stlu   = $pdo->prepare($queryl);
-        $stlu->bindParam(':capt', $capt);
-        $stlu->bindParam(':local', $local);
-        $stlu->execute();
-
-        $querypl = "insert into permalog "
-            ."(usuario,tipo,fechahora,gestor) "
-            ."values (:local, 'login', now(), :capt)";
-        $stlp    = $pdo->prepare($querypl);
-        $stlp->bindParam(':capt', $capt);
-        $stlp->bindParam(':local', $local);
-        $stlp->execute();
-    }
-    $echeck = 0;
-    if (($enlace != '') && ($echeck == 0)) {
-        $queryins = "INSERT INTO historia
-			(C_CVGE,C_CVBA,C_CONT,CUENTA,C_CVST,D_FECH,C_HRIN,C_HRFI)
-			VALUES (:capt, '', 0, 0, 'login', curdate(), curtime(), curtime())";
-        $stih     = $pdo->prepare($queryins);
-        $stih->bindParam(':capt', $capt);
-        $stih->execute();
+        $lc->setTicket($cpw, $capt, $tipo);
+        $lc->setInitialQueue($capt);
+        $lc->setUserlog($capt, $local);
+        $lc->insertPermalog($capt, $local);
+        $lc->insertHistoria($capt);
 
         $page = "Location: $enlace?find=$capt&field=$field&i=0&capt=$capt&go=ABINICIO";
         header($page);
