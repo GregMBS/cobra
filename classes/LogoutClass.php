e3c6e7c5bc8f361876efb0084caf56dbe69419f7
gmbs <gmbs@server>
2016-08-31 09:39:06 -0500

    fix logout

---
+++ classes/LogoutClass.php
@@ -0,0 +1,137 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace cobra_salsa;
+
+/**
+ * Description of LogoutClass
+ *
+ * @author gmbs
+ */
+class LogoutClass {
+
+    /**
+     * @var \PDO $pdo
+     */
+    protected $pdo;
+    
+    /**
+     *
+     * @var string 
+     */
+    protected $queryldt = "select d_fech,c_hrfi from historia 
+                    where c_cvge = :capt
+                    order by d_fech desc,c_hrin desc 
+                    limit 1";
+
+
+    /**
+     * 
+     * @param \PDO $pdo
+     */
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * 
+     * @param string $capt
+     */
+    public function unlockCuentas($capt) {
+        $queryunlock = "UPDATE resumen "
+                . "SET timelock = NULL, locker = NULL "
+                . "WHERE locker = :capt";
+        $stu = $this->pdo->prepare($queryunlock);
+        $stu->bindParam(':capt', $capt);
+        $stu->execute();
+    }
+
+    /**
+     * 
+     * @param string $capt
+     * @param string $go
+     * @return array
+     */
+    public function getLogoutDatetime($capt, $go) {
+        $date = date('Y-m-d');
+        $time = date('H:i:s');
+        if ($go == 'forgot') {
+            $go = 'salir';
+            $stl = $this->pdo->prepare($this->queryldt);
+            $stl->bindParam(':capt', $capt);
+            $stl->execute();
+            $resultldt = $stl->fetch(\PDO::FETCH_ASSOC);
+            if ($resultldt) {
+                $date = $resultldt['d_fech'];
+                $time = $resultldt['c_hrin'];
+            }
+        }
+        $output = array(
+            'date' => $date,
+            'time' => $time
+        );
+        return $output;
+    }
+
+    /**
+     * 
+     * @param string $capt
+     * @param string $go
+     * @param string $date
+     * @param string $time
+     */
+    public function insertHistoria($capt, $go, $date, $time) {
+	$queryins	 = "INSERT INTO historia
+		(C_CVGE, C_CVBA, C_CONT, CUENTA, C_CVST, D_FECH, C_HRIN, C_HRFI)
+		VALUES
+		(:capt,'', 0, 0, :go, :date, :timein, :timeout)";
+	$sti		 = $this->pdo->prepare($queryins);
+	$sti->bindParam(':capt', $capt);
+	$sti->bindParam(':go', $go);
+	$sti->bindParam(':date', $date);
+	$sti->bindParam(':timein', $time);
+	$sti->bindParam(':timeout', $time);
+	$sti->execute();
+    }
+    
+    /**
+     * 
+     * @param string $capt
+     */
+    public function clearResumenLocks($capt) {
+	$queryclr	 = "UPDATE resumen SET locker=NULL, timelock=NULL "
+	    ."WHERE locker = :capt";
+	$stc		 = $this->pdo->prepare($queryclr);
+	$stc->bindParam(':capt', $capt);
+	$stc->execute();
+    }
+    
+    /**
+     * 
+     * @param string $capt
+     */
+    public function clearRslice($capt) {
+	$querydel	 = "DELETE from rslice "
+	    ."WHERE user = :capt";
+	$std		 = $this->pdo->prepare($querydel);
+	$std->bindParam(':capt', $capt);
+	$std->execute();
+    }
+    
+    /**
+     * 
+     * @param string $capt
+     */
+    public function expireTicket($capt) {
+	$querynom	 = "update nombres set ticket = NULL "
+	    ."where iniciales = :capt";
+	$stn		 = $this->pdo->prepare($querynom);
+	$stn->bindParam(':capt', $capt);
+	$stn->execute();
+    }
+}
--- logout.php
+++ logout.php
@@ -1,62 +1,23 @@
 <?php
 
 use cobra_salsa\PdoClass;
+use cobra_salsa\LogoutClass;
 
-require_once 'classes/PdoClass.php'; // returns $pdo
+require_once 'classes/PdoClass.php';
+require_once 'classes/LogoutClass.php';
 $pdoc		 = new PdoClass();
 $pdo		 = $pdoc->dbConnectUser();
+$lc              = new LogoutClass($pdo);
 $capt		 = filter_input(INPUT_GET, 'capt');
 $go		 = filter_input(INPUT_GET, 'gone');
-$queryunlock	 = "UPDATE resumen "
-    ."SET timelock = NULL, locker = NULL "
-    ."WHERE locker = :capt";
-$stu		 = $pdo->prepare($queryunlock);
-$stu->bindParam(':capt', $capt);
-$stu->execute();
+$lc->unlockCuentas($capt);
 if ($go != "") {
-	$date		 = date('Y-m-d');
-	$time		 = date('H:i:s');
-	$queryins	 = "INSERT INTO historia
-		(C_CVGE, C_CVBA, C_CONT, CUENTA, C_CVST, D_FECH, C_HRIN, C_HRFI)
-		VALUES
-		(:capt,'', 0, 0, :go, :date, :timein, :timeout)";
-	if ($go == 'forgot') {
-		$go		 = 'salir';
-		$queryldt	 = "select d_fech,c_hrfi from historia "
-		    ."where c_cvge = :capt"
-		    ."order by d_fech desc,c_hrin desc "
-		    ."limit 1";
-		$stl		 = $pdo->prepare($queryldt);
-		$stl->bindParam(':capt', $capt);
-		$stl->execute();
-		$resultldt	 = $stl->fetchAll(\PDO::FETCH_ASSOC);
-		foreach ($resultldt as $answerldt) {
-			$date	 = $answerldt['d_fech'];
-			$time	 = $answerldt['c_hrin'];
-		}
-	}
-	$sti		 = $pdo->prepare($queryins);
-	$sti->bindParam(':capt', $capt);
-	$sti->bindParam(':go', $go);
-	$sti->bindParam(':date', $date);
-	$sti->bindParam(':timein', $time);
-	$sti->bindParam(':timeout', $time);
-	$sti->execute();
-	$queryclr	 = "UPDATE resumen SET locker=NULL, timelock=NULL"
-	    ."WHERE locker = :capt";
-	$stc		 = $pdo->prepare($queryclr);
-	$stc->bindParam(':capt', $capt);
-	$stc->execute();
-	$querydel	 = "DELETE from rslice "
-	    ."WHERE user = :capt";
-	$std		 = $pdo->prepare($querydel);
-	$std->bindParam(':capt', $capt);
-	$std->execute();
-	$querynom	 = "update nombres set ticket = NULL "
-	    ."where iniciales=:capt";
-	$stn		 = $pdo->prepare($querynom);
-	$stn->bindParam(':capt', $capt);
-	$stn->execute();
+    $resultdt = $lc->getLogoutDatetime($capt, $go);
+    extract($resultdt);
+	$lc->insertHistoria($capt, $go, $date, $time);
+	$lc->clearResumenLocks($capt);
+	$lc->clearRslice($capt);
+	$lc->expireTicket($capt);
 	$page		 = "Location: index.php";
 	if (($go != "salir") && ($go != "error")) {
 		$page = "Location: breaks.php?capt=".$capt;
