382b09d879758e6a47fb339846b251ea4475c009
gmbs <gmbs@server>
2016-08-31 14:33:47 -0500

    ddh

--- classes/DhClass.php
+++ classes/DhClass.php
@@ -9,26 +9,24 @@ namespace cobra_salsa;
  */
 
 /**
- * Dtabase Class for ddh/pdh
+ * Database Class for ddh/pdh
  *
  * @author gmbs
  *
  */
-class DhClass
-{
+class DhClass {
+
     /**
      * @var \PDO $pdo
      */
     protected $pdo;
 
-    public function __construct($pdo)
-    {
+    public function __construct($pdo) {
         $this->pdo = $pdo;
     }
 
-    public function getPromesas($gestor, $fecha)
-    {
-        $query  = "select numero_de_cuenta,nombre_deudor,saldo_total,
+    public function getPromesas($gestor, $fecha) {
+        $query = "select numero_de_cuenta,nombre_deudor,saldo_total,
 status_de_credito,ejecutivo_asignado_call_center,
 pagos_vencidos,status_aarsa,
 saldo_descuento_1,producto,estado_deudor,ciudad_deudor,cliente,id_cuenta,
@@ -39,7 +37,7 @@ where c_cvge=:gestor and d_fech=:fecha and n_prom>0
 group by id_cuenta
 ORDER BY
 saldo_total desc, pagos_vencidos";
-        $stq    = $this->pdo->prepare($query);
+        $stq = $this->pdo->prepare($query);
         $stq->bindParam(':gestor', $gestor);
         $stq->bindParam(':fecha', $fecha);
         $stq->execute();
@@ -47,9 +45,8 @@ saldo_total desc, pagos_vencidos";
         return $result;
     }
 
-    public function getGestiones($gestor, $fecha)
-    {
-        $query  = "select numero_de_cuenta,nombre_deudor,saldo_total,
+    public function getGestiones($gestor, $fecha) {
+        $query = "select numero_de_cuenta,nombre_deudor,saldo_total,
 status_de_credito,ejecutivo_asignado_call_center,
 pagos_vencidos,c_cvst,
 saldo_descuento_1,producto,estado_deudor,ciudad_deudor,cliente,id_cuenta,
@@ -59,11 +56,38 @@ join dictamenes on dictamen=status_aarsa
 where c_cvge=:gestor and d_fech=:fecha
 ORDER BY
 saldo_total desc, pagos_vencidos,d_fech,c_hrin";
-        $stq    = $this->pdo->prepare($query);
+        $stq = $this->pdo->prepare($query);
         $stq->bindParam(':gestor', $gestor);
         $stq->bindParam(':fecha', $fecha);
         $stq->execute();
         $result = $stq->fetchAll(\PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    /**
+     * 
+     * @param string $gestor
+     * @param string $fecha
+     * @return array
+     */
+    public function getDhMain($gestor, $fecha) {
+        $querymain = "select numero_de_cuenta, nombre_deudor,
+    saldo_total, status_de_credito, status_aarsa,
+    ejecutivo_asignado_call_center,
+    dias_vencidos, c_cvst, c_hrin,
+    saldo_descuento_2, producto, estado_deudor,
+    ciudad_deudor, cliente, id_cuenta,
+    n_prom, d_prom, v_cc as 'vcc'
+    from resumen
+    join historia on id_cuenta=c_cont
+    join dictamenes on dictamen=status_aarsa
+    where c_cvge=:gestor and d_fech=:fecha
+    ORDER BY d_fech, c_hrin;";
+        $stm = $this->pdo->prepare($querymain);
+        $stm->bindParam(':gestor', $gestor);
+        $stm->bindParam(':fecha', $fecha);
+        $stm->execute();
+        $result = $stm->fetchAll(\PDO::FETCH_ASSOC);
         return $result;
     }
 
--- ddh.php
+++ ddh.php
@@ -1,27 +1,15 @@
 <?php
 
 use cobra_salsa\PdoClass;
+use cobra_salsa\DhClass;
 
 require_once 'classes/PdoClass.php';
-$pdoc      = new PdoClass();
-$pdo       = $pdoc->dbConnectAdmin();
-$capt      = filter_input(INPUT_GET, 'capt');
-$gestor    = filter_input(INPUT_GET, 'gestor');
-$fecha     = filter_input(INPUT_GET, 'fecha');
-$querymain = "select numero_de_cuenta, nombre_deudor,
-    saldo_total, status_de_credito, status_aarsa,
-    ejecutivo_asignado_call_center,
-    dias_vencidos, c_cvst, c_hrin,
-    saldo_descuento_2, producto, estado_deudor,
-    ciudad_deudor, cliente, id_cuenta,
-    n_prom, d_prom, vcc(status_aarsa) as 'vcc'
-    from resumen
-    join historia on id_cuenta=c_cont
-    where c_cvge=:gestor and d_fech=:fecha
-    ORDER BY d_fech, c_hrin;";
-$stm       = $pdo->prepare($querymain);
-$stm->bindParam(':gestor', $gestor);
-$stm->bindParam(':fecha', $fecha);
-$stm->execute();
-$result    = $stm->fetchAll(\PDO::FETCH_ASSOC);
+require_once 'classes/DhClass';
+$pdoc = new PdoClass();
+$pdo = $pdoc->dbConnectAdmin();
+$dc = new DhClass($pdo);
+$capt = filter_input(INPUT_GET, 'capt');
+$gestor = filter_input(INPUT_GET, 'gestor');
+$fecha = filter_input(INPUT_GET, 'fecha');
+$result = $dc->getDhMain($gestor, $fecha);
 require_once 'views/ddhView.php';
