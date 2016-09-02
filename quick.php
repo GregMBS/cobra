0563211c6c73e319e3a5c19a07ab45d59eb331fb
gmbs <gmbs@server>
2016-09-02 13:37:40 -0500

    quick cleanup

--- quick.php
+++ quick.php
@@ -12,28 +12,6 @@ $pdo    = $pc->dbConnectAdmin();
 $capt   = filter_input(INPUT_GET, 'capt');
 $folios = 0;
 $errors = 0;
-if ($capt == 'gmbs') {
-    $querytrouble  = "select count(1) as ct
-from trouble  
-where it_guy is null
-;";
-    $resulttrouble = $pdo->query($querytrouble);
-    foreach ($resulttrouble as $answertrouble) {
-        $errors = $answertrouble['ct'];
-    }
-}
-/*
-  $fout       = 9999;
-  $queryfout  = "select count(distinct folio) as ct
-  from folios
-  where usado=0
-  and enviado=0 and cliente like 'Credito Si%'
-  ;";
-  $resultfout = $pdo->query($queryfout);
-  foreach ($resultfout as $answerfout) {
-  $fout = $answerfout['ct'];
-  }
- */
 require_once 'classes/quickAhoraClass.php';
 $qa           = new QuickAhoraClass($pdo);
 $resultAhora  = $qa->getAhora();
