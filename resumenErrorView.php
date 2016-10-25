0ccdcfc1c7452de55ba7669a9e2f672896c9e1e4
gmbs <gmbs@matriz.gmbs-consulting.com>
2016-10-25 13:08:07 -0500

    PLEASE LOGIN

--- resumenErrorView.php
+++ resumenErrorView.php
@@ -4,12 +4,22 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-    <HTML>
-        <HEAD>
-            <TITLE>Error de capturar gestion</TITLE>
-        </HEAD>
-        <BODY>
-            <h2><?php echo $flagmsg; ?></h2>
+<HTML>
+    <HEAD>
+        <TITLE>Error de capturar gestion</TITLE>
+    </HEAD>
+    <BODY>
+        <h2><?php echo $flagmsg; ?></h2>
+        <?php
+        if (isset($C_CONT)) {
+            ?>
             <a href="resumen.php?capt=<?php echo $capt; ?>&field=id_cuenta&find=<?php echo $C_CONT; ?>">Regresa para arreglarlo</a>
-        </BODY>
-    </HTML>
+            <?php
+        } else {
+            ?>
+            <a href="index.php">Favor de LOGIN.</a>
+            <?php
+        }
+        ?>
+    </BODY>
+</HTML>
