b689f36b6d15415a1ee82e2078a7b8976ba54018
gmbs <gmbs@matriz.gmbs-consulting.com>
2016-12-07 08:45:06 -0600

    fix tels

--- classes/TelsClass.php
+++ classes/TelsClass.php
@@ -29,46 +29,46 @@ class TelsClass {
      *
      * @var string
      */
-    private $mercadosReportQuery = 'select cliente,nombre_deudor,concat(" ",numero_de_cuenta) as "numero_de_cuenta",
-concat(" ",tel_1) as "tel 1",tel_1 in (select c_tele from marcados) as tel_1_marcado,
-concat(" ",tel_2) as "tel 2",tel_2 in (select c_tele from marcados) as tel_2_marcado,
-concat(" ",tel_3) as "tel 3",tel_3 in (select c_tele from marcados) as tel_3_marcado,
-concat(" ",tel_4) as "tel 4",tel_4 in (select c_tele from marcados) as tel_4_marcado,
-concat(" ",tel_1_alterno) as "tel 1 alterno",tel_1_alterno in (select c_tele from marcados) as tel_1_alterno_marcado,
-concat(" ",tel_2_alterno) as "tel 2 alterno",tel_2_alterno in (select c_tele from marcados) as tel_2_alterno_marcado,
-concat(" ",tel_3_alterno) as "tel 3 alterno",tel_3_alterno in (select c_tele from marcados) as tel_3_alterno_marcado,
-concat(" ",tel_4_alterno) as "tel 4 alterno",tel_4_alterno in (select c_tele from marcados) as tel_4_alterno_marcado,
-concat(" ",tel_1_laboral) as "tel 1 laboral",tel_1_laboral in (select c_tele from marcados) as tel_1_laboral_marcado,
-concat(" ",tel_2_laboral) as "tel 2 laboral",tel_2_laboral in (select c_tele from marcados) as tel_2_laboral_marcado,
-concat(" ",tel_1_ref_1) as "tel 1 ref 1",tel_1_ref_1 in (select c_tele from marcados) as tel_1_ref_1_marcado,
-concat(" ",tel_1_ref_2) as "tel 1 ref 2",tel_1_ref_2 in (select c_tele from marcados) as tel_1_ref_2_marcado,
-concat(" ",tel_1_ref_3) as "tel 1 ref 3",tel_1_ref_3 in (select c_tele from marcados) as tel_1_ref_3_marcado,
-concat(" ",tel_1_ref_4) as "tel 1 ref 4",tel_1_ref_4 in (select c_tele from marcados) as tel_1_ref_4_marcado
+    private $mercadosReportQuery = "select cliente,nombre_deudor,concat(' ',numero_de_cuenta) as 'numero_de_cuenta',
+concat(' ',tel_1) as 'tel 1',tel_1 in (select c_tele from marcados) as tel_1_marcado,
+concat(' ',tel_2) as 'tel 2',tel_2 in (select c_tele from marcados) as tel_2_marcado,
+concat(' ',tel_3) as 'tel 3',tel_3 in (select c_tele from marcados) as tel_3_marcado,
+concat(' ',tel_4) as 'tel 4',tel_4 in (select c_tele from marcados) as tel_4_marcado,
+concat(' ',tel_1_alterno) as 'tel 1 alterno',tel_1_alterno in (select c_tele from marcados) as tel_1_alterno_marcado,
+concat(' ',tel_2_alterno) as 'tel 2 alterno',tel_2_alterno in (select c_tele from marcados) as tel_2_alterno_marcado,
+concat(' ',tel_3_alterno) as 'tel 3 alterno',tel_3_alterno in (select c_tele from marcados) as tel_3_alterno_marcado,
+concat(' ',tel_4_alterno) as 'tel 4 alterno',tel_4_alterno in (select c_tele from marcados) as tel_4_alterno_marcado,
+concat(' ',tel_1_laboral) as 'tel 1 laboral',tel_1_laboral in (select c_tele from marcados) as tel_1_laboral_marcado,
+concat(' ',tel_2_laboral) as 'tel 2 laboral',tel_2_laboral in (select c_tele from marcados) as tel_2_laboral_marcado,
+concat(' ',tel_1_ref_1) as 'tel 1 ref 1',tel_1_ref_1 in (select c_tele from marcados) as tel_1_ref_1_marcado,
+concat(' ',tel_1_ref_2) as 'tel 1 ref 2',tel_1_ref_2 in (select c_tele from marcados) as tel_1_ref_2_marcado,
+concat(' ',tel_1_ref_3) as 'tel 1 ref 3',tel_1_ref_3 in (select c_tele from marcados) as tel_1_ref_3_marcado,
+concat(' ',tel_1_ref_4) as 'tel 1 ref 4',tel_1_ref_4 in (select c_tele from marcados) as tel_1_ref_4_marcado
 from resumen
-where status_de_credito not like "%ivo"
-order by cliente,numero_de_cuenta';
+where status_de_credito not regexp '-'
+order by cliente,numero_de_cuenta";
 
     /**
      *
      * @var string
      */
-    private $contactosReportQuery = 'select cliente,nombre_deudor,concat(" ",numero_de_cuenta) as "numero_de_cuenta",
-concat(" ",tel_1) as "tel 1",tel_1 in (select c_tele from contactados) as tel_1_contacto,
-concat(" ",tel_2) as "tel 2",tel_2 in (select c_tele from contactados) as tel_2_contacto,
-concat(" ",tel_3) as "tel 3",tel_3 in (select c_tele from contactados) as tel_3_contacto,
-concat(" ",tel_4) as "tel 4",tel_4 in (select c_tele from contactados) as tel_4_contacto,
-concat(" ",tel_1_alterno) as "tel 1 alterno",tel_1_alterno in (select c_tele from contactados) as tel_1_alterno_contacto,
-concat(" ",tel_2_alterno) as "tel 2 alterno",tel_2_alterno in (select c_tele from contactados) as tel_2_alterno_contacto,
-concat(" ",tel_3_alterno) as "tel 3 alterno",tel_3_alterno in (select c_tele from contactados) as tel_3_alterno_contacto,
-concat(" ",tel_4_alterno) as "tel 4 alterno",tel_4_alterno in (select c_tele from contactados) as tel_4_alterno_contacto,
-concat(" ",tel_1_laboral) as "tel 1 laboral",tel_1_laboral in (select c_tele from contactados) as tel_1_laboral_contacto,
-concat(" ",tel_2_laboral) as "tel 2 laboral",tel_2_laboral in (select c_tele from contactados) as tel_2_laboral_contacto,
-concat(" ",tel_1_ref_1) as "tel 1 ref 1",tel_1_ref_1 in (select c_tele from contactados) as tel_1_ref_1_contacto,
-concat(" ",tel_1_ref_2) as "tel 1 ref 2",tel_1_ref_2 in (select c_tele from contactados) as tel_1_ref_2_contacto,
-concat(" ",tel_1_ref_3) as "tel 1 ref 3",tel_1_ref_3 in (select c_tele from contactados) as tel_1_ref_3_contacto,
-concat(" ",tel_1_ref_4) as "tel 1 ref 4",tel_1_ref_4 in (select c_tele from contactados) as tel_1_ref_4_contacto
+    private $contactosReportQuery = "select cliente,nombre_deudor,concat(' ',numero_de_cuenta) as 'numero_de_cuenta',
+concat(' ',tel_1) as 'tel 1',tel_1 in (select c_tele from contactados) as tel_1_contacto,
+concat(' ',tel_2) as 'tel 2',tel_2 in (select c_tele from contactados) as tel_2_contacto,
+concat(' ',tel_3) as 'tel 3',tel_3 in (select c_tele from contactados) as tel_3_contacto,
+concat(' ',tel_4) as 'tel 4',tel_4 in (select c_tele from contactados) as tel_4_contacto,
+concat(' ',tel_1_alterno) as 'tel 1 alterno',tel_1_alterno in (select c_tele from contactados) as tel_1_alterno_contacto,
+concat(' ',tel_2_alterno) as 'tel 2 alterno',tel_2_alterno in (select c_tele from contactados) as tel_2_alterno_contacto,
+concat(' ',tel_3_alterno) as 'tel 3 alterno',tel_3_alterno in (select c_tele from contactados) as tel_3_alterno_contacto,
+concat(' ',tel_4_alterno) as 'tel 4 alterno',tel_4_alterno in (select c_tele from contactados) as tel_4_alterno_contacto,
+concat(' ',tel_1_laboral) as 'tel 1 laboral',tel_1_laboral in (select c_tele from contactados) as tel_1_laboral_contacto,
+concat(' ',tel_2_laboral) as 'tel 2 laboral',tel_2_laboral in (select c_tele from contactados) as tel_2_laboral_contacto,
+concat(' ',tel_1_ref_1) as 'tel 1 ref 1',tel_1_ref_1 in (select c_tele from contactados) as tel_1_ref_1_contacto,
+concat(' ',tel_1_ref_2) as 'tel 1 ref 2',tel_1_ref_2 in (select c_tele from contactados) as tel_1_ref_2_contacto,
+concat(' ',tel_1_ref_3) as 'tel 1 ref 3',tel_1_ref_3 in (select c_tele from contactados) as tel_1_ref_3_contacto,
+concat(' ',tel_1_ref_4) as 'tel 1 ref 4',tel_1_ref_4 in (select c_tele from contactados) as tel_1_ref_4_contacto
 from resumen
-where status_de_credito not regexp "-"';
+where status_de_credito not regexp '-'";
 
     /**
      * 
