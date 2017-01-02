1b816a86e8444efce03150cf477c8b55dfa00cbe
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-01-02 12:55:13 -0600

    kill DownloadClass

--- CreditoRealReport.php
+++ CreditoRealReport.php
@@ -1,18 +1,22 @@
 <?php
+
+use cobra_salsa\OutputClass;
+
 require_once 'CreditoRealModels.php';
 $cr = new CreditoRealModels();
-require_once 'DownloadClass.php';
-$dl = new DownloadClass;
+require_once 'classes/OutputClass.php';
+$cc = new OutputClass();
 $output = array();
 $fromModel = $cr->outputReport();
 $headers = $fromModel['headers'];
 $data = $fromModel['data'];
+$filename = 'CreditoRealReport';
 
 foreach ($data as $row) {
     $output[]=$row;
 }
 if ($output) {
-    $dl->sendToCSV($headers,$output, 'CreditoRealReport');
+    $cc->writeCSVFile($filename, $output, $headers);
 } else {
     var_dump($output);die();
 }
