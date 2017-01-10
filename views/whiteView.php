0ea22b92884765b75fa031551fd1c0cedea8a208
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-01-10 10:15:08 -0600

    classify white

--- views/whiteView.php
+++ views/whiteView.php
@@ -1,6 +1,34 @@
-<?php
-require_once 'pdoConnect.php';
-$pdoc = new pdoConnect();
-$pdo  = $pdoc->dbConnectUser();
-$capt = filter_input(INPUT_GET, 'capt');
-require_once 'views/whiteView.php';
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Directorio</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.0/themes/redmond/jquery-ui.css" type="text/css" media="all" />
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
+        <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div>
+            <form action='whitesearch.php' method='get'>
+                <input type="hidden" name="capt" value="<?php echo $capt; ?>">
+                <input type="hidden" name="go" value="BUSCAR">
+                <label for="nombre">Nombre</label>
+                <input type='text' name='nombre' id="nombre" value=''><br>
+                <label for="tel">Teléfono</label>
+                <input type='text' name='tel' id='tel' value=''><br>
+                <label for="calle">Calle</label>
+                <input type='text' name='calle' id='calle' value=''><br>
+                <label for="colonia">Colonia</label>
+                <input type='text' name='colonia' id='colonia' value=''><br>
+                <label for="ciudad">Ciudad</label>
+                <input type='text' name='ciudad' id='ciudad' value=''><br>
+                <label for="estado">Estado</label>
+                <input type='text' name='estado' id='estado' value=''><br>
+                <label for="cp">CP</label>
+                <input type='text' name='cp' id='cp' value=""><br>
+                <input type="submit" value="BUSCAR">
+                <a href="javascript:window.location='white.php?capt=<?php echo $capt ?>';">CLARO</a>
+            </form>
+        </div>
+    </body>
+</html> 
