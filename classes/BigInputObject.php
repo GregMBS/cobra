a170eafcd3ec722349f1328a42ca7c77649086ab
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-02-14 17:59:21 -0600

    bigproms cleanup

--- classes/BigInputObject.php
+++ classes/BigInputObject.php
@@ -238,7 +238,7 @@ class BigInputObject {
      * @return boolean
      */
     private function validDate($date) {
-        if (preg_match('/^(\d{4})-(\d{2})-(\d{2})', $date) == true) {
+        if (!empty($date)) {
             return true;
         } else {
             return false;
