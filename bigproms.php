ef2d8ed72ae81e49aa3f7c074b65696fbf2e0dcd
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-02-15 17:46:35 -0600

    Remove confusion in bigproms output filename

--- bigproms.php
+++ bigproms.php
@@ -3,11 +3,11 @@
 set_time_limit(300);
 require_once 'vendor/autoload.php';
 
-use cobra_salsa\PdoClass;
+use Box\Spout\Common\Type;
+use Box\Spout\Writer\WriterFactory;
 use cobra_salsa\BigClass;
 use cobra_salsa\BigInputObject;
-use Box\Spout\Writer\WriterFactory;
-use Box\Spout\Common\Type;
+use cobra_salsa\PdoClass;
 
 require_once 'classes/PdoClass.php';
 require_once 'classes/BigClass.php';
@@ -26,26 +26,26 @@ $tipo = filter_input(INPUT_GET, 'tipo');
 $go = filter_input(INPUT_GET, 'go');
 
 if (!empty($fecha1)) {
-    $bio = new BigInputObject($fecha1, $fecha2, $gestor, $cliente, $fecha3, $fecha4, $tipo);
-    $result = $bc->getProms($bio);
-    if ($result) {
-        $filename = "Query_de_promesas_" . $fecha3 . '_' . $fecha4 . ".xlsx";
-        $output = array();
-        $output[] = array_keys($result[0]);
-        foreach ($result as $row) {
-            $output[] = $row;
-        }
-        $writer = WriterFactory::create(Type::XLSX);
-        $writer->openToBrowser($filename); // stream data directly to the browser
-        $writer->addRows($output); // add multiple rows at a time
-        $writer->close();
-    }
+	$bio = new BigInputObject($fecha1, $fecha2, $gestor, $cliente, $fecha3, $fecha4, $tipo);
+	$result = $bc->getProms($bio);
+	if ($result) {
+		$filename = "Query_de_promesas.xlsx";
+		$output = array();
+		$output[] = array_keys($result[0]);
+		foreach ($result as $row) {
+			$output[] = $row;
+		}
+		$writer = WriterFactory::create(Type::XLSX);
+		$writer->openToBrowser($filename); // stream data directly to the browser
+		$writer->addRows($output); // add multiple rows at a time
+		$writer->close();
+	}
 } else {
-    $resultg = $bc->getGestionGestores();
-    $resultc = $bc->getGestionClientes();
-    $resultdf = $bc->getGestionDates('ASC');
-    $resultfd = $bc->getGestionDates('DESC');
-    $resultdp = $bc->getPromDates('ASC');
-    $resultpd = $bc->getPromDates('DESC');
-    require 'views/bigpromsView.php';
+	$resultg = $bc->getGestionGestores();
+	$resultc = $bc->getGestionClientes();
+	$resultdf = $bc->getGestionDates('ASC');
+	$resultfd = $bc->getGestionDates('DESC');
+	$resultdp = $bc->getPromDates('ASC');
+	$resultpd = $bc->getPromDates('DESC');
+	require 'views/bigpromsView.php';
 }
