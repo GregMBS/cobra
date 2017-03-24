afbf362a67f8cfd5cd431a130a4fbc55cb94677a
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-03-24 16:04:06 -0600

    fix cookie

--- index.php
+++ index.php
@@ -33,9 +33,9 @@ if (!empty($go)) {
         }
         $cpw = $capt . sha1($pw) . date('U');
         if ($capt == "gmbs") {
-            setcookie('auth', $cpw, time() + 60 * 60 * 24);
+            setcookie('auth', $cpw, time() + 60 * 60 * 24, "/", "demo.gmbs-consulting.com", 0, 1);
         } else {
-            setcookie('auth', $cpw, time() + 60 * 60 * 11);
+            setcookie('auth', $cpw, time() + 60 * 60 * 11, "/", "demo.gmbs-consulting.com", 0, 1);
         }
         $lc->setTicket($cpw, $capt, $userData['tipo']);
         $lc->setInitialQueue($capt);
