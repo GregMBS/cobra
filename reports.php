3df1df48c0cd33ab43ba4c9772b777ec3ebf98db
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-03-25 16:17:32 -0600

    login issues

--- reports.php
+++ reports.php
@@ -5,7 +5,5 @@ use gregmbs\cobra\PdoClass;
 require_once 'classes/PdoClass.php';
 $pdoc = new PdoClass();
 $pdo = $pdoc->dbConnectAdmin();
-var_dump($pdo);
-die();
 $capt = filter_input(INPUT_GET, 'capt');
 require_once 'views/reportsView.php';
