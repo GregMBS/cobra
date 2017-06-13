344094616cf65ac043873d9323e115d6992d6616
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-06-13 14:56:48 -0500

    +5 protection vs empty clients

--- inventario.xls.php
+++ inventario.xls.php
@@ -20,11 +20,13 @@ set_time_limit(180);
 $go = filter_input(INPUT_GET, 'go');
 if (!empty($go)) {
     $cliente = filter_input(INPUT_GET, 'cliente');
-    $result = $ic->getInventarioReport($cliente);
-    if (is_array($result[0])) {
-        $filename = "Query_de_inventario_" . date('ymd') . ".xlsx";
-        $headers = array_keys($result[0]);
-        $oc->writeXLSXFile($filename, $result, $headers);
+    if (!empty($cliente)) {
+        $result = $ic->getInventarioReport($cliente);
+        if (is_array($result[0])) {
+            $filename = "Query_de_inventario_" . date('ymd') . ".xlsx";
+            $headers = array_keys($result[0]);
+            $oc->writeXLSXFile($filename, $result, $headers);
+        }
     }
 } else {
     $resultc = $ic->listClients();
