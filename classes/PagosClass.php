1256147d8c95979041d107e4fe8fa1d4a44ca155
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-08-07 09:28:35 -0500

    change pagos detail format

--- classes/PagosClass.php
+++ classes/PagosClass.php
@@ -72,9 +72,10 @@ group by cli, sdc with rollup";
      * @return array
      */
     public function detailsThisMonth() {
-        $queryActDet = "select cuenta, fecha, monto, cliente, status_de_credito as sdc, 
+        $queryActDet = "select cuenta, fecha, monto, pagos.cliente, 
+            status_de_credito as sdc, 
             gestor, confirmado, fechacapt, pagos.id_cuenta 
-from pagos ,resumen
+from pagos,resumen
 where fecha>last_day(curdate()-interval 1 month)
 and pagos.id_cuenta = resumen.id_cuenta
 order by cliente,gestor,fecha";
