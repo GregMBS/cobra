1017c8d01c82470824e6d2c2869f2a6bc30a4e48
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-08-09 16:03:11 -0500

    big proms error

--- classes/BigClass.php
+++ classes/BigClass.php
@@ -373,6 +373,9 @@ WHERE n_prom>0
 			$stm->bindParam(':cliente', $bio->getCliente());
 		}
 		$stm->execute();
+                var_dump($stm);
+                var_dump($bio);
+                die();
 		$result = $stm->fetchAll(\PDO::FETCH_ASSOC);
 		return $result;
 	}
