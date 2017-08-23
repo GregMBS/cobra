9dad269d927274a306f9678dbee122b470df7581
gmbs <gmbs@matriz.gmbs-consulting.com>
2017-08-23 18:08:10 -0500

    upload pics for EVERYONE

--- cargaPicLoad.php
+++ cargaPicLoad.php
@@ -12,17 +12,20 @@ $cuenta = filter_input(INPUT_POST, 'cuenta');
 $cliente = filter_input(INPUT_POST, 'cliente');
 $query = "SELECT id_cuenta FROM resumen 
         WHERE ciente = :clliente
-        AND numero_de_cuenta = :cuenta"; 
+        AND numero_de_cuenta = :cuenta";
 $sti = $pdo->prepare($query);
 $sti->bindParam(':cliente', $cliente);
 $sti->bindParam(':cuenta', $cuenta);
 $sti->execute();
 $qresult = $sti->fetch(PDO::FETCH_ASSOC);
 $id_cuenta = $qresult['id_cuenta'];
-if ($_FILES["file"]["error"] == 0) {
-    $deststr = $_SERVER['DOCUMENT_ROOT']."/uploads/".$id_cuenta.'.jpg';
-    move_uploaded_file($_FILES["file"]["tmp_name"], $deststr);
-    $result  = TRUE;
+if ($id_cuenta) {
+    if ($_FILES["file"]["error"] == 0) {
+        $deststr = $_SERVER['DOCUMENT_ROOT'] . "/uploads/" . $id_cuenta . '.jpg';
+        move_uploaded_file($_FILES["file"]["tmp_name"], $deststr);
+        $result = TRUE;
+    }
+} else {
+    $result = FALSE;
 }
-
 include 'cargaPic.php';
