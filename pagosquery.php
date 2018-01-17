3743af3c9356e71e291f8af5b3e1ac843ccdcd33
Gregory Miles Blumenthal Scharf <llameadrpc@gmail.com>
2018-01-16 18:02:45 -0600

    wtf

--- pagosquery.php
+++ pagosquery.php
@@ -17,8 +17,6 @@ $fecha2 = filter_input(INPUT_GET, 'fecha2');
 $cliente = filter_input(INPUT_GET, 'cliente');
 if (filter_has_var(INPUT_GET, 'go')) {
     $result = $pc->queryAll($fecha1, $fecha2, $cliente);
-    var_dump($result);
-    die();
     if (count($result > 0)) {
         $filename = "pagos.xlsx";
         $output = array();
