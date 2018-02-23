b375e5cc113d141d96e24c54029b5f6ccd14159c
Gregory Miles Blumenthal Scharf <llameadrpc@gmail.com>
2018-02-23 10:00:53 -0600

    wtf

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -118,6 +118,7 @@ class LoginController extends Controller
      * @return int
      */
     private function oldToNew(array $oldData, $password) {
+        dd($oldData);
         $new = new User();
         $new->completo = $oldData['completo'];
         $new->iniciales = $oldData['iniciales'];
