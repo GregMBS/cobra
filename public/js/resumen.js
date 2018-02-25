ae96f526e2231e3faddd451594d1164e51f237fe
Gregory Miles Blumenthal Scharf <llameadrpc@gmail.com>
2018-02-25 14:37:23 -0600

    define outside

--- public/js/resumen.js
+++ public/js/resumen.js
@@ -23,73 +23,73 @@ $(function() {
     if (inactivo.test(sdc) && (tipo != 'admin')) {
         $('#GUARTbutt').hide();
     }
-
-    function clock() {
-        var d = new Date();
-        var tn = d.getTime();
-        var tll = new Date(tlp);
-        var tl = tll.getTime();
-        $("#timer").val(tn - tl);
-        var timenow = $("#timer").val();
-        $("#timers").val(parseInt(parseInt(timenow) / 1000) % 60);
-        $("#timerm").val(parseInt(parseInt(timenow) / 1000 / 60));
-        var timemin = $("#timerm").val();
-        if (timemin > 2) {
-            $("#clock").css('backgroundColor', 'yellow');
-        }
-        if (timemin > 4) {
-            $("#clock").css('backgroundColor', 'red');
-        }
-        if (timenow % 2 == 0) {
-            $("#clock").css('backgroundColor', 'green');
-        }
-    }
-
-    function openSearch() {
-        setInterval('clock()', 1000);
-    }
-
-    function tooLong(e) {
-        if ($("#C_OBSE1").val().length > 250) {
-        	$("#C_OBSE1").val($("#C_OBSE1").val().replace(' 	', ' '));
-        	$("#C_OBSE1").val($("#C_OBSE1").val().substr(0, 200));
-            confirm('GESTION demasiado largo');
-            $("#C_OBSE1").css('backgroundColor',"yellow");
-            return false;
-        }
-    }
-
-    function showsearch() {
-    	$("#searchbox").show();
-        $('#find').focus();
-    }
-
-    function showbox(boxname) {
-    	var boxId = '#' + boxname;
-        $(boxId).show();
-    }
-
-    function cancelbox(boxname) {
-    	var boxId = '#' + boxname;
-        $(boxId).hide();
-    }
-
-    function statusChange(thisform) {
-        with(thisform) {
-            if (C_CVST.substr(0, 3) === "PAG") {
-                $("#pagocapt").css('backgroundColor', "yellow");
-                $("#pagocapt2").css('backgroundColor', "yellow");
-                $("#pagocaptv").css('backgroundColor', "yellow");
-            }
-        }
-    }
-
-    function addToTels(pos, tel) {
-    	$("#C_TELE").options[pos] = new Option(tel.value, tel.value, true, true);
-    	$("#C_TELE").options[pos].css('fontWeight', "bold");
-    	$("#C_TELE").options[pos].css('backgroundColor', "green");
-    }
 });
+
+function clock() {
+    var d = new Date();
+    var tn = d.getTime();
+    var tll = new Date(tlp);
+    var tl = tll.getTime();
+    $("#timer").val(tn - tl);
+    var timenow = $("#timer").val();
+    $("#timers").val(parseInt(parseInt(timenow) / 1000) % 60);
+    $("#timerm").val(parseInt(parseInt(timenow) / 1000 / 60));
+    var timemin = $("#timerm").val();
+    if (timemin > 2) {
+        $("#clock").css('backgroundColor', 'yellow');
+    }
+    if (timemin > 4) {
+        $("#clock").css('backgroundColor', 'red');
+    }
+    if (timenow % 2 == 0) {
+        $("#clock").css('backgroundColor', 'green');
+    }
+}
+
+function openSearch() {
+    setInterval('clock()', 1000);
+}
+
+function tooLong(e) {
+    if ($("#C_OBSE1").val().length > 250) {
+    	$("#C_OBSE1").val($("#C_OBSE1").val().replace(' 	', ' '));
+    	$("#C_OBSE1").val($("#C_OBSE1").val().substr(0, 200));
+        confirm('GESTION demasiado largo');
+        $("#C_OBSE1").css('backgroundColor',"yellow");
+        return false;
+    }
+}
+
+function showsearch() {
+	$("#searchbox").show();
+    $('#find').focus();
+}
+
+function showbox(boxname) {
+	var boxId = '#' + boxname;
+    $(boxId).show();
+}
+
+function cancelbox(boxname) {
+	var boxId = '#' + boxname;
+    $(boxId).hide();
+}
+
+function statusChange(thisform) {
+    with(thisform) {
+        if (C_CVST.substr(0, 3) === "PAG") {
+            $("#pagocapt").css('backgroundColor', "yellow");
+            $("#pagocapt2").css('backgroundColor', "yellow");
+            $("#pagocaptv").css('backgroundColor', "yellow");
+        }
+    }
+}
+
+function addToTels(pos, tel) {
+	$("#C_TELE").options[pos] = new Option(tel.value, tel.value, true, true);
+	$("#C_TELE").options[pos].css('fontWeight', "bold");
+	$("#C_TELE").options[pos].css('backgroundColor', "green");
+}
 
 function npromChange(thisform) {
     with(thisform) {
