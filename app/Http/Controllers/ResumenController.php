b9671b1aae7235a16251de332a5d62b4f56489ec
Gregory Miles Blumenthal Scharf <llameadrpc@gmail.com>
2018-02-26 12:07:07 -0600

    fix numgests

--- app/Http/Controllers/ResumenController.php
+++ app/Http/Controllers/ResumenController.php
@@ -165,7 +165,6 @@ class ResumenController extends Controller
         $id_cuenta = $result['id_cuenta'];
         $tipo = auth()->user()->tipo;
         $numgest = $this->rc->getNumGests($capt);
-        dd($numgest);
         $tl = $this->rc->getTimelock($id_cuenta);
         $notas = $this->nc->notAlert($capt);
         $view = view('resumen')
