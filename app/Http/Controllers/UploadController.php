87cc7c0843e99c9b4e2a04f5ba7d66fdcf0f21b9
Gregory Miles Blumenthal Scharf <llameadrpc@gmail.com>
2018-04-24 18:04:59 -0500

    INFONAVIT Upload

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\HistoriaClass;
+use App\InfonavitClass;
+use App\UploadClass;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+class UploadController extends Controller
+{
+    /**
+     * 
+     * @var UploadClass
+     */
+    private $uc;
+
+    /**
+     * @var HistoriaClass
+     */
+    private $hc;
+
+    public function __construct()
+    {
+        $this->uc = new UploadClass();
+        $this->hc = new HistoriaClass();
+    }
+
+    /**
+     * @param Request $r
+     * @return View
+     */
+    public function update(Request $r)
+    {
+        $file = $r->file('file');
+        $ext = $file->extension();
+        $size = $file->getSize();
+        if (($ext == 'xlsx') && ($size > 0)) {
+            $filename = $file->getFilename();
+            $data = $this->uc->reader($filename);
+        } else {
+            $data = array();
+        }
+        $INFONAVIT = new InfonavitClass($data);
+        $gestion = $INFONAVIT->getGestion();
+        $this->hc->insertGestiones($gestion);
+        $view = view('INFONAVITupload');
+        return $view;
+    }
+}
