9155179ffa2019f763c7d889c3970b11f3b31a6d
Greg Blumenthal <greg@gmbs-consulting.com>
2018-10-16 20:10:15 -0500

    resumen de-jqueryficado

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,24 +1,24 @@
 function clock() {
-    var d = new Date();
-    var tn = d.getTime();
-    var tll = new Date(tlp);
-    var tl = tll.getTime();
-    var timer = $("#timer");
-    var timers = $("#timers");
-    var timerm = $("#timerm");
-    var clock = $("#clock");
+    const d = new Date();
+    const tn = d.getTime();
+    const tll = new Date(resumen.tlp);
+    const tl = tll.getTime();
+    const timer = $("#timer");
+    const timers = $("#timers");
+    const timerm = $("#timerm");
+    const clock = $("#clock");
     timer.val(tn - tl);
-    var timenow = timer.val();
+    const timenow = timer.val();
     timers.val(parseInt(parseInt(timenow) / 1000) % 60);
     timerm.val(parseInt(parseInt(timenow) / 1000 / 60));
-    var timemin = timerm.val();
+    const timemin = timerm.val();
     if (timemin > 2) {
         clock.css('backgroundColor', 'yellow');
     }
     if (timemin > 4) {
         clock.css('backgroundColor', 'red');
     }
-    var evenodd = parseInt(timenow) % 2;
+    const evenodd = parseInt(timenow) % 2;
     if (0 === evenodd) {
         clock.css('backgroundColor', 'green');
     }
