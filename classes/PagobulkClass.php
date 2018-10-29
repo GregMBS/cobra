ebdf537857ba44f5a84564cbdc5686d51ecb4e96
Greg Blumenthal <greg@gmbs-consulting.com>
2018-10-29 16:39:02 -0500

    fix pagobulk

--- classes/PagobulkClass.php
+++ classes/PagobulkClass.php
@@ -10,20 +10,20 @@ namespace cobra_salsa;
 class PagobulkClass extends BaseClass {
     
     private function buildTemp() {
-        $queryBuildTemp = "CREATE TEMPORARY TABLE pagotemp 
-                SELECT numero_de_cuenta AS 'cuenta', 
+        $select = "SELECT numero_de_cuenta AS 'cuenta', 
                 fecha_de_ultimo_pago AS 'fecha', 
                 monto_ultimo_pago AS 'monto', 
-                cliente, ejecutivo_aasignado_call_center AS 'c_cvge', 
-                1 AS 'confirmado, id_cuenta
+                cliente, ejecutivo_asignado_call_center AS 'c_cvge', 
+                1 AS 'confirmado', id_cuenta
                 FROM resumen
                 LIMIT 0";
+        $queryBuildTemp = "CREATE TEMPORARY TABLE pagotemp " . $select;
         $this->pdo->query($queryBuildTemp);        
     }
     
     private function insertPagos() {
-        $querypagoins = "INSERT IGNORE INTO pagos SELECT * FROM pagotemp";
-        $this->pdo->query($querypagoins);        
+        $query = "INSERT IGNORE INTO pagos SELECT * FROM pagotemp";
+        $this->pdo->query($query);
     }
     
     /**
@@ -31,11 +31,11 @@ class PagobulkClass extends BaseClass {
      * @return \PDOStatement
      */
     private function prepareCleanOld() {
-        $querypagoclean = "delete from pagos
+        $query = "delete from pagos
 where confirmado=0 and cuenta=:cuenta
  and fecha<=:fecha";
-        $stpc = $this->pdo->prepare($querypagoclean);
-        return $stpc;
+        $stp = $this->pdo->prepare($query);
+        return $stp;
     }
 
     /**
@@ -43,13 +43,13 @@ where confirmado=0 and cuenta=:cuenta
      * @return \PDOStatement
      */
     private function prepareAddTemp() {
-        $queryAddTemp = "INSERT INTO pagotemp 
-                SELECT :cuenta, :fecha, :monto, 
-                cliente, :c_cvge, 1 AS 'confirmado, id_cuenta
+        $select = "SELECT :cuenta, :fecha, :monto, 
+                cliente, :c_cvge, 1 AS 'confirmado', id_cuenta
                 FROM resumen
                 WHERE numero_de_cuenta = :cuenta";
-        $stpa = $this->pdo->prepare($queryAddTemp);
-        return $stpa;
+        $queryAddTemp = "INSERT INTO pagotemp " . $select;
+        $stp = $this->pdo->prepare($queryAddTemp);
+        return $stp;
     }
 
     /**
@@ -57,57 +57,58 @@ where confirmado=0 and cuenta=:cuenta
      * @return \PDOStatement
      */
     private function prepareFindGestor() {
-        $queryFindGestor = "SELECT c_cvge FROM historia "
-                . "WHERE d_fech <= :fecha "
-                . "AND cuenta = :cuenta "
-                . "AND n_prom > 0"
-                . "ORDER BY d_fech DESC, c_hrin DESC "
-                . "LIMIT 1";
-        $stpf = $this->pdo->prepare($queryFindGestor);
-        return $stpf;
+        $queryFindGestor = "SELECT c_cvge 
+FROM historia 
+WHERE d_fech <= :fecha 
+AND cuenta = :cuenta 
+AND n_prom > 0 
+ORDER BY d_fech DESC, c_hrin DESC 
+LIMIT 1";
+        $stp = $this->pdo->prepare($queryFindGestor);
+        return $stp;
     }
     
     /**
      * 
-     * @param \PDOStatement $stpc
+     * @param \PDOStatement $stp
      * @param string $cuenta
      * @param string $fecha
      */
-    private function runCleanOld($stpc, $cuenta, $fecha) {
-            $stpc->bindParam(':cuenta', $cuenta);
-            $stpc->bindParam(':fecha', $fecha);
-            $stpc->execute();
+    private function runCleanOld($stp, $cuenta, $fecha) {
+            $stp->bindValue(':cuenta', $cuenta);
+            $stp->bindValue(':fecha', $fecha);
+            $stp->execute();
     }
 
     /**
      * 
-     * @param \PDOStatement $stpa
+     * @param \PDOStatement $stp
      * @param string $cuenta
      * @param string $fecha
      * @param float $monto
      * @param string $c_cvge
      */
-    private function runAddTemp($stpa, $cuenta, $fecha, $monto, $c_cvge) {
-            $stpa->bindParam(':cuenta', $cuenta);
-            $stpa->bindParam(':fecha', $fecha);
-            $stpa->bindParam(':monto', $monto);
-            $stpa->bindParam(':c_cvge', $c_cvge);
-            $stpa->execute();
+    private function runAddTemp($stp, $cuenta, $fecha, $monto, $c_cvge) {
+            $stp->bindValue(':cuenta', $cuenta);
+            $stp->bindValue(':fecha', $fecha);
+            $stp->bindValue(':monto', $monto);
+            $stp->bindValue(':c_cvge', $c_cvge);
+            $stp->execute();
     }
 
     /**
      * 
-     * @param \PDOStatement $stpf
+     * @param \PDOStatement $stp
      * @param string $cuenta
      * @param string $fecha
      * 
      * @return array
      */
-    private function runFindGestor($stpf, $cuenta, $fecha) {
-            $stpf->bindParam(':cuenta', $cuenta);
-            $stpf->bindParam(':fecha', $fecha);
-            $stpf->execute();
-            $result = $stpf->fetch(\PDO::FETCH_ASSOC);
+    private function runFindGestor($stp, $cuenta, $fecha) {
+            $stp->bindValue(':cuenta', $cuenta);
+            $stp->bindValue(':fecha', $fecha);
+            $stp->execute();
+            $result = $stp->fetch(\PDO::FETCH_ASSOC);
             return $result;
     }
 
@@ -116,21 +117,21 @@ where confirmado=0 and cuenta=:cuenta
         $data = preg_split("/[\s,]+/", $input, 0, PREG_SPLIT_NO_EMPTY);
         $max = ceil(count($data) / 3);
         $this->buildTemp();
-        $stpc = $this->prepareCleanOld();
-        $stpa = $this->prepareAddTemp();
-        $stpf = $this->prepareFindGestor();
+        $stc = $this->prepareCleanOld();
+        $sta = $this->prepareAddTemp();
+        $stf = $this->prepareFindGestor();
         for ($i = 0; $i < $max; $i++) {
             $cuenta = $i * 3;
             $fecha = $i * 3 + 1;
             $monto = $i * 3 + 2;
-            $this->runCleanOld($stpc, $data[$cuenta], $data[$fecha]);
-            $result = $this->runFindGestor($stpf, $data[$cuenta], $data[$fecha]);
+            $this->runCleanOld($stc, $data[$cuenta], $data[$fecha]);
+            $result = $this->runFindGestor($stf, $data[$cuenta], $data[$fecha]);
             if ($result) {
                 $c_cvge = $result['c_cvge'];
             } else {
                 $c_cvge = '';
             }
-            $this->runAddTemp($stpa, $data[$cuenta], $data[$fecha], $data[$monto], $data[$c_cvge]);
+            $this->runAddTemp($sta, $data[$cuenta], $data[$fecha], $data[$monto], $data[$c_cvge]);
         }
         $this->insertPagos();
     }
