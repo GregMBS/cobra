b8cfac88f06ab1b1db9717cfe53092807ebb6d38
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-23 17:34:41 -0500

    fix pagos

--- classes/PagosClass.php
+++ classes/PagosClass.php
@@ -7,6 +7,7 @@ use PDOStatement;
 
 require_once __DIR__ . '/ResumenObject.php';
 require_once __DIR__ . '/PagosObject.php';
+require_once __DIR__ . '/HistoriaObject.php';
 
 /**
  * Description of PagosClass
