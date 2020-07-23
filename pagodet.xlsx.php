fb6ea7c5fccee86b8bf47feaa537849b848586ca
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-23 15:54:01 -0500

    pagodet.xlsx.php

--- pagodet.xlsx.php
+++ pagodet.xlsx.php
@@ -1,15 +1,17 @@
 <?php
-require_once 'vendor/autoload.php';
 
+use cobra_salsa\OutputClass;
 use cobra_salsa\PdoClass;
 use cobra_salsa\PagosClass;
-use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 
-require_once 'classes/PdoClass.php';
-require_once 'classes/PagosClass.php';
+require_once __DIR__ . '/classes/PdoClass.php';
+require_once __DIR__ . '/classes/PagosClass.php';
+require_once __DIR__ . '/classes/OutputClass.php';
+
 $pd      = new PdoClass();
 $pdo       = $pd->dbConnectAdmin();
 $pc = new PagosClass($pdo);
+$oc = new OutputClass();
 $capt      = filter_input(INPUT_GET, 'capt');
 $go        = filter_input(INPUT_GET, 'go');
 $thismonth = strftime("%B %Y");
@@ -22,7 +24,8 @@ foreach ($result as $row) {
     $row['monto'] = (float) $row['monto'];
     $output[]     = $row;
 }
-$writer = WriterEntityFactory::createXLSXWriter();
-$writer->openToBrowser($filename); // stream data directly to the browser
-$writer->addRows($output); // add multiple rows at a time
-$writer->close();
+try {
+    $oc->writeXLSXFile($filename, $output);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
