fd99e2e73a29898d6dd1b21b1bd9f204ca32567d
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-23 17:39:21 -0500

    fix pagos ant

--- pagodetant.xlsx.php
+++ pagodetant.xlsx.php
@@ -1,15 +1,18 @@
 <?php
 require_once 'vendor/autoload.php';
 
-use cobra_salsa\PdoClass;
+use cobra_salsa\OutputClass;
 use cobra_salsa\PagosClass;
-use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use cobra_salsa\PdoClass;
 
-require_once 'classes/PdoClass.php';
-require_once 'classes/PagosClass.php';
+require_once __DIR__ . '/classes/PdoClass.php';
+require_once __DIR__ . '/classes/PagosClass.php';
+require_once __DIR__ . '/classes/OutputClass.php';
+
 $pd      = new PdoClass();
 $pdo       = $pd->dbConnectAdmin();
 $pc = new PagosClass($pdo);
+$oc = new OutputClass();
 $capt      = filter_input(INPUT_GET, 'capt');
 $go        = filter_input(INPUT_GET, 'go');
 $thismonth = strftime("%B %Y");
@@ -22,7 +25,8 @@ foreach ($result as $row) {
     $row['monto'] = (float) $row['monto'];
     $output[]     = $row;
 }
-$writer = WriterEntityFactory::createXLSXWriter();
-$writer->openToBrowser($filename); // stream data directly to the browser
-$writer->addRows($output); // add multiple rows at a time
-$writer->close();
+try {
+    $oc->writeXLSXFile($filename, $output);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
