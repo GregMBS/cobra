8fcce458253bf7197b546e0f7e00913a468260b9
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-23 08:43:49 -0500

    speclist weekly

--- classes/SpeclistqcClass.php
+++ classes/SpeclistqcClass.php
@@ -1,14 +1,10 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace cobra_salsa;
 
 use PDO;
+
+require_once __DIR__ . '/ResumenObject.php';
 
 /**
  * Description of SpeclistqcClass
