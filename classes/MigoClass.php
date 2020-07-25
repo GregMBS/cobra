487b8efefbdfd2cc2d0477c311519d0cc1f27eb0
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-25 18:01:19 -0500

    fix migo memory

--- classes/MigoClass.php
+++ classes/MigoClass.php
@@ -155,8 +155,6 @@ and ejecutivo_asignado_call_center = :capt
             $stmt->bindValue(':limit', (int)$row, PDO::PARAM_INT);
             $stmt->bindValue(':offset', (int)$rowPerPage, PDO::PARAM_INT);
             $stmt->execute();
-            var_dump($row, $rowPerPage);
-            die();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (PDOException $p) {
             var_dump($p);
