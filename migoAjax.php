c3798010d5fed5f5fff7abb70a9064a46a08d9ed
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-25 15:53:17 -0500

    fix migo memory

--- migoAjax.php
+++ migoAjax.php
@@ -1,7 +1,7 @@
 <?php
 
-use cobra_salsa\PdoClass;
 use cobra_salsa\MigoClass;
+use cobra_salsa\PdoClass;
 use cobra_salsa\ResumenObject;
 
 require_once 'classes/PdoClass.php';
@@ -12,14 +12,26 @@ $pdo = $pd->dbConnectUser();
 $mc = new MigoClass($pdo);
 $capt = $pd->capt;
 $tipo = $pd->tipo;
+$keys = [
+    'numero_de_cuenta',
+    'nombre_deudor',
+    'cliente',
+    'status_de_credito',
+    'saldo_total',
+    'saldo_descuento_2',
+    'saldo_aarsa',
+    'fecha_ultima_gestion'
+];
 if ($tipo == 'admin') {
     $main = $mc->adminReport();
     while ($row = $main->fetchObject(ResumenObject::class)) {
-        echo json_encode($row);
+        $data = array_values(array_intersect_key($row, $keys));
+        echo json_encode($data);
     }
 } else {
     $main = $mc->userReport($capt);
     while ($row = $main->fetchObject(ResumenObject::class)) {
-        echo json_encode($row);
+        $data = array_values(array_intersect_key($row, $keys));
+        echo json_encode($data);
     }
 }
