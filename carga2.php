5e745c2fab10a312876fb2398b9c9eef270621e6
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-27 17:50:20 -0500

    carga work

--- carga2.php
+++ carga2.php
@@ -21,7 +21,7 @@ $cc = new CargaClass($pc->dbConnectAdmin());
         <button onclick="window.location = 'reports.php?capt=<?php echo $capt; ?>'">Regresar a la plantilla administrativa</button><br>
         <form action="carga2.php" method="post" enctype="multipart/form-data" name="cargar">
             <p>Filename:
-                <input type="hidden" name="capt" id="capt" value="<?php echo $capt ?>" />
+                <input type="hidden" name="capt" id="capt" value="<?php echo $pd->capt ?>" />
                 <input type="file" name="file" id="file"><br>
                 <button type="submit" name="go" value="cargar">Elegir archivo</button>
             </p>
