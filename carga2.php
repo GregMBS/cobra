9dec81bcae25211d1c29dbd77933a1c429de5895
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-28 10:13:06 -0500

    missing asociar

--- carga2.php
+++ carga2.php
@@ -78,7 +78,7 @@ case 'asociar':
 case 'clientePick':
 ?>
 <p>
-<form action="carga2.php" method="post" name="assoc" onclick="alert(this);">
+<form action="carga2.php" method="post" name="assoc" id="aForm" onclick="alert(this);">
     <?php
     list($cliente, $post, $fecha_de_actualizacion, $filename, $handle, $data, $num) = $cc->clientePick($post);
     ?>
