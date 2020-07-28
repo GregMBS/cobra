e7ef93a543dad4c6869385001576e0845dc8ca89
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-28 10:32:02 -0500

    missing asociar

--- carga2.php
+++ carga2.php
@@ -35,126 +35,124 @@ $go = $post['go'];
 
 switch ($go) {
 
-case 'cargar':
-    if ($_FILES["file"]["error"] > 0) {
-        echo "<p>Error: " . $_FILES["file"]["error"] . "</p>";
-    } else {
-        echo "<p>Upload: " . $_FILES["file"]["name"] . "<br>";
-        echo "Type: " . $_FILES["file"]["type"] . "<br>";
-        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br>";
-        echo "Stored in: " . $_FILES["file"]["tmp_name"];
-        $destination = $cc->moveLoadedFile();
-        echo "Stored in: " . $destination . "</p>";
-        ?>
-        <p>
-        <form action="carga2.php" method="post" name="clientePick">
-            <table>
-                <tr>
-                    <td><label for="pickCliente">Client</label></td>
-                    <td><input type="text" name="cliente" id="pickCliente"/>
-                        <input type="hidden" name="filename" value="<?php
-                        echo $destination;
-                        ?>"/>
-                        <input type="hidden" name="capt" value="<?php
-                        echo $capt;
-                        ?>"/>
-                    </td>
-                </tr>
-            </table>
-            <button type="submit" name="go" value="clientePick">Elegir cliente</button>
-        </form>
-        <?php
-    }
-    break;
-
-case 'asociar':
-    try {
-        $cc->asociar($post);
-    } catch (Exception $e) {
-        die($e->getMessage());
-    }
-    break;
-
-case 'clientePick':
-?>
-<p>
-<form action="carga2.php" method="post" name="assoc" id="aForm">
-    <?php
-    list($cliente, $post, $fecha_de_actualizacion, $filename, $handle, $data, $num) = $cc->clientePick($post);
-    ?>
-    <input name="cliente" type="hidden" value="<?php
-    echo $cliente;
-    ?>"/>
-    <input name="fecha_de_actualizacion" type="hidden" value="<?php
-    echo $fecha_de_actualizacion;
-    ?>"/>
-    <input type="hidden" name="filename" value='<?php
-    echo $filename;
-    ?>'/>
-    <input type="hidden" name="capt" value="<?php
-    echo $capt;
-    ?>"/>
-</form>
-
-<table>
-    <?php
-    $cargadexCount = $cc->countCargadex($cliente);
-
-    for ($c = 0; $c < $num; $c++) {
-
-        if (!empty($data[$c])) {
-            ?>
-            <tr>
-                <td><label for="pos<?php
-                    echo $c
-                    ?>"><?php
-                        echo trim($data[$c])
-                        ?></label></td>
-                <td>
-                    <select form="assoc" name="pos<?php
-                    echo $c
-                    ?>" id="pos<?php
-                    echo $c
-                    ?>">
-                        <option value='nousar<?php echo $c ?>'>no usar</option>
-                        <?php
-                        $columns = $cc->getDBColumnNames();
-                        $k = count($columns);
-
-                        foreach ($columns as $k => $column) {
-                            ?>
-                            <option value='<?php echo $k ?>'<?php
-                            if (trim($data[$c]) == $column) {
-                                echo " selected='selected'";
-                            }
-                            ?>><?php echo $column; ?></option>
-                            <?php
-                        }
-                        ?>
-                    </select></td>
-            </tr>
-            <?php
+    case 'cargar':
+        if ($_FILES["file"]["error"] > 0) {
+            echo "<p>Error: " . $_FILES["file"]["error"] . "</p>";
         } else {
+            echo "<p>Upload: " . $_FILES["file"]["name"] . "<br>";
+            echo "Type: " . $_FILES["file"]["type"] . "<br>";
+            echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br>";
+            echo "Stored in: " . $_FILES["file"]["tmp_name"];
+            $destination = $cc->moveLoadedFile();
+            echo "Stored in: " . $destination . "</p>";
             ?>
-            <input form="assoc" type="hidden" value="nousar" name="pos<?php
-            echo $c
-            ?>"/>
+            <p>
+            <form action="carga2.php" method="post" name="clientePick">
+                <table>
+                    <tr>
+                        <td><label for="pickCliente">Client</label></td>
+                        <td><input type="text" name="cliente" id="pickCliente"/>
+                            <input type="hidden" name="filename" value="<?php
+                            echo $destination;
+                            ?>"/>
+                            <input type="hidden" name="capt" value="<?php
+                            echo $capt;
+                            ?>"/>
+                        </td>
+                    </tr>
+                </table>
+                <button type="submit" name="go" value="clientePick">Elegir cliente</button>
+            </form>
             <?php
         }
-    }
+        break;
 
-    fclose($handle);
-    ?>
-</table>
-    <p>
-        <input form="assoc" type="hidden" name="maxc" value="<?php echo $c ?>"/>
-        <input form="assoc" type="hidden" name="capt" value="<?php echo $capt ?>"/>
-        <input form="assoc" type="submit" name="go" id="aSubmit" value="asociar"/>
-    </p>
-    <?php
-    break;
-    }
+    case 'asociar':
+        try {
+            $cc->asociar($post);
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+        break;
 
-    ?>
+    case 'clientePick':
+        ?>
+        <p>
+        <form action="carga2.php" method="post" name="assoc" id="aForm">
+            <?php
+            list($cliente, $post, $fecha_de_actualizacion, $filename, $handle, $data, $num) = $cc->clientePick($post);
+            ?>
+            <input name="cliente" type="hidden" value="<?php
+            echo $cliente;
+            ?>"/>
+            <input name="fecha_de_actualizacion" type="hidden" value="<?php
+            echo $fecha_de_actualizacion;
+            ?>"/>
+            <input type="hidden" name="filename" value='<?php
+            echo $filename;
+            ?>'/>
+            <input type="hidden" name="capt" value="<?php
+            echo $capt;
+            ?>"/>
+            <input type="hidden" name="maxc" value="<?php echo $c ?>"/>
+            <input type="hidden" name="capt" value="<?php echo $capt ?>"/>
+            <input type="submit" name="go" id="aSubmit" value="asociar"/>
+        </form>
+
+        <table>
+            <?php
+            $cargadexCount = $cc->countCargadex($cliente);
+
+            for ($c = 0; $c < $num; $c++) {
+
+                if (!empty($data[$c])) {
+                    ?>
+                    <tr>
+                        <td><label for="pos<?php
+                            echo $c
+                            ?>"><?php
+                                echo trim($data[$c])
+                                ?></label></td>
+                        <td>
+                            <select form="assoc" name="pos<?php
+                            echo $c
+                            ?>" id="pos<?php
+                            echo $c
+                            ?>">
+                                <option value='nousar<?php echo $c ?>'>no usar</option>
+                                <?php
+                                $columns = $cc->getDBColumnNames();
+                                $k = count($columns);
+
+                                foreach ($columns as $k => $column) {
+                                    ?>
+                                    <option value='<?php echo $k ?>'<?php
+                                    if (trim($data[$c]) == $column) {
+                                        echo " selected='selected'";
+                                    }
+                                    ?>><?php echo $column; ?></option>
+                                    <?php
+                                }
+                                ?>
+                            </select></td>
+                    </tr>
+                    <?php
+                } else {
+                    ?>
+                    <input form="assoc" type="hidden" value="nousar" name="pos<?php
+                    echo $c
+                    ?>"/>
+                    <?php
+                }
+            }
+
+            fclose($handle);
+            ?>
+        </table>
+        <?php
+        break;
+}
+
+?>
 </body>
 </html>
