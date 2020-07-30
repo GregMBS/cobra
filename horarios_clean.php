3e3f5e6a45334a27f0ebc7837bc7fa39c4efb6d8
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-30 12:56:44 -0500

    deal with nulls

--- horarios_clean.php
+++ horarios_clean.php
@@ -3,6 +3,7 @@
 use cobra_salsa\PdoClass;
 use cobra_salsa\HorariosClass;
 use cobra_salsa\HorariosAllClass;
+use cobra_salsa\TimesheetViewClass;
 
 require_once 'classes/PdoClass.php';
 $pc              = new PdoClass();
@@ -11,6 +12,8 @@ require_once 'classes/HorariosClass.php';
 $hc              = new HorariosClass($pdo);
 require_once 'classes/HorariosAllClass.php';
 $hac             = new HorariosAllClass($pdo);
+require_once __DIR__ . '/classes/TimesheetViewClass.php';
+$tv = new TimesheetViewClass();
 $yr              = date('Y');
 $mes             = date('m');
 $dhoy            = date('d');
