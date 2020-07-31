c239b295b790684a63764280b64e51e8d3765a88
Greg Blumenthal <greg@gmbs-consulting.com>
2020-07-31 17:28:21 -0500

    nomina missing

--- horarios_single.php
+++ horarios_single.php
@@ -1,36 +1,31 @@
 <?php
 
-use cobra_salsa\PdoClass;
-use cobra_salsa\HorariosClass;
 use cobra_salsa\HorariosAllClass;
+use cobra_salsa\HorariosClass;
+use cobra_salsa\PdoClass;
 use cobra_salsa\TimesheetViewClass;
 
 require_once 'classes/PdoClass.php';
-$pd              = new PdoClass();
-$pdo             = $pd->dbConnectAdmin();
+$pd = new PdoClass();
+$pdo = $pd->dbConnectAdmin();
 require_once 'classes/HorariosClass.php';
-$hc              = new HorariosClass($pdo);
+$hc = new HorariosClass($pdo);
 require_once 'classes/HorariosAllClass.php';
-$hac             = new HorariosAllClass($pdo);
+$hac = new HorariosAllClass($pdo);
 require_once __DIR__ . '/classes/TimesheetViewClass.php';
 $tv = new TimesheetViewClass();
-$yr              = date('Y');
-$mes             = date('m');
-$dhoy            = date('d');
-$hoy             = date('Y-m-d');
-$capt            = $pd->capt;
+$yr = date('Y');
+$mes = date('m');
+$dhoy = date('d');
+$hoy = date('Y-m-d');
+$capt = $pd->capt;
 $gestores = [];
 $gestor = filter_input(INPUT_GET, 'gestor');
+$gestores = $hc->listGestores();
 if (!empty($gestor)) {
-    $gestores[] = ['c_cvge' => $gestor];
-} else {
-    $gestores = $hc->listGestores();
-}
-$sheet = [];
-$sum = [];
-foreach ($gestores as $name) {
-    $nombre = $name['c_cvge'];
-    $sheet[$nombre] = $hc->prepareSheet($hc, $nombre, $dhoy);
-    $sum[$nombre] = $hc->prepareMonthSum($sheet[$nombre]);
+    $sheet = [];
+    $sum = [];
+    $sheet[$gestor] = $hc->prepareSheet($hc, $gestor, $dhoy);
+    $sum[$gestor] = $hc->prepareMonthSum($sheet[$gestor]);
 }
 require_once __DIR__ . '/views/horarios2View.php';
