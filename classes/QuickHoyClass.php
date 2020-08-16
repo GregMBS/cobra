f912f0560f1061a8d04b4e903a748eebe63e94ae
Greg Blumenthal <greg@gmbs-consulting.com>
2020-08-16 10:41:13 -0500

    emergency surgery

--- classes/QuickHoyClass.php
+++ classes/QuickHoyClass.php
@@ -43,7 +43,19 @@ class QuickHoyClass
     protected $updateHoyBreaktemp = "update hoy, breaktemp
         set Break_min = sum_diff
         where hoy.gestor = breaktemp.gestor";
-    protected $queryHoy = "SELECT * FROM hoy";
+    protected $queryHoy = "select c_cvge as 'gestor',
+        time_to_sec(subtime(max(c_hrin),min(c_hrin)))/3600 as 'Horas',
+        count(1) as 'Gestiones',
+        sum(C_CVST like 'PRO% DE%') as 'Promesas_Hoy',
+        sum(C_CVST like 'CLIENTE NEG%') as 'Negociaciones,
+        count(1)/time_to_sec(subtime(max(c_hrin),min(c_hrin)))*3600 as 'Gestiones_por_hora',
+        sum(n_prom) as 'Monto_Promesas_Hoy'
+        from historia
+        where D_FECH = curdate()
+        and c_cont>0
+        and c_msge is null
+        and c_cniv is null
+        group by C_CVGE order by c_cvge";
 
     public function __construct($pdo)
     {
