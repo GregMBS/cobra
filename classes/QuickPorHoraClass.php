ae731f12b715497ddc88e7030e54b5deb5c633fc
Greg Blumenthal <greg@gmbs-consulting.com>
2020-08-16 11:36:12 -0500

    emergency surgery

--- classes/QuickPorHoraClass.php
+++ classes/QuickPorHoraClass.php
@@ -32,7 +32,18 @@ and c_cont>0
 and c_msge is null
 and c_cniv is null
 group by C_CVGE;";
-    protected $queryPorHora  = "SELECT * FROM porhora;";
+    protected $queryPorHora  = "select c_cvge as 'gestor', 
+    sum((C_CARG is not null)&&(C_CARG<>'')) as 'contactos',
+count(1)as 'gestiones', 
+sum(n_prom>0) as 'promesas',
+concat(round(sum((C_CARG is not null)&&(C_CARG<>''))/count(1)*100),'%') as 'porciento'
+from historia
+where D_FECH = curdate()
+and C_HRIN > hour(now()) - 1
+and c_cont>0
+and c_msge is null
+and c_cniv is null
+group by C_CVGE";
 
     public function __construct($pdo)
     {
@@ -44,8 +55,8 @@ group by C_CVGE;";
      */
     public function getPorHora()
     {
-        $this->pdo->query($this->createPorHora);
-        $this->pdo->query($this->insertPorHora);
+//        $this->pdo->query($this->createPorHora);
+//        $this->pdo->query($this->insertPorHora);
         $sta    = $this->pdo->query($this->queryPorHora);
         return $sta->fetchAll(PDO::FETCH_ASSOC);
     }
