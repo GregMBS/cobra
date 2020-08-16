661a3ea95b9f4f1bfc13499cc2469b4d1dff0d1f
Greg Blumenthal <greg@gmbs-consulting.com>
2020-08-16 12:33:57 -0500

    emergency surgery

--- classes/QuickHoyClass.php
+++ classes/QuickHoyClass.php
@@ -44,11 +44,11 @@ class QuickHoyClass
         set Break_min = sum_diff
         where hoy.gestor = breaktemp.gestor";
     protected $queryHoy = "select c_cvge as 'gestor',
-        time_to_sec(subtime(max(c_hrin),min(c_hrin)))/3600 as 'Horas',
+        time_to_sec(subtime(max(c_hrfi),min(c_hrin)))/3600 as 'Horas',
         count(1) as 'Gestiones',
         sum(C_CVST like 'PRO% DE%') as 'Promesas_Hoy',
         sum(C_CVST like 'CLIENTE NEG%') as 'Negociaciones',
-        count(1)/time_to_sec(subtime(max(c_hrin),min(c_hrin)))*3600 as 'Gestiones_por_hora',
+        count(1)/time_to_sec(subtime(max(c_hrfi),min(c_hrin)))*3600 as 'Gestiones_por_hora',
         sum(n_prom) as 'Monto_Promesas_Hoy'
         from historia
         where D_FECH = curdate()
