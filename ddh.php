6c8c29a238bd88c7994e29c64883e1dea29caf9c
Greg Blumenthal <greg@gmbs-consulting.com>
2020-08-18 16:19:46 -0500

    dh fail

--- ddh.php
+++ ddh.php
@@ -12,4 +12,8 @@ $capt = $pd->capt;
 $gestor = filter_input(INPUT_GET, 'gestor');
 $fecha = filter_input(INPUT_GET, 'fecha');
 $result = $dc->getDhMain($gestor, $fecha);
+$sum = 0;
+foreach($result as $item){
+    $sum += $item->n_prom;
+}
 require_once 'views/dhView.php';
