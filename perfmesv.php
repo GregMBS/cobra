165e97acbaf36e8c54f5b1fab96701d2cf08d9c1
Greg Blumenthal <greg@gmbs-consulting.com>
2020-09-22 13:13:07 -0500

    visitadores last month

--- perfmesv.php
+++ perfmesv.php
@@ -3,6 +3,7 @@
 use cobra_salsa\PdoClass;
 use cobra_salsa\PerfmesAllClass;
 use cobra_salsa\PerfmesClass;
+use cobra_salsa\TimesheetViewClass;
 
 $day_esp = array("DOM", "LUN", "MAR", "MIE", "JUE", "VIE", "SAB");
 require_once 'classes/PdoClass.php';
@@ -12,6 +13,8 @@ require_once 'classes/PerfmesClass.php';
 $hc = new PerfmesClass($pdo);
 require_once 'classes/PerfmesAllClass.php';
 $hac = new PerfmesAllClass($pdo);
+require_once __DIR__ . '/classes/TimesheetViewClass.php';
+$tv = new TimesheetViewClass(FALSE);
 $yr = date('Y', strtotime('last day of previous month'));
 $mes = date('m', strtotime('last day of previous month'));
 $dhoy = date('d', strtotime('last day of previous month'));
