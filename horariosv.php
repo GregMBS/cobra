993d45fb724217f17b7f05b1f474106c78549d4e
Greg Blumenthal <greg@gmbs-consulting.com>
2020-09-21 20:14:24 -0500

    no visitadores

--- horariosv.php
+++ horariosv.php
@@ -6,7 +6,7 @@ $sheet = [];
 $sum = [];
 var_dump($visitadores);
 foreach ($visitadores as $gestor) {
-    if ($gestor) {
+    if (!empty($gestor)) {
         print_r($gestor);
         $mySheet = $hc->prepareVisitSheet($hc, $gestor, $dhoy);
         var_dump($mySheet);
@@ -14,8 +14,6 @@ foreach ($visitadores as $gestor) {
         $mySum = $hc->prepareMonthSum($sheet[$gestor]);
         $sheet[$gestor] = $mySheet;
         $sum[$gestor] = $mySum;
-//    $sheet[$gestor] = [];
-//    $sum[$gestor] = [];
     }
 }
 require_once __DIR__ . '/views/visitadorTimesheetView.php';
