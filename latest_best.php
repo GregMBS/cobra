0101e3ab39dd84151be0961c82a966c3a14892d3
Greg Blumenthal <greg@gmbs-consulting.com>
2022-10-15 15:40:16 -0500

    best big speedup gamble

--- latest_best.php
+++ latest_best.php
@@ -12,32 +12,11 @@ use cobra_salsa\ResumenObject;
 require_once 'classes/PdoClass.php';
 $pc = new PdoClass();
 $pdo = $pc->dbConnectAdmin();
-$pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
 require_once 'classes/BestClass.php';
 $bc = new BestClass($pdo);
-$fields = [
-    'ejecutivo_asignado_call_center',
-    'numero_de_cuenta',
-    'nombre_deudor',
-    'cliente',
-    'status_de_credito',
-    'id_cuenta',
-    'saldo_total',
-    'saldo_descuento_1',
-    'saldo_descuento_2',
-    'fecha_ultima',
-    'hora_ultima',
-    'producto',
-    'subproducto',
-    'status_aarsa',
-    'tel_1',
-    'tel_2',
-    'fecha_de_ultimo_pago',
-    'monto_ultimo_pago'
-];
 $filename = "Ultimo_y_mejor_" . date('ymd') . ".xlsx";
-$output = array();
-$i = 1;
+$bc->createBestTemp();
+$bc->createLastBest();
 $stq = $bc->getLastBest();
 $output = $stq->fetchAll(PDO::FETCH_ASSOC);
 $count = count($output);
