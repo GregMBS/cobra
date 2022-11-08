9d9292a804645b6062d82ee7af221451c5496e54
Greg Blumenthal <greg@gmbs-consulting.com>
2022-11-08 11:09:56 -0600

    carteritas pretty

--- carteritas.php
+++ carteritas.php
@@ -15,7 +15,40 @@ $post = filter_input_array(INPUT_POST);
 $get = filter_input_array(INPUT_GET);
 $ic = new InputClass();
 $cc = new CarteritasClass($pdo);
-?>
+$go = '';
+$count = 0;
+if (!empty($post)) {
+    if (array_key_exists('go', $post)) {
+        $go = $post['go'];
+    }
+}
+if ($go == 'cargar') {
+    if ($_FILES["file"]["error"] > 0) {
+        echo "<p>Error: " . $_FILES["file"]["error"] . "</p>";
+    }
+    if ($_FILES["file"]["error"] == 0) {
+        $filename = $cc->moveLoadedFile();
+        try {
+            $data = $ic->readXLSXFile($filename);
+            $loadVisitas = $cc->loadVisitas($data);
+            $fixVisitas = $cc->fix_visitas;
+            $fixTels = $cc->fix_tels;
+            $fixProms = $cc->fix_proms;
+            $sql = $pdo->prepare($loadVisitas);
+            $sql->execute();
+            $count = $sql->rowCount();
+            $sqv = $pdo->prepare($fixVisitas);
+            $sqv->execute();
+            $sqt = $pdo->prepare($fixTels);
+            $sqt->execute();
+            $sqp = $pdo->prepare($fixProms);
+            $sqp->execute();
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
+        }
+    }
+}
+    ?>
     <!DOCTYPE HTML>
 
     <html lang="es">
@@ -26,6 +59,7 @@ $cc = new CarteritasClass($pdo);
 <button onclick="window.location = 'reports.php?capt=<?php echo $capt; ?>'">Regresar a la pagina administrativa
 </button>
 <br>
+<h1>COBRA Carga de Gestiones</h1>
 <form action="carteritas.php" method="post" enctype="multipart/form-data" name="cargar">
     <p>Filename:
         <input type="hidden" name="capt" id="capt" value="<?php echo $capt ?>"/>
@@ -34,32 +68,11 @@ $cc = new CarteritasClass($pdo);
     </p>
 </form>
 <?php
-$go = '';
-if (!empty($post)) {
-    if (array_key_exists('go', $post)) {
-        $go = $post['go'];
-    }
+if ($count > 0) {
+?>
+    <h2><?php echo $count; ?> gestiones cargadas</h2>
+<?php
 }
-if ($go == 'cargar') if ($_FILES["file"]["error"] > 0) {
-    echo "<p>Error: " . $_FILES["file"]["error"] . "</p>";
-} else {
-    $filename = $cc->moveLoadedFile();
-    try {
-        $data = $ic->readXLSXFile($filename);
-        $loadVisitas = $cc->loadVisitas($data);
-        $fixVisitas = $cc->fix_visitas;
-        $fixTels = $cc->fix_tels;
-        $fixProms = $cc->fix_proms;
-        $sql = $pdo->prepare($loadVisitas);
-        $sql->execute();
-        $sqv = $pdo->prepare($fixVisitas);
-        $sqv->execute();
-        $sqt = $pdo->prepare($fixTels);
-        $sqt->execute();
-        $sqp = $pdo->prepare($fixProms);
-        $sqp->execute();
-    } catch (Exception $e) {
-        var_dump($e->getMessage());
-    }
-}
-
+?>
+</body>
+</html>
