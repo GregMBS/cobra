9ba72cf2ce60e996f916d4bd0826dd71a59986cc
Greg Blumenthal <greg@gmbs-consulting.com>
2022-11-16 13:47:50 -0600

    Reader not open error

--- classes/InputClass.php
+++ classes/InputClass.php
@@ -4,6 +4,10 @@ namespace cobra_salsa;
 
 use OpenSpout\Reader\Common\Creator\ReaderEntityFactory;
 use Exception;
+use OpenSpout\Reader\XLSX\Reader;
+use OpenSpout\Reader\XLSX\RowIterator;
+use OpenSpout\Reader\XLSX\Sheet;
+use OpenSpout\Common\Entity\Cell;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -24,14 +28,19 @@ class InputClass
     public function readXLSXFile(string $filename): array
     {
         try {
-            $reader = ReaderEntityFactory::createReaderFromFile($filename);
+            /** @var Reader $reader */
+            $reader = ReaderEntityFactory::createXLSXReader();
+            $reader->open($filename);
             $output = [];
+            /** @var Sheet $sheet */
             foreach ($reader->getSheetIterator() as $sheet) {
                 if ($sheet->getIndex() === 0) { // index is 0-based
                     $rowIndex = 0;
+                    /** @var RowIterator $row */
                     foreach ($sheet->getRowIterator() as $row) {
                         $rowOut = [];
                         if ($rowIndex !== 0) {
+                            /** @var Cell[] $cells */
                             $cells = $row->getCells();
                             foreach ($cells as $cell) {
                                 $rowOut[] = $cell->getValue();
