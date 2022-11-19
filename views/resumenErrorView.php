8739678ee03da534d375f52052885fb7a7fb69f5
Greg Blumenthal <greg@gmbs-consulting.com>
2022-11-19 12:24:42 -0600

    fix flagmsg

--- views/resumenErrorView.php
+++ views/resumenErrorView.php
@@ -1,17 +1,10 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-    <html lang="es">
-        <HEAD>
-            <TITLE>Error de capturar gestion</TITLE>
-        </HEAD>
-        <BODY>
-            <h2><?php echo $flagMsg; ?></h2>
-            <a href="/resumen.php?capt=<?php
-            echo $capt;
-            ?>&field=id_cuenta&find=<?php echo $C_CONT; ?>">Regresa para arreglarlo</a>
-        </BODY>
-    </HTML>
+<html lang="es">
+<head>
+    <TITLE>Error de capturar gestion</TITLE>
+</head>
+<body>
+    <h2><?php echo $flagMsg; ?></h2>
+    <a href="/resumen.php?capt=<?php echo $capt;?>&field=id_cuenta&find=<?php echo $C_CONT; ?>">Regresa para arreglarlo</a>
+</body>
+</html>
