59dbd7c9d86f17d4b650407fd1079b8cb2a69f96
Greg Blumenthal <greg@gmbs-consulting.com>
2022-11-20 09:40:27 -0600

    move stupid

--- views/resumenErrorView.php
+++ views/resumenErrorView.php
@@ -1,12 +1,27 @@
+<?php 
+/**
+ * @var string $flagMsg
+ * @var string $capt
+ * @var int|null $C_CONT
+ */
+?>
 <!DOCTYPE html>
 <html lang="es">
-<HEAD>
-    <TITLE>Error de capturar gestion</TITLE>
-</HEAD>
-<BODY>
-<h2><?php echo $flagMsg; ?></h2>
-<a href="/resumen.php?capt=<?php
-echo $capt;
-?>&field=id_cuenta&find=<?php echo $C_CONT; ?>">Regresa para arreglarlo</a>
-</BODY>
+    <HEAD>
+        <TITLE>Error de capturar gestion</TITLE>
+    </HEAD>
+    <BODY>
+        <h2><?php echo $flagMsg; ?></h2>
+        <?php
+        if (isset($C_CONT)) {
+            ?>
+            <a href="../resumen.php?capt=<?php echo $capt; ?>&field=id_cuenta&find=<?php echo $C_CONT; ?>">Regresa para arreglarlo</a>
+            <?php
+        } else {
+            ?>
+            <a href="../index.php">Favor de LOGIN.</a>
+            <?php
+        }
+        ?>
+    </BODY>
 </HTML>
