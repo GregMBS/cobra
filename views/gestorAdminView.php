d2aef88580a275290c3881d14e219ce2674f4f6f
Gregory M. Blumenthal <greg@gmbs-consulting.com>
2025-06-04 11:50:12 +0200

    more Bootstrap, less jQuery

--- views/gestorAdminView.php
+++ views/gestorAdminView.php
@@ -2,118 +2,88 @@
 <html lang="es">
     <head>
         <title>Administraci&oacute;n de las cuentas de los gestores</title>
-        <title>CobraMas - Cambio de Status</title>
-        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.0/themes/redmond/jquery-ui.css" type="text/css" media="all" />
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
-        <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js" type="text/javascript"></script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
         <style>
-            tr:hover {background-color: yellow;}
+            tr:hover {background-color: #fff9c4;}
         </style>
     </head>
     <body>
-        <button onclick="window.location = 'reports.php?capt=<?php echo $capt; ?>'">Regresar a la pagina administrativa</button><br>
-        <table class="ui-widget">
-            <thead class="ui-widget-header">
-                <tr>
-                <th>Gestor</th>
-                <th>Completo</th>
-                <th>Contrase&ntilde;a</th>
-                <th>Tipo</th>
-                </tr>
-                </thead>
-                <tbody class="ui-widget-content">
-                    <?php
-                    $j = 0;
-
-                    foreach ($result as $row) {
-                        $j++; 
-                        $usuaria = $row['USUARIA'];
-                        $completo = $row['COMPLETO'];
-                        $tipo = $row['TIPO'];
-                        $camp = $row['CAMP'];
-                        $gestor = $row['INICIALES'];
-                        $passw = $row['PASSW'];
+        <div class="container py-4">
+            <button class="btn btn-secondary mb-3" onclick="window.location = 'reports.php?capt=<?php echo $capt; ?>'">Regresar a la pagina administrativa</button>
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover align-middle">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Gestor</th>
+                            <th>Completo</th>
+                            <th>Contrase&ntilde;a</th>
+                            <th>Tipo</th>
+                            <th colspan="2">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $j = 0;
+                        foreach ($result as $row) {
+                            $j++;
+                            $usuaria = $row['USUARIA'];
+                            $completo = mb_convert_encoding($row['COMPLETO'], 'Windows-1252', 'UTF-8');
+                            $tipo = $row['TIPO'];
+                            $camp = $row['CAMP'];
+                            $gestor = $row['INICIALES'];
+                            $passw = $row['PASSW'];
                         ?>
                         <tr>
-
-                        <td><form class="gestorChange"
-                                  name="gestorChange<?php echo $j; ?>"
-                                  method="get"
-                                  action="/gestorAdmin.php"
-                                  id="gestorChange<?php echo $j; ?>">
-                                <input type="hidden" name="capt" value="<?php echo $capt ?>">
-                                <input type="hidden" name="j" value="<?php echo $j ?>">
-                                <label><input type="text" name="usuaria" readonly="readonly" value="<?php echo $usuaria; ?>" /></label>
-                            </form>
-                        </td>
-                        <td><label><input form="gestorChange<?php echo $j; ?>" type="text" name="completo" value="<?php echo $completo; ?>" /></label></td>
-                        <td><label><input form="gestorChange<?php echo $j; ?>" type="password" name="passw" value="<?php echo $passw; ?>" /></label></td>
-                        <td><label>
-                            <select form="gestorChange<?php echo $j; ?>" name="tipo">
-                                <option value=""></option>
-                                <?php
-                                foreach ($groups as $g) {
-                                    $group = $g['grupo'];
+                            <td>
+                                <form class="gestorChange" name="gestorChange<?php echo $j; ?>" method="get" action="/gestorAdmin.php" id="gestorChange<?php echo $j; ?>">
+                                    <input type="hidden" name="capt" value="<?php echo $capt ?>">
+                                    <input type="hidden" name="j" value="<?php echo $j ?>">
+                                    <input type="text" name="usuaria" readonly class="form-control-plaintext" value="<?php echo $usuaria; ?>" />
+                                </form>
+                            </td>
+                            <td><input form="gestorChange<?php echo $j; ?>" type="text" name="completo" class="form-control" value="<?php echo $completo; ?>" /></td>
+                            <td><input form="gestorChange<?php echo $j; ?>" type="password" name="passw" class="form-control" value="<?php echo $passw; ?>" /></td>
+                            <td>
+                                <select form="gestorChange<?php echo $j; ?>" name="tipo" class="form-select">
+                                    <option value=""></option>
+                                    <?php foreach ($groups as $g) {
+                                        $group = $g['grupo'];
                                     ?>
-                                    <option value="<?php
-                                    if (isset($group)) {
-                                        echo $group;
-                                    }
-                                    ?>" style="font-size:120%;" <?php
-                                            if (strtolower($group) == strtolower($tipo)) {
-                                                echo "selected='selected'";
-                                            }
-                                            ?>>
-                                                <?php
-                                                if (isset($group)) {
-                                                    echo $group;
-                                                }
-                                                ?></option>
-                                <?php }
-                                ?>
-                            </select></label></td>
-                        <td><input form="gestorChange<?php echo $j; ?>" type="submit" name="go" value="GUARDAR">
-                        </td>
-                        <td><input form="gestorChange<?php echo $j; ?>" type="submit" name="go" value="BORRAR">
-                        </td>
-                    </tr>
-                <?php }
-                ?>
-                <tr>
-                <td><form class="gestorAdd"
-                              name="gestorAdd"
-                              method="get"
-                              action="/gestorAdmin.php"
-                              id="gestorAdd">
-                            <input type="hidden" name="capt" value="<?php echo $capt ?>">
-                            <label><input type="text" name="usuaria"  value="" /></label>
-                    </form></td>
-                    <td><label><input form="gestorAdd" type="text" name="completo" value="" /></label></td>
-                    <td><label><input form="gestorAdd" type="password" name="passw" value="" /></label></td>
-                    <td><label>
-                        <select form="gestorAdd" name="tipo">
-                            <option value=""></option>
-                            <?php
-                            foreach ($groups as $g) {
-                                    $group = $g['grupo'];
-                                ?>
-                                <option value="<?php
-                                if (isset($group)) {
-                                    echo $group;
-                                }
-                                ?>" style="font-size:120%;">
-                                    <?php
-                                    if (isset($group)) {
-                                        echo $group;
-                                    }
-                                    ?></option>
-                            <?php }
-                            ?>
-                        </select></label></td>
-                    <td><input form="gestorAdd" type="submit" name="go" value="AGREGAR">
-                    </td>
-                </tr>
-                </tbody>
-        </table>
+                                    <option value="<?php echo $group ?? ''; ?>" <?php if (strtolower($group) == strtolower($tipo)) echo "selected"; ?>><?php echo $group ?? ''; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </td>
+                            <td><input form="gestorChange<?php echo $j; ?>" type="submit" name="go" value="GUARDAR" class="btn btn-success btn-sm"></td>
+                            <td><input form="gestorChange<?php echo $j; ?>" type="submit" name="go" value="BORRAR" class="btn btn-danger btn-sm"></td>
+                        </tr>
+                        <?php } ?>
+                        <tr>
+                            <td>
+                                <form class="gestorAdd" name="gestorAdd" method="get" action="/gestorAdmin.php" id="gestorAdd">
+                                    <input type="hidden" name="capt" value="<?php echo $capt ?>">
+                                    <input type="text" name="usuaria" class="form-control" value="" />
+                                </form>
+                            </td>
+                            <td><input form="gestorAdd" type="text" name="completo" class="form-control" value="" /></td>
+                            <td><input form="gestorAdd" type="password" name="passw" class="form-control" value="" /></td>
+                            <td>
+                                <select form="gestorAdd" name="tipo" class="form-select">
+                                    <option value=""></option>
+                                    <?php foreach ($groups as $g) {
+                                        $group = $g['grupo'];
+                                    ?>
+                                    <option value="<?php echo $group ?? ''; ?>"><?php echo $group ?? ''; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </td>
+                            <td colspan="2"><input form="gestorAdd" type="submit" name="go" value="AGREGAR" class="btn btn-primary btn-sm w-100"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <script src="/js/bootstrap.bundle.min.js"></script>
     </body>
-</html> 
+</html>
--- views/indexView.php
+++ views/indexView.php
@@ -1,42 +1,80 @@
 <?php
-$msg = 'Cobra';
+$msg = 'COBRA';
 ?>
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <title>COBRA</title>
-        <?php
-        $msg = 'COBRA';
-        ?>
-        <style>
-            body {text-align: center; background-color: #ebd5b3; width: 50em; color:#000000;}
-            div.forma {margin-left:5.5cm; font-weight: bold;}
-            fieldset {width: 21em; background-color: #c0c0c0;}
-        </style>
-    </head>
-    <body>
-        <h1><?php echo $msg; ?></h1>
-        <em>versi&oacute;n configurado 2025</em>
-        <div class="forma">
+<head>
+    <title>COBRA</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap 5 CSS (local) -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #ebd5b3;
+            color: #222;
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+        .login-container {
+            max-width: 400px;
+            margin: 3rem auto;
+            background: #fffbe9;
+            border-radius: 1rem;
+            box-shadow: 0 0 16px #c0c0c0;
+            padding: 2rem 2.5rem 1.5rem 2.5rem;
+        }
+        .form-label {
+            font-weight: 500;
+        }
+        .brand {
+            font-size: 2.5rem;
+            font-weight: bold;
+            letter-spacing: 0.1em;
+            color: #795548;
+        }
+        .version {
+            color: #888;
+            font-size: 1rem;
+        }
+        .copyright {
+            color: #888;
+            font-size: 0.95rem;
+            margin-top: 2rem;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="login-container">
+            <div class="text-center mb-4">
+                <span class="brand"><?php echo $msg; ?></span>
+                <div class="version">versi&oacute;n configurado 2025</div>
+            </div>
             <form action="/index.php" method="post" autocomplete="off">
-                <fieldset>
-                    <div class="username">
-                        <label for="capt" class="formCap">Usuario:</label>
-                        <input type="text" name="capt" id="capt" value=""
-                               onchange="this.value = this.value.replace(/ /g, '');" />
-                        <br>
-                    </div>
-                    <div class="contrasena">
-                        <label for="pwd" class="formCap">Contrase&ntilde;a:</label>
-                        <input type="password" name="pwd" id="pwd" value=""/><br>
-                    </div>
-                    <input type="submit" name="go" value="Empezar" />
-                </fieldset>
+                <div class="mb-3">
+                    <label for="capt" class="form-label">Usuario:</label>
+                    <input type="text" class="form-control" name="capt" id="capt" required
+                        onchange="this.value = this.value.replace(/ /g, '');" autocomplete="username">
+                </div>
+                <div class="mb-3">
+                    <label for="pwd" class="form-label">Contrase&ntilde;a:</label>
+                    <input type="password" class="form-control" name="pwd" id="pwd" required autocomplete="current-password">
+                </div>
+                <div class="d-grid">
+                    <button type="submit" name="go" class="btn btn-primary" value="<?php echo time(); ?>">Empezar</button>
+                </div>
             </form>
         </div>
-        <p>&nbsp;</p>
-        <a href="/licencia.txt">
-            <cite>&copy;GMBS Consulting 2025</cite>
-        </a>
-    </body>
+        <div class="text-center copyright">
+            <a href="/licencia.txt" class="text-decoration-none">
+                <cite>&copy; GMBS Consulting 2025</cite>
+            </a>
+        </div>
+    </div>
+    <!-- Bootstrap 5 JS (local) -->
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
